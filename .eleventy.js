48f691a6cabad510c2bf0fef750abaeb27a36299
billy.oliver <billy.oliver@just-eat.com>
2022-06-17 15:21:20 +0100

    Added Single File 'Vue' page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const eleventyVue = require("@11ty/eleventy-plugin-vue");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyVue);
+  
   return {
     dir: {
       input: "src",
