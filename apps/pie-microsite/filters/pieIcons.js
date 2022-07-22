4f2b57adeeefe46f1a64dce522e85ff377c20da2
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-07-22 17:06:31 +0100

    allow attributes to be passed into icons

--- apps/pie-microsite/filters/pieIcons.js
+++ apps/pie-microsite/filters/pieIcons.js
@@ -1,9 +1,12 @@
 const pieicons = require('@justeattakeaway/pie-icons');
 
-module.exports = function(iconName) {
+module.exports = function(iconConfig = {
+  name: '',
+  attrs: {}
+}) {
     try {
-      return pieicons.default.icons[iconName].toSvg();
+      return pieicons.default.icons[iconConfig.name].toSvg(iconConfig.attrs);
     } catch (error) {
-      console.error(`Could not find icon of name: ${iconName}. Error: ${error}`);
+      console.error(`Could not find icon of name: ${iconConfig.name}. Error: ${error}`);
     }
 }
