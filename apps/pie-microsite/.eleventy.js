ca2d1acb44e56ad7f8f810613bc277a17ce234f1
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-07-25 14:17:09 +0100

    use webpack for css extraction and compilation

--- apps/pie-microsite/.eleventy.js
+++ apps/pie-microsite/.eleventy.js
@@ -1,23 +1,4 @@
-const sass = require('sass');
-
 module.exports = function (eleventyConfig) {
-
-  eleventyConfig.addTemplateFormats('scss');
-
-  eleventyConfig.addExtension('scss', {
-    outputFileExtension: 'css',
-
-    compile: async function(inputContent) {
-      let result = sass.compileString(inputContent, {
-        includePaths: ['node_modules']
-      });
-
-      return async () => {
-        return result.css;
-      };
-    }
-  });
-
   return {
     dir: {
       input: 'src',
---
+++ apps/pie-microsite/webpack.config.js
@@ -0,0 +1,26 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // fallback to style-loader in development
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
