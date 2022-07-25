b66c05eaebc9a6b059f315ad57b5a4007fc6469d
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-07-25 16:48:29 +0100

    build sass with 11ty

--- apps/pie-microsite/.eleventy.js
+++ apps/pie-microsite/.eleventy.js
@@ -1,4 +1,6 @@
 // const eleventyVue = require("@11ty/eleventy-plugin-vue");
+const sass = require("sass");
+const path = require("node:path");
 
 /**
  * Returns a collection of all unique page category tag strings (excluding the 'pages' tag)
@@ -25,6 +27,29 @@ const getAllPageCategories = collectionApi => {
 module.exports = function (eleventyConfig) {
   // eleventyConfig.addPlugin(eleventyVue);
   eleventyConfig.addCollection("pageCategories", getAllPageCategories);
+  // add as a valid template language to process, e.g. this adds to --formats
+  eleventyConfig.addTemplateFormats("scss");
+
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css", // optional, default: "html"
+
+    // can be an async function
+    compile: function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [
+          parsed.dir || ".",
+          this.config.dir.includes,
+          '../../node_modules/'
+        ]
+      });
+
+      return (data) => {
+        return result.css;
+      };
+    }
+  });
 
   return {
     dir: {
