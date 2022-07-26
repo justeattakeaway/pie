f26e9f368f9ed66417c04fac7c4410cad5b1ccbc
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-07-26 14:03:31 +0100

    improve 11ty project structure with comments

--- apps/pie-microsite/src/_11ty/filters/index.js
+++ apps/pie-microsite/src/_11ty/filters/index.js
@@ -1,5 +1,9 @@
 const pieIconsSvg = require('./pieIconsSvg');
 
+/**
+ * All custom 11ty filters should be part of the exported object
+ * So that they can be registered in the .eleventy.js file
+ */
 module.exports = {
   pieIconsSvg
 }
