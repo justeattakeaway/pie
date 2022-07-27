917ddb24b23df91fc504abdea4c8ca3bb6e1ba12
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-07-27 11:34:04 +0100

    extract magic strings to constants

--- apps/pie-microsite/src/_11ty/template-handlers/scss/scss.js
+++ apps/pie-microsite/src/_11ty/template-handlers/scss/scss.js
@@ -2,14 +2,18 @@ const sass = require("sass");
 const path = require("node:path");
 const postCSSProcessor = require("./postcss");
 
+const TEMPLATE_EXTENSION = "scss";
+const OUTPUT_EXTENSION = "css";
+const PATH_TO_NODE_MODULES = "../../node_modules/"; // Turborepo keeps dependencies at the project root. This allows SCSS files to reference extenal packages in node_modules
+
 /**
  * Tells Eleventy how to compile SCSS files into CSS when building the project
  * @param {*} eleventyConfig 
  */
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addTemplateFormats("scss");
-  eleventyConfig.addExtension("scss", {
-    outputFileExtension: "css",
+  eleventyConfig.addTemplateFormats(TEMPLATE_EXTENSION);
+  eleventyConfig.addExtension(TEMPLATE_EXTENSION, {
+    outputFileExtension: OUTPUT_EXTENSION,
 
     compile: function (inputContent, inputPath) {
       const parsed = path.parse(inputPath);
@@ -23,7 +27,7 @@ module.exports = function (eleventyConfig) {
         loadPaths: [
           parsed.dir || ".",
           this.config.dir.includes,
-          "../../node_modules/", // Turborepo keeps dependencies at the project root. This allows SCSS files to reference extenal packages in node_modules
+          PATH_TO_NODE_MODULES,
         ],
       });
 
