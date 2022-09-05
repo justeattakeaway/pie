ddfd65eda19f74b20d7408188b72dd7dc90c6263
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-09-05 16:26:30 +0100

    add colour page images

--- apps/pie-microsite/src/_11ty/shortcodes/contentPageImage.js
+++ apps/pie-microsite/src/_11ty/shortcodes/contentPageImage.js
@@ -1,8 +1,32 @@
+createCaption = function (config) {
+  if (!config.caption) {
+    return '';
+  }
+
+  if (config.captionType === 'list') {
+    return `<figcaption>
+      ${config.caption}
+      <ul>
+        ${config.captionListItems.map(item => `<li>${item}</li>`).join('')}
+      </ul>
+    </figcaption>`;
+  }
+
+  return `<figcaption>${config.caption}</figcaption>`;
+};
+
 module.exports = function (config) {
+  if (config.type !== 'spread') {
+    return `<figure class="c-contentPage-image c-contentPage-image--with-bg">
+      <div>
+        <img style="--img-width: ${config.width};" src="${config.src}" ${config.alt ? `alt="${config.alt}"` : ''} />
+      </div>
+      ${createCaption(config)}
+    </figure>`;
+  }
+  
   return `<figure class="c-contentPage-image">
-    <div>
-      <img style="--img-width: ${config.width};" src="${config.src}" ${config.alt ? `alt="${config.alt}"` : ''} />
-    </div>
-    ${config.caption ? `<figcaption>${config.caption}</figcaption>` : '' }
-  </figure>`;
+      <img src="${config.src}" ${config.alt ? `alt="${config.alt}"` : ''} />
+      ${createCaption(config)}
+    </figure>`;
 }
