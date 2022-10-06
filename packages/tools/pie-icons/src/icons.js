acd8dd820f5b62e7149a0d0dedc12f7d8f5a5aef
Ashley Watson-Nolan <nolly00@gmail.com>
2022-10-06 11:47:24 +0100

    Adding github action config for running unit tests

--- packages/tools/pie-icons/src/icons.js
+++ packages/tools/pie-icons/src/icons.js
@@ -1,5 +1,5 @@
 import Icon from './icon';
-import icons from '../dist/icons.json';
+import icons from '../dist/icons.json'; // eslint-disable-line import/no-unresolved
 
 export default Object.keys(icons)
   .map(key => new Icon(key, icons[key].contents, icons[key].attrs))
