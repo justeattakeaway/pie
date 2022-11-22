3effc9646988fec62ccbaab3d60a275d8197d745
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-11-22 13:13:14 +0000

    remove unecessary data file

--- apps/pie-microsite/src/_data/designTokens.js
+++
@@ -1,6 +0,0 @@
-const designTokensJson = require('@justeat/pie-design-tokens/dist/tokens.json');
-
-// eslint-disable-next-line func-names
-module.exports = function () {
-    return designTokensJson.theme.jet;
-};
