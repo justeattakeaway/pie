d25f7f4fd2cd519aa397ae8560c60459daabdc3d
siggerzz <bensiggery28@gmail.com>
2022-12-04 21:58:51 +0000

    Add headless args for CI

--- wdio.conf.js
+++ wdio.conf.js
@@ -60,7 +60,15 @@ exports.config = {
         maxInstances: 1,
         //
         browserName: 'chrome',
-        acceptInsecureCerts: true
+        acceptInsecureCerts: true,
+        'goog:chromeOptions': {
+            args: [].concat(process.env.CI ? [
+                '--no-sandbox',
+                '--disable-infobars',
+                '--headless',
+                '--disable-gpu',
+                '--window-size=1920,1080'] : [])
+        }
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
