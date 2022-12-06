50faae2e66b346c76d81bc03f27a44c2d37df38b
Ben Siggery <bensiggery28@gmail.com>
2022-12-06 22:48:07 +0000

    Fix lint

--- apps/pie-microsite/test/helpers/configuration-helper.js
+++ apps/pie-microsite/test/helpers/configuration-helper.js
@@ -1,14 +1,9 @@
-const { path } = require("chromedriver");
-
 exports.getBaseUrl = () => {
     if (!process.env.CI) {
         return 'http://localhost:8080/';
     }
-    else {
-        
-        const baseUrl = 'https://pie.design/';
-        const path = process.env.GITHUB_REF_NAME !== 'main' ? PIE_URL_PREFIX : '/'
+    const baseUrl = 'https://pie.design/';
+    const path = process.env.GITHUB_REF_NAME !== 'main' ? process.env.PIE_URL_PREFIX : '/';
 
-        return baseUrl + path;
-    }
+    return baseUrl + path;
 };
