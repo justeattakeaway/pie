2765e4cb20bf50195fbfcb3e59b77df275a8af35
Ben Siggery <bensiggery28@gmail.com>
2022-12-06 21:06:17 +0000

    Fix config

--- apps/pie-microsite/test/helpers/configuration-helper.js
+++ apps/pie-microsite/test/helpers/configuration-helper.js
@@ -1,14 +1,11 @@
-const getBaseUrl = () => {
+exports.getBaseUrl = () => {
     if (!process.env.CI) {
         return 'http://localhost:8080';
-    }
-    else if (process.env.GITHUB_REF_NAME === 'main') {
+    } else if (process.env.GITHUB_REF_NAME === 'main') {
         return 'https://www.pie.design';
     } else if (process.env.GITHUB_REF_NAME !== 'main') {
         return process.env.PREVIEW_URL;
-    } else {
-        throw new Error('Unable to retrieve base url.');
     }
-}
 
-export { getBaseUrl };
+    throw new Error('Unable to retrieve base url.');
+};
