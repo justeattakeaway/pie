bb290b9a1601f5df799979a64da25d139ba79d74
FayeCarter <faye.carter@justeattakeaway.com>
2022-12-13 11:23:03 +0000

    Update function descriptions and rename metadata files

--- apps/pie-microsite/src/_11ty/shortcodes/tokensTable.js
+++ apps/pie-microsite/src/_11ty/shortcodes/tokensTable.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-trailing-spaces */
 const pieDesignTokens = require('@justeat/pie-design-tokens/dist/tokens.json');
-const pieTokensMetadata = require('../../pieDesignTokensMetadata.json');
-const pieTokenCategories = require('../../pieDesignTokenCategories.json');
+const pieTokensMetadata = require('../../tokensMetadata.json');
+const pieTokenCategories = require('../../tokenCategories.json');
 const { stringHelpers, objectHelpers } = require('../../utilities/helpers');
 const tokenTypes = require('../../_data/tokenTypes');
 const { isColorDark } = require('../../utilities/colors');
@@ -10,7 +10,7 @@ const { isColorDark } = require('../../utilities/colors');
  * Takes the token key and token type and
  * Creates a SCSS token name such as '$color-black'
  * @param {string} tokenKey - the token key i.e. 'support-positive-02'
- * @param {string} tokenType - the type of token such as color, spacing or radius
+ * @param {string} tokenType - the type of token i.e. color, spacing, radius
  * @returns {string} the SCSS variable name
  */
 const createScssTokenName = (tokenKey, tokenType) => `$${tokenType}-${tokenKey}`;
@@ -18,7 +18,7 @@ const createScssTokenName = (tokenKey, tokenType) => `$${tokenType}-${tokenKey}`
 /**
  * Creates a display name of the provided token. 'system-purple' would become 'System Purple'
  * @param {string} tokenKey - the token key i.e. 'support-positive-02'
- * @param {string} tokenType - the type of token such as color, spacing or radius
+ * @param {string} tokenType - the type of token i.e. color, spacing, radius
  * @returns {string} the display name of the token
  */
 const createTokenDisplayName = (tokenKey, tokenType) => {
@@ -35,7 +35,7 @@ const createTokenDisplayName = (tokenKey, tokenType) => {
 
 /**
  * Splits a color token into it's hexcode and opacity value (if one is provided)
- * @param {string} token - the color token to split
+ * @param {string} token - the token value i.e. #000, #ffffff, #000|0.85 or #000000|0.85
  * @returns {object} an object containing a hexcode and opacity value (if opacity was provided)
  */
 const splitColorToken = token => {
@@ -50,7 +50,7 @@ const splitColorToken = token => {
 /**
  * Builds the example color swatch to show on the token list item
  * @param {string} token - the token value i.e. #000, #ffffff, #000|0.85 or #000000|0.85
- * @returns {string} the color swatch example HTML string
+ * @returns {string} - the color swatch example HTML string
  */
 const buildColorExample = token => {
     const tokenValues = splitColorToken(token);
@@ -75,9 +75,9 @@ const buildColorExample = token => {
 /**
  * Builds an example element to display in the token list item.
  * This could be a color swatch, a representation of border radius or spacing etc.
- * @param {string} token - the token value
+ * @param {string} token - the token value i.e. #000, #ffffff, #000|0.85 or #000000|0.85
  * @param {string} tokenType - the type of token i.e. color, spacing, radius
- * @returns {string} the example HTML string
+ * @returns {string} - the example HTML string
  */
 const buildTokenExampleElement = (token, tokenType) => {
     const tokenExampleElementHandler = {
@@ -94,20 +94,20 @@ const buildTokenExampleElement = (token, tokenType) => {
 
 /**
  * Builds a token pill element to display the SCSS token name in the token list item.
- * @param {string} tokenScssName - the text to display
- * @returns {string} the token pill HTML string
+ * @param {string} tokenScssName - the design token SCSS name i.e. '$color-black'
+ * @returns {string} - the token pill HTML string
  */
 const buildTokenPill = tokenScssName => `<span class="c-tokensTable-token">${tokenScssName}</span>`;
 
 /**
  * Builds a token list item element to add to the tokens list.
  * @param {object} config
- * @param {string} config.token - the design token value
+ * @param {string} config.token - the token value i.e. #000, #ffffff, #000|0.85 or #000000|0.85
  * @param {string} config.tokenType - the type of design token i.e. color, spacing, radius
  * @param {string} config.tokenScssName - the design token SCSS name i.e. '$color-black'
  * @param {string} config.tokenDisplayName - the display name of the token i.e. 'Black'
  * @param {object} config.tokenMetadata - the metadata for the token. data such as descriptions
- * @returns {string} the list item HTML string
+ * @returns {string} - the list item HTML string
  */
 const buildTokenListElements = ({
     token,
@@ -138,7 +138,7 @@ const buildTokenListElements = ({
 /**
  * Takes a list of design token list items and outputs these with appropriate column headers to be displayed above them
  * @param {string[]} listElements - the list items to render within the list
- * @returns {string} the tokens list HTML elements
+ * @returns {string} - the tokens list HTML elements
  */
 const buildTokensList = listElements => `<div class="c-tokensTable-row u-spacing-e--top u-showAboveWide c-tokensTable-heading">
   <span>Example</span>
@@ -151,11 +151,11 @@ const buildTokensList = listElements => `<div class="c-tokensTable-row u-spacing
 
 /**
  * Creates a tokens list for a given category
- * @param {*} tokens 
- * @param {string} path 
- * @param {string} category 
- * @param {string} tokenType 
- * @returns 
+ * @param {object} tokens
+ * @param {string} path - path to the category i.e.  'path:color.alias.default' / 'path:color.alias.dark'
+ * @param {string} category - category that pie tokens are grouped by i.e.  'containerBackgrounds' / 'borders'
+ * @param {string} tokenType - the type of token i.e. color, spacing, radius
+ * @returns - a list of HTML token components separated by a <hr />
  */
 const buildTokensListForCategory = (tokens, path, category, tokenType) => {
     const tokenTypeMetadata = getTokenTypeMetadata(path);
@@ -175,7 +175,10 @@ const buildTokensListForCategory = (tokens, path, category, tokenType) => {
 
 /**
  * Builds a categorised list of tokens
- * @returns {string}
+ * @param {string} path - path to the category i.e.  'path:color.alias.default' / 'path:color.alias.dark'
+ * @param {string} tokenType - the type of token i.e. color, spacing, radius
+ * @param {object} tokens
+ * @returns - object of token categories with display names i.e.  whiteBlack: { displayName: 'White and Black' }
  */
 const buildCategorisedLists = ({
     path, tokenType, tokens 
@@ -197,16 +200,16 @@ const buildCategorisedLists = ({
 
 /**
  * Gets all the metadata associated with tokens of a given type such as colour.
- * @param {string} path 
- * @returns {object}
+ * @param {string} path - path to the category i.e. 'path:color.alias.default' / 'path:color.alias.dark'
+ * @returns {object} - object of tokens and the category they are sorted by i.e. white: { category: 'whiteBlack' }
  */
 const getTokenTypeMetadata = path => objectHelpers.getObjectPropertyByPath(pieTokensMetadata, path);
 
 /**
  * Gets all tokens for a given category such as 'orange'
- * @param {string} category 
- * @param {object} tokenTypeMetadata 
- * @returns {string[]}
+ * @param {string} category - category that pie tokens are grouped by i.e.  'containerBackgrounds' / 'borders'
+ * @param {string} tokenTypeMetadata - the type of token i.e. color, spacing, radius
+ * @returns {string[]} - i.e. an array of tokens in each category [ 'divider-default', 'divider-inverse' ]
  */
 const getTokensForCategory = (category, tokenTypeMetadata) => Object
     .keys(tokenTypeMetadata)
@@ -215,9 +218,9 @@ const getTokensForCategory = (category, tokenTypeMetadata) => Object
 
 /**
  * Gets all subcategory keys for a given parent category
- * @param {object} tokenTypeCategories 
- * @param {string} parentCategoryKey 
- * @returns 
+ * @param {object} tokenTypeCategories - object of token category information
+ * @param {string} parentCategoryKey - token category name i.e. 'reactive'
+ * @returns - list of subcategory keys for a given parent category
  */
 const getSubcategoriesForParentCategory = (tokenTypeCategories, parentCategoryKey) => Object
           .keys(tokenTypeCategories)
@@ -226,7 +229,7 @@ const getSubcategoriesForParentCategory = (tokenTypeCategories, parentCategoryKe
 /**
  * Builds a list of tokens that are categorised by parent and subcategory
  * @param {object} config 
- * @returns {string}
+ * @returns {string} - list of tokens categorised by parent and subcategory
  */
 const buildCategoryListsWithParents = ({
     parentCategories, path, tokenType, isGlobal, tokens 
@@ -258,17 +261,16 @@ const buildCategoryListsWithParents = ({
 
 /**
  * Builds all of the token lists for a given token type
- * @param {string} pathToTokens 
- * @param {string} path 
- * @param {string} tokenType 
- * @returns {string}
+ * @param {string} path - path to the category i.e.  'path:color.alias.default' / 'path:color.alias.dark'
+ * @param {string} tokenType - the type of token i.e. color, spacing, radius
+ * @returns {string} - HTML list of tokens
  */
 const buildTokenLists = (path, tokenType) => {
     const isGlobal = path.includes('global');
     const tokens = objectHelpers.getObjectPropertyByPath(pieDesignTokens, `theme.jet.${path}`);
     const parentCategories = getParentCategoriesForTokenType(`${tokenType}.${isGlobal ? 'global' : 'alias'}.parentCategories`);
 
-    const config = {
+    const config = { 
         parentCategories,
         path,
         tokenType,
@@ -303,8 +305,8 @@ const validateConfiguration = ({ path, tokenType }) => {
 
 /**
  * Gets any parent categories for a token type such as colour, spacing
- * @param {*} parentCategoryPath 
- * @returns 
+ * @param {string} parentCategoryPath 
+ * @returns - any parent categories for a token type
  */
 const getParentCategoriesForTokenType = parentCategoryPath => objectHelpers.getObjectPropertyByPath(pieTokenCategories, parentCategoryPath);
 
