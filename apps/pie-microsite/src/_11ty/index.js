aecd9b3128a8b50d3f3410f4396f0d492680fb9e
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-01-03 16:25:50 +0000

    add unit tests

--- apps/pie-microsite/src/_11ty/index.js
+++ apps/pie-microsite/src/_11ty/index.js
@@ -2,6 +2,7 @@ const collections = require('./collections');
 const filters = require('./filters');
 const shortcodes = require('./shortcodes');
 const plugins = require('./plugins');
+
 /**
  * All resources needed for the .eleventy.js file should be exported from here
  * such as custom collections, filters, template handlers and shortcodes
--- apps/pie-microsite/src/_11ty/shortcodes/index.js
+++ apps/pie-microsite/src/_11ty/shortcodes/index.js
@@ -2,14 +2,7 @@ const contentPageImage = require('./contentPageImage');
 const notification = require('./notification');
 const tokensTable = require('./tokensTable');
 
-/**
- * Removes any whitespace created by indenting template strings inside shortcode functions.
- * Without doing this, indentation can accidentally escape code when the markdown/nunjucks engine tries to render the code.
- * Example of issue: https://github.com/11ty/eleventy/issues/402
- * @param {String} htmlString - the HTML string returned from a shortcode
- * @returns {String} the HTML string with indentation whitespace removed
- */
-const deindentHTML = htmlString => htmlString.replace(/\n|\t/g, '').trim();
+const { deindentHTML } = require('./shortcode-utilities');
 
 /**
  * Adds all 11ty shortcodes
