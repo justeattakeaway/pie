3bff474ace9bb1ec2b408310de29338271b9f25c
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-01-05 13:12:19 +0000

    add comment to shortcode max width and extract to variable

--- apps/pie-microsite/src/_11ty/shortcodes/contentPageImage.js
+++ apps/pie-microsite/src/_11ty/shortcodes/contentPageImage.js
@@ -33,10 +33,14 @@ module.exports = function (config) {
         ...(!isImageFullContainerWidth ? ['c-contentImage--hasBackdrop'] : [])
     ];
 
+    // This is based on the narrowMid breakpoint defined in fozzie:
+    // https://github.com/justeat/fozzie-components/blob/7fd01766b3126b30b1ab704c131b8ac767f882ea/packages/tools/fozzie/src/scss/tools/helpers/_breakpoints.scss#L9
+    const mobileImageMaxWidth = '600px';
+
     return `<figure class="${figureClasses.join(' ')}">
         <div class="c-contentImage-backdrop">
           <picture>
-            ${config.mobileSrc ? `<source ${imageStyles} media="(max-width: 600px)" srcset="${config.mobileSrc}">` : ''}
+            ${config.mobileSrc ? `<source ${imageStyles} media="(max-width: ${mobileImageMaxWidth})" srcset="${config.mobileSrc}">` : ''}
             <img src="${config.src}" ${imageStyles} ${imageAlt}> 
           </picture>
         </div>
