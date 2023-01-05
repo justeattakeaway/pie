94bfd41c36d23a4350e2e9373aa4292b57cad371
FayeCarter <faye.carter@justeattakeaway.com>
2023-01-05 11:38:57 +0000

    Update notification to render markdown inline

--- apps/pie-microsite/src/_11ty/filters/markdown.js
+++ apps/pie-microsite/src/_11ty/filters/markdown.js
@@ -4,10 +4,4 @@ const md = new MarkdownIt({
     html: true
 });
 
-/**
- * Converts markdown to HTML
- * @param {string} content - The content to be converted.
- * @param {boolean} inline - if true will return content in <p></p> tags. If false will return content without enclosing tags.
- * @returns {string}
- */
-module.exports = (content, inline = false) => (inline ? `<span>${md.renderInline(content)}</span>` : md.render(content));
+module.exports = (content, inline = false) => (inline ? md.renderInline(content) : md.render(content));
