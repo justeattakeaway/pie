1e91ed4222a87cd5a5aa63f204a384bd0134104e
FayeCarter <faye.carter@justeattakeaway.com>
2023-01-06 14:27:32 +0000

    Add documentation and update styles

--- apps/pie-microsite/src/_11ty/filters/markdown.js
+++ apps/pie-microsite/src/_11ty/filters/markdown.js
@@ -4,4 +4,10 @@ const md = new MarkdownIt({
     html: true
 });
 
+/**
+ * A List HTML component
+ * @param {string} content - Type of list: orderd, pill
+ * @param {boolean} inline - if true will return content in <p></p> tags. if false will return content without enclosing tags.
+ * @returns {string}
+ */
 module.exports = (content, inline = false) => (inline ? md.renderInline(content) : md.render(content));
