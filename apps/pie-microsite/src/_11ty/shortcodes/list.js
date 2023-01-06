51dcd4b28b57d7d8380a8570dc50fc86239fb5fb
FayeCarter <faye.carter@justeattakeaway.com>
2023-01-06 14:39:12 +0000

    Add listTypes

--- apps/pie-microsite/src/_11ty/shortcodes/list.js
+++ apps/pie-microsite/src/_11ty/shortcodes/list.js
@@ -1,20 +1,26 @@
 const markdownFilter = require('../filters/markdown');
+const listTypes = require('../../_data/listTypes');
 
 /**
  * A List HTML component
- * @param {string} type - Type of list: orderd, pill
- * @param {string} items - An array of list items
+ * @param {string} type - Type of list: ordered, pill
+ * @param {string[]} items - An array of list items
  * @returns {string}
  */
 // eslint-disable-next-line func-names
 module.exports = function ({ type, items }) {
+    if (!type || !listTypes[type]) {
+        throw new Error(`List 'type = ${type}' not recognised. Try 'ordered' or 'pill'`);
+    }
+
     const listItems = items.map(item => `<li class="c-list-item"><span>${markdownFilter(item, true)}</span></li>`).join('');
+    const listTag = type === listTypes.ordered ? 'ol' : 'ul';
     const listClasses = [
         'c-list',
         `c-list--${type}`
     ];
 
-    return `<ul class="${listClasses.join(' ')}">
+    return `<${listTag} class="${listClasses.join(' ')}">
         ${listItems}
-    </ul>`;
+    </${listTag}>`;
 };
---
+++ apps/pie-microsite/src/_data/listTypes.js
@@ -0,0 +1,9 @@
+/**
+ * Defines all types of list i.e. ordered, pill.
+ * Prevents duplicate uses of magic strings across the project
+ * @returns {Object} - an option of string values
+ */
+module.exports = {
+    ordered: 'ordered',
+    pill: 'pill'
+};
