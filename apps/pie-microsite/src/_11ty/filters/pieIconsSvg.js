91ef1f244559883329cf83ccf1bdcf5dd1ac960f
FayeCarter <faye.carter@justeattakeaway.com>
2023-01-11 15:22:01 +0000

    pie-microsite@v1.32.0 - add Iconography library page (#67)
    * Add page structure
    
    * Add pill and ordered list shortcodes
    
    * Update notification to render markdown inline
    
    * Add copy
    
    * create list shortcode
    
    * Add tests
    
    * Fix lists on mobile
    
    * re-fix lists on mobile
    
    * update tests
    
    * Add documentation and update styles
    
    * Add listTypes
    
    * Fix documentation
    
    * update test
    
    * Change CSS selectors
    
    * Use inline markdown on image caption
    
    * Update snapshots
    
    * Updates from review comments
    
    * Add icon Table
    
    * rename shortcode
    
    * Update version
    
    * Update docs
    
    * Re-add test
    
    * Update changelog and filter docs
    
    * v1.31.0 - Added prism. (#68)
    
    * v1.31.0 - Added prism.
    
    * v1.31.0 - disable eslint.
    
    * v1.31.0 - Use default styles.
    
    * Update changelog and filter docs
    
    * Sort icons alphabetically
    
    * update icons
    
    * Handle small screens
    
    * Add snapshot test for iconList
    
    Co-authored-by: Kevin Rodrigues <kevin.rodrigues@just-eat.com>

--- apps/pie-microsite/src/_11ty/filters/pieIconsSvg.js
+++ apps/pie-microsite/src/_11ty/filters/pieIconsSvg.js
@@ -1,9 +1,25 @@
-const pieicons = require('@justeattakeaway/pie-icons');
+const pieIcons = require('@justeattakeaway/pie-icons');
+
+
+// eslint-disable-next-line consistent-return
+const getIconByName = (iconName, iconAttributes) => {
+    try {
+        return pieIcons.default.icons[iconName].toSvg(iconAttributes);
+    } catch (error) {
+    // eslint-disable-next-line no-console
+        console.error(`Could not find icon of name: ${iconName}. Error: ${error}`);
+    }
+};
+
+const getAllIcons = iconAttributes => Object.entries(pieIcons.default.icons).map(([key, value]) => ({
+    name: key,
+    icon: value.toSvg(iconAttributes)
+})).sort((a, b) => a.name.localeCompare(b.name)); // sort icons alphabetically by name
 
 /**
- * Custom filter that returns an SVG HTML string for a specified PIE Icon
+ * Custom filter that returns either an SVG HTML string for a specified PIE Icon or a HTML string for all PIE icons
  * @param {*} iconConfig
- * @param {string} iconConfig.name - the name of the icon to retrieve
+ * @param {string} iconConfig.name - the name of the icon to retrieve. If not provided, filter will return all PIE icons
  * @param {object} iconConfig.attrs - any attributes to add to the svg such as height, width, classes and fill
  * @returns
  */
@@ -12,18 +28,12 @@ module.exports = function (iconConfig = {
     name: '',
     attrs: {}
 }) {
-    try {
-        const defaultAttributes = {
-            height: 50,
-            width: 50,
-            fill: '#000'
-        };
+    const defaultAttributes = {
+        height: 50,
+        width: 50,
+        fill: '#000'
+    };
 
-        const attributes = { ...defaultAttributes, ...iconConfig.attrs };
-
-        return pieicons.default.icons[iconConfig.name].toSvg(attributes);
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error(`Could not find icon of name: ${iconConfig.name}. Error: ${error}`);
-    }
+    const attributes = { ...defaultAttributes, ...(iconConfig.attrs ? iconConfig.attrs : {}) };
+    return iconConfig && iconConfig.name ? getIconByName(iconConfig.name, attributes) : getAllIcons(attributes);
 };
