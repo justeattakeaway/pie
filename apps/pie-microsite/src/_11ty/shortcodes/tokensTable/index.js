1f38a6a77da36a3a781b7af6cf2453791aba4aa8
Fernando de Franca <fernando.defranca@justeattakeaway.com>
2023-01-13 09:32:13 +0100

    Add missing JSDoc and remove unnecessary content from example

--- apps/pie-microsite/src/_11ty/shortcodes/tokensTable/index.js
+++ apps/pie-microsite/src/_11ty/shortcodes/tokensTable/index.js
@@ -77,13 +77,18 @@ const buildColorExample = token => {
     return `<div class="${classes.join(' ')}" style="${cssVariable}";></div>`;
 };
 
+/**
+ * Builds the example radius swatch to show on the token list item
+ * @param {string} token - the token value in pixels
+ * @returns {string} - the radius swatch example HTML string
+ */
 const buildRadiusExample = token => {
     const classes = ['c-tokensTable-example-radius'];
     const style = `--example-radius: ${token}px`;
 
     return `
         <div class="c-tokensTable-example-radius-container">
-            <div class="${classes.join(' ')}" style="${style}">${token}</div>
+            <div class="${classes.join(' ')}" style="${style}"></div>
         </div>
     `;
 };
