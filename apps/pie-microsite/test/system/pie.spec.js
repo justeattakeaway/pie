84bf1298e1d67335d978cda7e36e8b77c0fc3e22
Ben Siggery <bensiggery28@gmail.com>
2023-01-22 18:47:45 +0000

    :art: (packages): Fix linting in pie-microsite

--- apps/pie-microsite/test/system/pie.spec.js
+++ apps/pie-microsite/test/system/pie.spec.js
@@ -3,15 +3,14 @@ import expectedRoutesJson from '../snapshots/expected-routes.snapshot.json';
 
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:8080');
+    await page.goto('http://localhost:8080');
 });
 
 
 test.describe('PIE - Status Code Tests - @system', () => {
     expectedRoutesJson.forEach(route => {
         test(`Should respond with a '200' status code for route - ${route}`, async ({ page }) => {
-            const url = await page.url() + `/content/pages/${route}`;
-
+            const url = `${await page.url()}/content/pages/${route}`;
 
             const response = await page.goto(url);
             await expect(response.status()).toBe(200);
