6d6364b4e34dc86bd3fddd2027d5cc8fb6daa091
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2023-01-23 10:03:18 +0000

    pie-button@0.2.0 - Add action type property

--- packages/components/pie-button/CHANGELOG.md
+++ packages/components/pie-button/CHANGELOG.md
@@ -4,6 +4,14 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+v0.2.0
+---------------------
+*January 23, 2023*
+
+### Added
+- `actionType` property mapped to `type` attribute.
+
+
 v0.1.0
 ---------------------
 *January 19, 2023*
--- packages/components/pie-button/src/index.ts
+++ packages/components/pie-button/src/index.ts
@@ -1,9 +1,14 @@
 import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 
 @customElement('pie-button')
 export class PieButton extends LitElement {
+    @property() actionType = 'submit';
+
     render () {
-        return html`<button>I'm a PIE button</button>`;
+        return html`
+        <button type=${this.actionType}>
+            I'm a PIE button
+        </button>`;
     }
 }
