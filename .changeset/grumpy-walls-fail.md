ef5b079c350394e01e588bb6ea952535ebde57ab
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-01-24 10:23:19 +0000

    new feature

---
+++ .changeset/grumpy-walls-fail.md
@@ -0,0 +1,5 @@
+---
+"pie-microsite": minor
+---
+
+Modifies babel config
--- apps/pie-microsite/babel.config.js
+++ apps/pie-microsite/babel.config.js
@@ -1,4 +1,6 @@
 // Babel config to allow import/export syntax in WDIO tests.
+
+//this is a new feature
 module.exports = {
     presets: [
         ['@babel/preset-env', {
