c53c5d04f579ccc954454f7eb606b5a97a16e56f
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-01-24 16:37:22 +0000

    Add new feature change to PIE microsite

--- apps/pie-microsite/babel.config.js
+++ apps/pie-microsite/babel.config.js
@@ -1,5 +1,6 @@
 // Babel config to allow import/export syntax in WDIO tests.
 module.exports = {
+    // this is a feature change
     presets: [
         ['@babel/preset-env', {
             targets: {
