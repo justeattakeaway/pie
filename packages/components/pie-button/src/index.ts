31273f3bb23dfa095ee22a79bde8e9173afe3a41
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2023-01-25 11:15:57 +0000

    v0.3.0 - Adds button styling. (#110)
    * v0.3.0 - Adds button styling.
    
    * v0.3.0 - Update changelog.
    
    * v0.3.0 - PR comment.
    
    * v0.3.0 - PR comments.

--- packages/components/pie-button/src/index.ts
+++ packages/components/pie-button/src/index.ts
@@ -1,9 +1,24 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 @customElement('pie-button')
 export class PieButton extends LitElement {
     @property() actionType = 'submit';
+
+    static styles = [css`
+      button {
+        background-color: var(--dt-color-interactive-brand);
+        border-radius: var(--dt-radius-rounded-e);
+        border: none;
+        color: var(--dt-color-content-interactive-primary);
+        font-size: 20px; // A future ticket will pull in a helper for font size & line height.
+        line-height: 1.4;
+        font-family: var(--dt-font-interactive-m-family);
+        font-weight: var(--dt-font-interactive-m-weight);
+        padding: 10px var(--dt-spacing-e);
+        min-height: 48px;
+      }
+    `];
 
     render () {
         return html`
