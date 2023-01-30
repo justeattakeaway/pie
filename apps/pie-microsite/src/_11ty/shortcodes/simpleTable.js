9f4ce1e4fd397178d8b41a83c71b7268389bd622
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2023-01-30 16:50:42 +0000

    pie-microsite@1.48.0 - Use join to get rid of pesky commas

--- apps/pie-microsite/src/_11ty/shortcodes/simpleTable.js
+++ apps/pie-microsite/src/_11ty/shortcodes/simpleTable.js
@@ -2,15 +2,13 @@ module.exports = ({ tableData }) => {
     const data = JSON.parse(tableData);
 
     return `<table>
-        <tr>
-            ${data.headings.map(heading => `
-            <th>${heading.text}</th>`)}
+        <tr>${data.headings.map(heading => `
+            <th>${heading.text}</th>`).join('')}
         </tr>
         ${data.rows.map(row => `
-        <tr>
-            ${row.data.map(data => `
-            <td>${data.text}</td>`)}
+        <tr>${row.data.map(data => `
+            <td>${data.text}</td>`).join('')}
         </tr>
-        `)}
+        `).join('')}
     </table>`;
 };
