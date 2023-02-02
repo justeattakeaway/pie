3710ac010426db522dcc8fb6ecf832dbcc292665
Ashley Watson-Nolan <ashley.watson-nolan@justeattakeaway.com>
2023-02-02 16:03:49 +0000

    pie-icons-react@v1.0.0-beta.1 – Updating to use pie-icons latest (#143)
    * pie-icons-react@v1.0.0-beta.1 – Updating to use pie-icons latest
    
    * Updating Changeset config to link icon package versions
    
    * Removing version bumps so that these are updated with changesets

---
+++ packages/tools/pie-icons-react/icons/NavigationExpand.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { SVGProps } from "react";
+const NavigationExpand = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" className="c-pieIcon c-pieIcon--navigationExpand" width="1em" height="1em" {...props}><path d="M1.93018 2.37341V13.4494H10.5694C11.4507 13.4494 12.2958 13.0993 12.919 12.4761C13.5421 11.853 13.8922 11.0078 13.8922 10.1266V2.37341H1.93018ZM3.25929 3.70253H6.36056V12.1202H3.25929V3.70253ZM12.5631 10.1266C12.5631 10.6553 12.353 11.1624 11.9792 11.5363C11.6053 11.9102 11.0982 12.1202 10.5694 12.1202H7.68967V3.70253H12.5631V10.1266Z" fill="#242E30" /><path d="M8.79727 9.66581L11.4023 7.8405L8.79727 6.16581" fill="#242E30" /></svg>;
+export default NavigationExpand;
--- packages/tools/pie-icons-react/scripts/react-icons-gen.js
+++ packages/tools/pie-icons-react/scripts/react-icons-gen.js
@@ -1,32 +1,45 @@
-const svgr = require("@svgr/core");
-const fs = require("fs");
-const pieIcons = require("@justeattakeaway/pie-icons").default;
-const { pascalCase } = require("pascal-case");
-const path = require("path");
+const svgr = require('@svgr/core');
+const pieIcons = require('@justeattakeaway/pie-icons').default;
+const { pascalCase } = require('pascal-case');
+const path = require('path');
+const fs = require('fs-extra');
 
-const icons = pieIcons.icons;
+const { icons } = pieIcons;
 
-const ICONS_DIR = process.cwd() + "/icons";
+const ICONS_DIR = `${process.cwd()}/icons`;
+const filePath = path.join(ICONS_DIR, '/index.tsx');
 
+async function checkDirExists (directoryPath) {
+    try {
+        await fs.ensureDir(directoryPath);
+        console.log(`Directory "${directoryPath}" exists.`);
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+// check that the /icons directory exists, if not create it
+checkDirExists(ICONS_DIR);
+
+// open a write stream to index.tsx
 const indexFile = fs.createWriteStream(
-  path.join(ICONS_DIR, "/index.tsx"),
-  (err) => {
-    console.error(err);
-  }
+    filePath,
+    err => {
+        console.error(err);
+    }
 );
 
-Object.keys(icons).map((iconKey) => {
-  const componentName = pascalCase(iconKey);
-  const Comp = svgr.transform.sync(
-    icons[iconKey].toSvg(),
-    { icon: true, typescript: true },
-    { componentName }
-  );
-  fs.writeFile(`${ICONS_DIR}/${componentName}.tsx`, Comp, (err) => {
-    if (err) console.error(err);
-  });
+// loop through the icons in pie-icons, generate each component and add it to the index.tsx
+Object.keys(icons).map(iconKey => {
+    const componentName = pascalCase(iconKey);
+    const Comp = svgr.transform.sync(
+        icons[iconKey].toSvg(),
+        { icon: true, typescript: true },
+        { componentName }
+    );
+    fs.writeFile(`${ICONS_DIR}/${componentName}.tsx`, Comp, err => {
+        if (err) console.error(err);
+    });
 
-  indexFile.write(
-    `export { default as ${componentName} } from './${componentName}';\n`
-  );
+    indexFile.write(`export { default as ${componentName} } from './${componentName}';\n`);
 });
