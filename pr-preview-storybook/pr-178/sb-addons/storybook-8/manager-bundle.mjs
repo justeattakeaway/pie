ab936296c3773484bdda449f17af6476f3cdc914
xander-marjoram <xander-marjoram@users.noreply.github.com>
2023-02-14 15:54:09 +0000

    Deploy preview for PR 178 🛫

--- pr-preview-storybook/pr-178/sb-addons/storybook-8/manager-bundle.mjs
+++ pr-preview-storybook/pr-178/sb-addons/storybook-8/manager-bundle.mjs
@@ -1,4 +1,4 @@
 try{
-var a=__STORYBOOKADDONS__,{addons:t,types:s,mockChannel:_}=__STORYBOOKADDONS__;var c=__STORYBOOKTHEMING__,{CacheProvider:h,ClassNames:i,Global:l,ThemeProvider:d,background:p,color:T,convert:g,create:S,createCache:f,createGlobal:u,createReset:x,css:y,darken:C,ensure:N,ignoreSsrWarning:R,isPropValid:k,jsx:B,keyframes:D,lighten:G,styled:K,themes:o,typography:Y,useTheme:b,withTheme:v}=__STORYBOOKTHEMING__;t.setConfig({theme:o.light});
+var s=__STORYBOOKADDONS__,{addons:o,types:_,mockChannel:n}=__STORYBOOKADDONS__;var i=__STORYBOOKTHEMING__,{CacheProvider:l,ClassNames:d,Global:p,ThemeProvider:T,background:g,color:S,convert:f,create:u,createCache:x,createGlobal:y,createReset:C,css:N,darken:R,ensure:k,ignoreSsrWarning:B,isPropValid:D,jsx:G,keyframes:K,lighten:Y,styled:b,themes:a,typography:v,useTheme:P,withTheme:A}=__STORYBOOKTHEMING__;o.setConfig({theme:a.light});
 }catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
 //# sourceMappingURL=manager-bundle.mjs.map
