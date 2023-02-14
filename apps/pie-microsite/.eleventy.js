41dbadb8e20e0c1527b54e28634dbf1728634f94
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-02-14 10:19:32 +0000

    add favicon files

--- apps/pie-microsite/.eleventy.js
+++ apps/pie-microsite/.eleventy.js
@@ -1,7 +1,7 @@
-const { 
+const {
   filters,
   libraries,
-  shortcodes, 
+  shortcodes,
   plugins
 } = require('./src/_11ty');
 
@@ -23,6 +23,16 @@ module.exports = eleventyConfig => {
 
   // Pass CNAME file into dist output (needed for domain to work correctly)
   eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
+  eleventyConfig.addPassthroughCopy("android-chrome-256x256.png");
+  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("browserconfig.xml");
+  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("mstile-150x150.png");
+  eleventyConfig.addPassthroughCopy("safari-pinned-tab.svg");
+  eleventyConfig.addPassthroughCopy("site.webmanifest");
 
   // Allow JS to pass through (can add a bundling step in future if needed)
   eleventyConfig.addPassthroughCopy("./src/assets/js");
