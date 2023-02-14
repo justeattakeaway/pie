34d0dd3745a6e90629aba8dc5cf4ce3f9ff0c7c4
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-02-14 12:40:07 +0000

    format 11ty file and add pass through comment

--- apps/pie-microsite/.eleventy.js
+++ apps/pie-microsite/.eleventy.js
@@ -1,48 +1,50 @@
 const {
-  filters,
-  libraries,
-  shortcodes,
-  plugins
+    filters,
+    libraries,
+    shortcodes,
+    plugins
 } = require('./src/_11ty');
 
 module.exports = eleventyConfig => {
-  // Copy over img directory to dist directory.
-  eleventyConfig.addPassthroughCopy({ 'src/assets/img': 'assets/img' });
+    // Copy over img directory to dist directory.
+    eleventyConfig.addPassthroughCopy({ 'src/assets/img': 'assets/img' });
 
-  // Plugins
-  plugins.addAllPlugins(eleventyConfig);
+    // Plugins
+    plugins.addAllPlugins(eleventyConfig);
 
-  // Filters
-  filters.addAllFilters(eleventyConfig);
+    // Filters
+    filters.addAllFilters(eleventyConfig);
 
-  // Libraries to amend the used library instances
-  libraries.amendAllLibraries(eleventyConfig);
+    // Libraries to amend the used library instances
+    libraries.amendAllLibraries(eleventyConfig);
 
-  // Shortcodes
-  shortcodes.addAllShortCodes(eleventyConfig);
+    // Shortcodes
+    shortcodes.addAllShortCodes(eleventyConfig);
 
-  // Pass CNAME file into dist output (needed for domain to work correctly)
-  eleventyConfig.addPassthroughCopy("CNAME");
-  eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
-  eleventyConfig.addPassthroughCopy("android-chrome-256x256.png");
-  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
-  eleventyConfig.addPassthroughCopy("browserconfig.xml");
-  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
-  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("mstile-150x150.png");
-  eleventyConfig.addPassthroughCopy("safari-pinned-tab.svg");
-  eleventyConfig.addPassthroughCopy("site.webmanifest");
+    // Pass CNAME file into dist output (needed for domain to work correctly)
+    eleventyConfig.addPassthroughCopy("CNAME");
 
-  // Allow JS to pass through (can add a bundling step in future if needed)
-  eleventyConfig.addPassthroughCopy("./src/assets/js");
+    // Pass favicon files into dist output
+    eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
+    eleventyConfig.addPassthroughCopy("android-chrome-256x256.png");
+    eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+    eleventyConfig.addPassthroughCopy("browserconfig.xml");
+    eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+    eleventyConfig.addPassthroughCopy("favicon-32x32.png");
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+    eleventyConfig.addPassthroughCopy("mstile-150x150.png");
+    eleventyConfig.addPassthroughCopy("safari-pinned-tab.svg");
+    eleventyConfig.addPassthroughCopy("site.webmanifest");
 
-  return {
-    dir: {
-      input: "src",
-      output: "dist",
-    },
-    markdownTemplateEngine: "njk",
-    pathPrefix: process.env.GITHUB_REF_NAME === 'main' ? '/' : process.env.PIE_URL_PREFIX
-  };
+    // Allow JS to pass through (can add a bundling step in future if needed)
+    eleventyConfig.addPassthroughCopy("./src/assets/js");
+
+    return {
+        dir: {
+        input: "src",
+        output: "dist",
+        },
+        markdownTemplateEngine: "njk",
+        pathPrefix: process.env.GITHUB_REF_NAME === 'main' ? '/' : process.env.PIE_URL_PREFIX
+    };
 };
