88beeb71ebf0f818ccd3621fd4d4b80ef8e94c61
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2023-02-14 17:07:36 +0000

    PR comments

--- packages/components/pie-button/src/index.ts
+++ packages/components/pie-button/src/index.ts
@@ -11,24 +11,14 @@ export { BUTTON_SIZE, BUTTON_TYPE, BUTTON_VARIANT };
 
 @customElement('pie-button')
 export class PieButton extends LitElement {
-    /**
-     * The size of the button. Available sizes are xsmall, small, medium (default) and large.
-     */
     @property()
     @validPropertyValues(Object.values(BUTTON_SIZE), BUTTON_SIZE.MEDIUM)
     size : BUTTON_SIZE = BUTTON_SIZE.MEDIUM;
 
-    /**
-     * The Button type. Maps directly to the `type` attribute on the button element.
-     * Default value is `submit`.
-     */
     @property()
     @validPropertyValues(Object.values(BUTTON_TYPE), BUTTON_TYPE.SUBMIT)
     type : BUTTON_TYPE = BUTTON_TYPE.SUBMIT;
 
-    /**
-     * The Button style variant to use. Valid values are primary (default), secondary, outline and ghost.
-     */
     @property()
     @validPropertyValues(Object.values(BUTTON_VARIANT), BUTTON_VARIANT.PRIMARY)
     variant : BUTTON_VARIANT = BUTTON_VARIANT.PRIMARY;
