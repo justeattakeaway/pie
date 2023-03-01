32b20ee3e0935f474fc6f191421d8bc030d2917c
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2023-03-01 14:22:24 +0100

    feat(pie-eslint-config): setup ESLint package

---
+++ packages/tools/pie-eslint-config/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+v0.1.0
+------------------------------
+*February 7, 2023*
+
+### Added
+- Initial version
---
+++ packages/tools/pie-eslint-config/strict/rules/style.js
@@ -0,0 +1,12 @@
+module.exports = {
+    rules: {
+        // disallow multiple empty lines and only one newline at the end
+        'no-multiple-empty-lines': ['error', { max: 1 }],
+        'operator-linebreak': ['error', 'after', {
+            overrides: {
+                '?': 'before',
+                ':': 'before',
+            },
+        }],
+    },
+};
