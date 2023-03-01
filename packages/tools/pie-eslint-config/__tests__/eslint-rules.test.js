7b47b5a9c42059e1fa219c5b14364ce88ce843c8
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2023-03-01 11:03:06 +0100

    chore(pie-eslint-config): update tests

--- packages/tools/pie-eslint-config/__tests__/eslint-rules.test.js
+++ packages/tools/pie-eslint-config/__tests__/eslint-rules.test.js
@@ -36,10 +36,13 @@ const srcCodeWith2EmptyLines = `function foo () {}
 export default foo;
 `;
 
+const baseConfig = { overrideConfigFile: './base/index.js' };
+const strictConfig = { overrideConfigFile: './strict/index.js' };
+
 describe('ESLint', () => {
     describe('base', () => {
         it('should find the expected errors', async () => {
-            const eslint = new ESLint();
+            const eslint = new ESLint(baseConfig);
             const results = await eslint.lintText(srcCodeWithIssues);
 
             expect(results.length).toBe(1);
@@ -47,7 +50,7 @@ describe('ESLint', () => {
         });
 
         it('shouldnt find any errors', async () => {
-            const eslint = new ESLint();
+            const eslint = new ESLint(baseConfig);
             const results = await eslint.lintText(srcCodeWithoutIssues);
 
             expect(results.length).toBe(1);
@@ -55,8 +58,7 @@ describe('ESLint', () => {
         });
 
         it('console.log usage should raise a warning', async () => {
-            const eslint = new ESLint();
-
+            const eslint = new ESLint(baseConfig);
             const results = await eslint.lintText('console.log(1);\n');
 
             expect(results[0].messages).toEqual(expect.arrayContaining([
@@ -69,7 +71,7 @@ describe('ESLint', () => {
 
         it('no-multiple-empty-lines should not raise an error for 2 empty lines', async () => {
             const src = srcCodeWith2EmptyLines;
-            const eslint = new ESLint();
+            const eslint = new ESLint(baseConfig);
             const results = await eslint.lintText(src);
 
             expect(results[0].messages).not.toEqual(expect.arrayContaining([
@@ -84,7 +86,7 @@ describe('ESLint', () => {
     describe('strict', () => {
         describe('inherits base', () => {
             it('should find the expected errors', async () => {
-                const eslint = new ESLint();
+                const eslint = new ESLint(strictConfig);
                 const results = await eslint.lintText(srcCodeWithIssues);
 
                 expect(results.length).toBe(1);
@@ -92,7 +94,7 @@ describe('ESLint', () => {
             });
 
             it('shouldnt find any errors', async () => {
-                const eslint = new ESLint();
+                const eslint = new ESLint(strictConfig);
                 const results = await eslint.lintText(srcCodeWithoutIssues);
 
                 expect(results.length).toBe(1);
@@ -102,21 +104,20 @@ describe('ESLint', () => {
 
         describe('own rules', () => {
             it('console.log usage should raise an error', async () => {
-                const eslint = new ESLint({ overrideConfigFile: './strict/index.js' });
-
+                const eslint = new ESLint(strictConfig);
                 const results = await eslint.lintText('console.log(1);\n');
 
                 expect(results[0].messages).toEqual(expect.arrayContaining([
                     expect.objectContaining({
                         ruleId: 'no-console',
-                        severity: ESLINT_SEVERITY.ERROR,
+                        severity: ESLINT_SEVERITY.WARN,
                     }),
                 ]));
             });
 
             it('no-multiple-empty-lines should raise an error when more than 1 empty line is found', async () => {
                 const src = srcCodeWith2EmptyLines;
-                const eslint = new ESLint({ overrideConfigFile: './strict/index.js' });
+                const eslint = new ESLint(strictConfig);
                 const results = await eslint.lintText(src);
 
                 expect(results[0].messages).toEqual(expect.arrayContaining([
