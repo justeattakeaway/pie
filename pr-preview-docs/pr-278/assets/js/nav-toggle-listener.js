0f87685387e1004f2d9a699cfcdc5619aae7a33f
siggerzz <siggerzz@users.noreply.github.com>
2023-03-03 18:17:09 +0000

    Deploy preview for PR 278 🛫

---
+++ pr-preview-docs/pr-278/assets/js/nav-toggle-listener.js
@@ -0,0 +1,14 @@
+const navEl = document.querySelector('#navToggle');
+const headerEl = document.querySelector('[data-test-id="site_header"]');
+const NAV_OPEN_ATTRIBUTE = 'data-nav-open';
+const NAV_OPEN_ATTRIBUTE_VALUE = 'true';
+
+window.addEventListener('DOMContentLoaded', () => {
+    navEl.addEventListener('click', () => {
+        if (headerEl.getAttribute(NAV_OPEN_ATTRIBUTE) === NAV_OPEN_ATTRIBUTE_VALUE) {
+            headerEl.removeAttribute(NAV_OPEN_ATTRIBUTE);
+        } else {
+            headerEl.setAttribute(NAV_OPEN_ATTRIBUTE, NAV_OPEN_ATTRIBUTE_VALUE);
+        }
+    });
+});
