777a2be741acf9e5a4ee122d0e220f7c644678dc
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-03-03 18:26:54 +0000

    format(pie-docs): uncomment test

--- apps/pie-docs/test/system/navigation.spec.js
+++ apps/pie-docs/test/system/navigation.spec.js
@@ -1,16 +1,16 @@
 import expectedRoutesJson from '../snapshots/expected-routes.snapshot.json';
 
 describe('PIE - Status Code Tests', async () => {
-    // expectedRoutesJson.forEach((route) => {
-    //     it(`Should respond with a '200' status code for route - ${route}`, async () => {
-    //         const puppeteer = await browser.getPuppeteer();
-    //         const [page] = await puppeteer.pages();
-    //         const url = `${browser.options.baseUrl}/${route}`;
+    expectedRoutesJson.forEach((route) => {
+        it(`Should respond with a '200' status code for route - ${route}`, async () => {
+            const puppeteer = await browser.getPuppeteer();
+            const [page] = await puppeteer.pages();
+            const url = `${browser.options.baseUrl}/${route}`;
 
-    //         const response = await page.goto(url);
-    //         await expect(response.status()).toBe(200);
-    //     });
-    // });
+            const response = await page.goto(url);
+            await expect(response.status()).toBe(200);
+        });
+    });
 
     it('Should open and close the mobile navigation menu', async () => {
         await browser.emulateDevice('iPhone X');
