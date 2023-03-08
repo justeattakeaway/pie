2fd0e60b6851259bc2927cd26d3899b2bb4b1a21
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2023-03-08 16:42:57 +0000

    test(pie-docs): add missing a11y and system tests for docs site 404 page

---
+++ apps/pie-docs/test/system/404.spec.js
@@ -0,0 +1,19 @@
+describe('PIE - 404 Page', async () => {
+    it('Should go to the homepage when clicking "Visit homepage" link', async () => {
+        // Arrange
+        const url = `${browser.options.baseUrl}/404.html`;
+        const puppeteer = await browser.getPuppeteer();
+        const [page] = await puppeteer.pages();
+        await page.goto(url);
+        const visitHomepageLinkSelector = '[data-test-id="404-visit-homepage"]';
+
+        // Act
+        const [response] = await Promise.all([
+            page.waitForNavigation(),
+            page.click(visitHomepageLinkSelector)
+        ]);
+
+        // Assert
+        await expect(response.url()).toEqual(browser.options.baseUrl);
+    });
+});
