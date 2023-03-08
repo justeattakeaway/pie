9f5be869903c98a9669785e5bb294b2943072026
FayeCarter <faye.carter@justeattakeaway.com>
2023-03-08 11:04:09 +0000

    feat(pie-docs): update card component to handle internal links

--- apps/pie-docs/src/_11ty/shortcodes/card.js
+++ apps/pie-docs/src/_11ty/shortcodes/card.js
@@ -1,4 +1,5 @@
 const pieIconsSvg = require('../filters/pieIconsSvg');
+const pieDesignTokenColours = require('../filters/pieDesignTokenColours');
 
 /**
  * A Card HTML component – takes an array of list items and turns them into a list of cards
@@ -6,40 +7,93 @@ const pieIconsSvg = require('../filters/pieIconsSvg');
  * @returns {string}
  */
 // eslint-disable-next-line func-names
-module.exports = function ({ items }) {
-    const iconLink = pieIconsSvg({
-        name: 'link-external',
-        attrs: {
-            'aria-hidden': 'true',
-            height: 21,
-            width: 21,
-        },
-    });
+module.exports = function ({ items, shouldFillContainer = false }) {
+    const buildLinkIcon = (isInternalLink) => {
+        const internalLinkIcon = pieIconsSvg({
+            name: 'arrow-right',
+            attrs: {
+                'aria-hidden': 'true',
+                height: 16,
+                width: 16,
+            },
+        });
+
+        const externalLinkIcon = pieIconsSvg({
+            name: 'link-external',
+            attrs: {
+                'aria-hidden': 'true',
+                height: 21,
+                width: 21,
+            },
+        });
+
+        return isInternalLink ? internalLinkIcon : externalLinkIcon;
+    }
+
+    const buildCardContent = ({ icon, iconColour, heading, content }) => {
+        const iconStyles = iconColour ? `style="--icon-container-colour: ${pieDesignTokenColours({ tokenName: iconColour, tokenPath: ['alias', 'default'] })};"` : '';
+
+        const cardIcon = icon && pieIconsSvg({
+            name: icon,
+            attrs: {
+                'aria-hidden': 'true',
+                height: 32,
+                width: 32
+            },
+        });
+
+        return `<div class="c-card-container">
+                    ${cardIcon ? `<div class="c-card-icon" ${iconStyles}>${cardIcon}</div>` : ''}
+                    ${heading ? `<h2 class="c-card-heading">${heading}</h2>` : ''}
+                    ${content ? `<p class="c-card-content">${content}</p>` : ''}
+                </div>`
+
+    };
 
     const buildCard = ({
-        href, linkText, src, shouldOpenInNewTab = false,
+        content,
+        heading,
+        href,
+        icon,
+        iconColour,
+        isInternalLink,
+        linkText,
+        shouldOpenInNewTab = false,
+        src,
     }) => {
         const target = shouldOpenInNewTab ? 'target="_blank"' : '';
 
         const labelClasses = [
             'c-card-labelContainer',
-            src && 'c-card-labelContainer--hasImage'
+            src && 'c-card-labelContainer--hasImage',
+            isInternalLink && 'c-card-labelContainer--internalLink'
         ].filter(Boolean).join(' ');
 
+        const cardHasContent = (!!icon && !!iconColour) || !!heading || !!content;
+
         return `<article class="c-card">
-                    ${src ? `<img class="c-card-image" src="${src}" role="presentation" alt="">` : ''}
+                    ${ cardHasContent
+                        ? `${ buildCardContent({icon, iconColour, heading, content }) }`
+                        : ''
+                    }
+                    ${ src ? `<img class="c-card-image" src="${src}" role="presentation" alt="">` : '' }
                     <div class="${labelClasses}">
                         <p class="c-card-label">
                             <a href="${href}" ${target}>${linkText}</a>
                         </p>
-                        ${iconLink}
+                        ${href ? `${buildLinkIcon(isInternalLink)}` : ''}
                     </div>
                 </article>`;
     };
 
     if (items.length > 1) {
+        const listClasses = [
+            'c-card-list',
+            shouldFillContainer && 'c-card-list--fillContainer'
+        ].filter(Boolean).join(' ');
+
         return `<div class="c-card-wrapper">
-                    <ul class="c-card-list">
+                    <ul class="${listClasses}">
                         ${Object.values(items).map((card) => `<li>${buildCard(card)}</li>`).join('')}
                     </ul>
                 </div>`;
--- apps/pie-docs/src/index.md
+++ apps/pie-docs/src/index.md
@@ -7,6 +7,42 @@ heroCtaTitle: More about PIE
 heroCtaHref: /all-about-pie/what-is-pie
 ---
 
+## Start exploring
+
+{% card {
+  shouldFillContainer: true,
+  items: [
+        {
+          heading: "For designers",
+          content: "Find out how to use PIE in your products and the multiple ways how we can support you or your team.",
+          linkText: "Get started",
+          icon: "bulb-lightning",
+          iconColour: "support-brand-03",
+          href: "/designers/getting-started/overview/",
+          isInternalLink: "true"
+        },
+        {
+          heading: "For engineers",
+          content: "Learn how to link the PIE repository and start using our foundations and components in your projects.",
+          linkText: "Get started",
+          icon: "engineers",
+          iconColour: "support-brand-04",
+          href: "/engineers/getting-started/overview/",
+          isInternalLink: "true"
+        },
+        {
+          heading: "Our libraries",
+          content: "Find out how to enable PIE foundations and components libraries in Figma and apply them to your designs.",
+          linkText: "Get the libraries",
+          icon: "components",
+          iconColour: "support-brand-05",
+          href: "/foundations/colour/overview/",
+          isInternalLink: "true"
+        }
+    ]
+} %}
+
+---
 
 ## Get support
 
