23621d1c94e11800b903fb8eb692789365bf5e88
Joshua Ng <joshua.ng@just-eat.com>
2023-03-09 15:03:35 +0000

    ci(pie-docs): updated cookies domain and path

--- apps/pie-docs/test/helpers/playwright-helper.js
+++ apps/pie-docs/test/helpers/playwright-helper.js
@@ -1,15 +1,21 @@
 const COOKIE_NAMES = require('../../../../constants/cookies');
 
 exports.disableCookieBanner = async (page, context) => {
+    const url = new URL(process.env.BASE_URL);
+    const hostName = url.hostname;
+    const pathName = url.pathname;
+
     await context.addCookies([
         {
             name: COOKIE_NAMES.JE_COOKIE_CONSENT,
             value: 'full',
-            url: process.env.BASE_URL,
+            domain: hostName,
+            path: pathName,
         }, {
             name: COOKIE_NAMES.JE_BANNER_COOKIE,
             value: '130315',
-            url: process.env.BASE_URL,
+            domain: hostName,
+            path: pathName,
         }
     ]);
     await page.reload();
