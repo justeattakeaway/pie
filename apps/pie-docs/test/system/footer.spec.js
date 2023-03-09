2381cd123f699e334359b97e582a5613211cac90
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-03-09 11:00:09 +0000

    test(pie-docs): new test for footer privacy policy

---
+++ apps/pie-docs/test/system/footer.spec.js
@@ -0,0 +1,21 @@
+describe('PIE - Footer', async () => {
+    it('Should go to the Privacy Policy when clicking the associated link', async () => {
+        // Arrange
+        const url = browser.options.baseUrl;
+        const privacyPolicyUrl = 'https://www.just-eat.co.uk/info/privacy-policy';
+        const puppeteer = await browser.getPuppeteer();
+        const [page] = await puppeteer.pages();
+        await page.goto(url);
+        const visitHomepageLinkSelector = '[data-test-id="privacy-policy"]';
+
+        // Act
+        const [response] = await Promise.all([
+            page.waitForNavigation(),
+            page.click(visitHomepageLinkSelector)
+        ]);
+
+        // Assert
+        await expect(response.url()).toEqual(privacyPolicyUrl);
+        await expect(response.status()).toBe(200);
+    });
+});
