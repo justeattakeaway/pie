56df0c17d87b1425609fc00d853324f2206d8c00
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-03-10 11:00:12 +0000

    format(pie-docs): fix linting

--- apps/pie-docs/test/system/cookie-banner.spec.js
+++ apps/pie-docs/test/system/cookie-banner.spec.js
@@ -5,23 +5,22 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('PIE - Cookie Banner Tests - @desktop', async () => {
-
     const cookieTypes = [
         'accept-all-cookies-button',
         'accept-necessary-cookies-button'
     ];
 
-    cookieTypes.forEach(cookieType => {
+    cookieTypes.forEach((cookieType) => {
         test(`Should close the cookie banner when the ${cookieType} button is clicked`, async ({ page }) => {
             // Arrange
             const cookieBannerComponent = page.getByTestId('cookie-banner-component');
             const cookieAcceptSelector = page.getByTestId(cookieType);
-    
+
             // Ensure Cookie banner exists before clicking
             await expect.soft(cookieBannerComponent).toBeVisible();
-    
+
             await cookieAcceptSelector.click();
-    
+
             // Assert
             await expect(cookieBannerComponent).not.toBeVisible();
         });
