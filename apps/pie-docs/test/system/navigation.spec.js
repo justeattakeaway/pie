d3676cba798ce0921efce5ce44963791dc2bab85
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-03-10 16:33:35 +0000

    chore(pie-docs): resolve conflict

--- apps/pie-docs/test/system/navigation.spec.js
+++ apps/pie-docs/test/system/navigation.spec.js
@@ -24,20 +24,6 @@ test.describe('PIE - Status Code Tests - @desktop', () => {
 });
 
 test.describe('PIE - site nav menu - @mobile', () => {
-<<<<<<< HEAD
-    test('Should open and close the mobile navigation menu', async ({ page }) => {
-        // Arrange
-        const navToggleLabel = page.getByTestId('nav_toggle_label');
-        const navMenu = page.getByTestId('site_nav');
-
-        // Act - Open nav menu
-        await navToggleLabel.click();
-
-        // Act - Close nav menu
-        await navToggleLabel.click();
-
-        // Assert
-=======
     test.beforeEach(async ({ page, context }) => {
         await disableCookieBanner(page, context);
     });
@@ -57,7 +43,6 @@ test.describe('PIE - site nav menu - @mobile', () => {
         await navToggleLabel.click();
 
         // Assert - Nav menu is closed
->>>>>>> 00336f3f4bac220f3829ed5e0df90ba0dea64598
         await expect(navMenu).not.toBeVisible();
     });
 });
