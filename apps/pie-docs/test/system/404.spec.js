a4d228441185c93edac2435cc048308ea4409133
Ben Siggery <bensiggery28@gmail.com>
2023-03-10 21:58:42 +0000

    fix(pie-docs): fix test in CI

--- apps/pie-docs/test/system/404.spec.js
+++ apps/pie-docs/test/system/404.spec.js
@@ -1,25 +1,22 @@
 import { test, expect } from '@playwright/test';
 import { disableCookieBanner } from '../helpers/playwright-helper';
 
-test.beforeEach(async ({ page }) => {
-    await page.goto(process.env.BASE_URL);
-});
-
 test.describe('PIE - 404 Page - @desktop', () => {
     test.beforeEach(async ({ page, context }) => {
+        await page.goto(process.env.BASE_URL);
         await disableCookieBanner(page, context, false);
     });
 
     test('Should go to the homepage when clicking "Visit homepage" link', async ({ page }) => {
         // Arrange
-        const url = `${await page.url()}/404.html`;
+        const url = `${page.url()}/404.html`;
         await page.goto(url);
 
         const visitHomepageLink = page.getByTestId('404-visit-homepage');
 
         // Act
         await Promise.all([
-            page.waitForURL(process.env.BASE_URL),
+            page.waitForResponse((resp) => resp.url() === process.env.BASE_URL),
             visitHomepageLink.click()
         ]);
 
