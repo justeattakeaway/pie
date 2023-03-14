3c1ff7f15744ef560055f4d00065ac339b0c9b75
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2023-03-14 13:39:12 +0100

    feat(wc-vue3) - add simple counter for testing button click events

--- apps/examples/wc-vue3/src/App.vue
+++ apps/examples/wc-vue3/src/App.vue
@@ -1,7 +1,34 @@
 <template>
-    <pie-button>this is a pie button</pie-button>
+    <div class="container">
+        <pie-button @click="decrement" />
+        <div>{{ count }}</div>
+        <pie-button @click="increment" />
+    </div>
 </template>
 
 <script>
 import '@justeattakeaway/pie-button';
+
+export default {
+    data () {
+        return {
+            count: 0,
+        };
+    },
+    methods: {
+        decrement () {
+            this.count--;
+        },
+        increment () {
+            this.count++;
+        },
+    },
+};
 </script>
+
+<style>
+.container {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
