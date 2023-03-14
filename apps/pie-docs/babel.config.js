ff48c54fe797f252d12980794a6f747423bc8484
Joshua Ng <joshua.ng@just-eat.com>
2023-03-14 14:47:13 +0000

    fix(pie-docs): re-added babel config

---
+++ apps/pie-docs/babel.config.js
@@ -0,0 +1,10 @@
+// Babel config to allow import/export syntax in WDIO tests.
+module.exports = {
+    presets: [
+        ['@babel/preset-env', {
+            targets: {
+                node: 'current',
+            },
+        }]
+    ],
+};
