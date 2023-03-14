617caac87fc6953ff716e7ab71e1d02b4c52b4c9
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2023-03-14 11:38:09 +0100

    feat(wc-vue3) - update vite config enabling web components support

--- apps/examples/wc-vue3/vite.config.ts
+++ apps/examples/wc-vue3/vite.config.ts
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    // treat all tags with a dash as custom elements
+                    isCustomElement: (tag) => tag.includes('-'),
+                },
+            },
+        }),
+    ],
 });
