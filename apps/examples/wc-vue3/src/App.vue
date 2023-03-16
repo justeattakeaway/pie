5126ec9a2142049d8dd3d9be7e1c49631209885f
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2023-03-16 16:08:56 +0100

    feat(wc-vue3) - display different button variants and sizes

--- apps/examples/wc-vue3/src/App.vue
+++ apps/examples/wc-vue3/src/App.vue
@@ -1,21 +1,35 @@
 <template>
-    <div class="container">
-        <pie-button variant="ghost" />
-    </div>
+    <h2>Button click event</h2>
     <div class="container">
         <pie-button @click="decrement" />
         <div>{{ count }}</div>
         <pie-button @click="increment" />
     </div>
+    <h2>Button variants</h2>
+    <div class="container">
+        <pie-button
+            v-for="(value, key) in BUTTON_VARIANT"
+            :key="key"
+            :variant="value" />
+    </div>
+    <h2>Button sizes</h2>
+    <div class="container">
+        <pie-button
+            v-for="(value, key) in BUTTON_SIZE"
+            :key="key"
+            :size="value" />
+    </div>
 </template>
 
 <script>
-import '@justeattakeaway/pie-button';
+import { BUTTON_VARIANT, BUTTON_SIZE } from '@justeattakeaway/pie-button';
 
 export default {
     data () {
         return {
             count: 0,
+            BUTTON_VARIANT,
+            BUTTON_SIZE,
         };
     },
     methods: {
@@ -33,5 +47,6 @@ export default {
 .container {
     display: flex;
     justify-content: space-around;
+    padding: 1rem;
 }
 </style>
