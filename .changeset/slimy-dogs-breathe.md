87ee1fb599461ce19fe44b1e85a47812b32fc593
Joshua Ng <joshua.ng@just-eat.com>
2023-03-21 10:59:46 +0000

    ci(pie-monorepo): added turborepo s3 support
    ci(pie-monorepo): remove local turborepo cache from CI in favour of remote caching

---
+++ .changeset/slimy-dogs-breathe.md
@@ -0,0 +1,5 @@
+---
+"pie-monorepo": minor
+---
+
+[Removed] - WDIO references and dependencies
--- apps/pie-docs/test/playwright/fixtures.js
+++ apps/pie-docs/test/playwright/fixtures.js
@@ -6,6 +6,7 @@ const AxeBuilder = require('@axe-core/playwright').default;
 // This new "test" can be used in multiple test files, and each of them will get
 // a consistently configured AxeBuilder instance.
 exports.test = base.test.extend({
+
     makeAxeBuilder: [async ({ page }, use) => {
         const makeAxeBuilder = () => new AxeBuilder({ page })
           .withTags(['wcag21a', 'wcag21aa', 'wcag143', 'cat.color', 'cat.aria'])
