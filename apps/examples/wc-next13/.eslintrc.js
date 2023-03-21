e1a489fd458544971251b20e3d58569b08daa260
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2023-03-21 09:10:37 +0100

    chore(wc-next13): add ESLint settings

---
+++ apps/examples/wc-next13/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: [
+        require.resolve('@justeattakeaway/pie-eslint-config/strict'),
+        'eslint:recommended',
+        'next',
+    ],
+    // parserOptions: {
+    //     ecmaFeatures: {
+    //         jsx: true,
+    //     },
+    // },
+};
