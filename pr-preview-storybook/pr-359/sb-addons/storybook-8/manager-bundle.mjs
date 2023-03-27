16ec0ccf8e9ce76d02cc9dd900b99968c7d07df9
siggerzz <siggerzz@users.noreply.github.com>
2023-03-27 10:03:06 +0000

    Remove preview for PR 359 🛬

--- pr-preview-storybook/pr-359/sb-addons/storybook-8/manager-bundle.mjs
+++
@@ -1,4 +0,0 @@
-try{
-var s=__STORYBOOKADDONS__,{addons:o,types:_,mockChannel:n}=__STORYBOOKADDONS__;var i=__STORYBOOKTHEMING__,{CacheProvider:l,ClassNames:d,Global:p,ThemeProvider:T,background:g,color:S,convert:f,create:u,createCache:x,createGlobal:y,createReset:C,css:N,darken:R,ensure:k,ignoreSsrWarning:B,isPropValid:D,jsx:G,keyframes:K,lighten:Y,styled:b,themes:a,typography:v,useTheme:P,withTheme:A}=__STORYBOOKTHEMING__;o.setConfig({theme:a.light});
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
-//# sourceMappingURL=manager-bundle.mjs.map
