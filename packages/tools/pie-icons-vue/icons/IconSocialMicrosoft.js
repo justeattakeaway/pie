b47d353bdee121bf4f29e3f020992ec35dd97178
Ashley Watson-Nolan <nolly00@gmail.com>
2023-03-29 13:50:37 +0100

    fix(pie-icons-vue): clip-path id prefixes now generated correctly

--- packages/tools/pie-icons-vue/icons/IconSocialMicrosoft.js
+++ packages/tools/pie-icons-vue/icons/IconSocialMicrosoft.js
@@ -15,7 +15,7 @@ export default {
             class: 'c-pieIcon c-pieIcon--microsoft',
         }, ctx.data]), [h('g', {
             attrs: {
-                'clip-path': 'url(#prefix__a)',
+                'clip-path': 'url(#prefix__clip0_2820_3494)',
             },
         }, [h('path', {
             attrs: {
@@ -39,7 +39,7 @@ export default {
             },
         })]), h('defs', [h('clipPath', {
             attrs: {
-                id: 'prefix__a',
+                id: 'prefix__clip0_2820_3494',
             },
         }, [h('rect', {
             attrs: {
--- packages/tools/pie-icons-vue/icons/IconWallet.js
+++ packages/tools/pie-icons-vue/icons/IconWallet.js
@@ -15,7 +15,7 @@ export default {
             class: 'c-pieIcon c-pieIcon--wallet',
         }, ctx.data]), [h('g', {
             attrs: {
-                'clip-path': 'url(#prefix__a)',
+                'clip-path': 'url(#prefix__clip0_4814_3596)',
             },
         }, [h('path', {
             attrs: {
@@ -26,7 +26,7 @@ export default {
             },
         })]), h('defs', [h('clipPath', {
             attrs: {
-                id: 'prefix__a',
+                id: 'prefix__clip0_4814_3596',
             },
         }, [h('rect', {
             attrs: {
