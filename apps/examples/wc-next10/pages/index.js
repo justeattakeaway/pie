daa01610d133790c8d2bfdaae678500300033503
Lizzie Turney <49618712+LTurns@users.noreply.github.com>
2023-04-04 09:33:48 +0100

    feat(wc-next10): add next10 example app (#370)
    * feat(wc-next10): added empty next10 app
    
    * feat(wc-next10): updated yarn.lock
    
    * feat(wc-next10): updated readme.md
    
    * feat(wc-next10): added changeset entries
    
    * feat(wc-next10): pinned versions in package.json
    
    * feat(wc-next10): updated build script
    
    ---------
    
    Co-authored-by: Lizzie Turney <lizzie.turney@just-eat.com>
    Co-authored-by: Xander Marjoram <xander.marjoram@justeattakeaway.com>

---
+++ apps/examples/wc-next10/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+      </Head>
+      <main>
+        Hello World!
+      </main>
+    </>
+  )
+}
