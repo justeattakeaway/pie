11b48e86cc5fd77f3559fc22cdb4ea502b8b9ea4
FayeCarter <faye.carter@justeattakeaway.com>
2023-04-18 16:10:28 +0100

    format(pie-monorepo): lint

--- apps/examples/wc-angular12/src/app/app.component.ts
+++ apps/examples/wc-angular12/src/app/app.component.ts
@@ -2,33 +2,33 @@ import { Component } from '@angular/core';
 import { BUTTON_SIZE, BUTTON_VARIANT } from '@justeattakeaway/pie-button';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
-  title = 'wc-angular12';
+    title = 'wc-angular12';
 
-  buttonSizes = Object.values(BUTTON_SIZE).map((size) => size);
-  count = 0;
-  variantIndex = 0;
-  variantName = BUTTON_VARIANT.PRIMARY;
+    buttonSizes = Object.values(BUTTON_SIZE).map((size) => size);
+    count = 0;
+    variantIndex = 0;
+    variantName = BUTTON_VARIANT.PRIMARY;
 
-  increment () {
-    this.count ++;
-  }
+    increment () {
+        this.count ++;
+    }
 
-  decrement () {
-    this.count --;
-  }
+    decrement () {
+        this.count --;
+    }
 
-  switchVariant () {
-    this.variantIndex += 1;
-    const variantsKeys: string[] = Object.keys(BUTTON_VARIANT);
-    const variantKey: string = variantsKeys[this.variantIndex % variantsKeys.length];
-    const variant: BUTTON_VARIANT = BUTTON_VARIANT[variantKey as keyof typeof BUTTON_VARIANT];
+    switchVariant () {
+        this.variantIndex += 1;
+        const variantsKeys: string[] = Object.keys(BUTTON_VARIANT);
+        const variantKey: string = variantsKeys[this.variantIndex % variantsKeys.length];
+        const variant: BUTTON_VARIANT = BUTTON_VARIANT[variantKey as keyof typeof BUTTON_VARIANT];
 
-    this.variantName = variant;
-  }
+        this.variantName = variant;
+    }
 }
