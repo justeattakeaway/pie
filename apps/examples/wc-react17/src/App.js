2c4e063abe98edc611f77462f17ee0a4a5b0c79a
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2023-04-20 14:19:09 +0100

    format(pie-monorepo): remove eslint react extend and resolve eslint errors

--- apps/examples/wc-react17/src/App.js
+++ apps/examples/wc-react17/src/App.js
@@ -1,9 +1,9 @@
-function App() {
-  return (
-    <div className="App">
-        <button>Pie button to go here!</button>
-    </div>
-  );
+function App () {
+    return (
+        <div className="App">
+            <button>Pie button to go here!</button>
+        </div>
+    );
 }
 
 export default App;
