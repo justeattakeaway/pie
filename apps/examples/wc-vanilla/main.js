79f8fe87b6b0cdb90942a924bbf34b2cc1662647
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-04-21 17:17:31 +0100

    format(pie-monorepo): tidy up vanilla example app

--- apps/examples/wc-vanilla/main.js
+++ apps/examples/wc-vanilla/main.js
@@ -1,23 +1,12 @@
 import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.js';
+import { setupCounter } from './counter';
 
 document.querySelector('#app').innerHTML = `
     <div>
-        <a href="https://vitejs.dev" target="_blank">
-        <img src="${viteLogo}" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-        <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-        </a>
-        <h1>Hello Vite!</h1>
+        <h1>Hello World!</h1>
         <div class="card">
-        <pie-button id="counter" type="button"></pie-button>
+            <pie-button id="counter" type="button"></pie-button>
         </div>
-        <p class="read-the-docs">
-        Click on the Vite logo to learn more
-        </p>
 </div>
 `;
 
