cd918ba9f982fa09bbeca5546fcc9e96297e59b7
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-05-10 09:04:04 +0100

    format(pie-button): format some ts test files

--- packages/components/pie-button/test/component/pie-button.spec.ts
+++ packages/components/pie-button/test/component/pie-button.spec.ts
@@ -6,46 +6,49 @@ const sizes = Object.values(BUTTON_SIZE);
 const variants = Object.values(BUTTON_VARIANT);
 const disabledStates = [true, false];
 
-variants.forEach(variant => {
-  test(`should render - ${variant}`, async ({ mount }) => {
+variants.forEach((variant) => {
+    test(`should render - ${variant}`, async ({ mount }) => {
+        for (const size of sizes) {
+            for (const isDisabled of disabledStates) {
+                const component = await mount(
+                    PieButton,
+                    {
+                        props: {
+                            size,
+                            variant,
+                            disabled: isDisabled,
+                        },
+                        slots: {
+                            default: `Hello, ${size} ${variant} Button!`,
+                        },
+                    },
+                );
 
-    for (const size of sizes) {
-      for (const isDisabled of disabledStates) {
-        const component = await mount(PieButton,
-          {
-            props: {
-              size,
-              variant,
-              disabled: isDisabled
-            },
-            slots: {
-              default: `Hello, ${size} ${variant} Button!`
-            },
-          });
-
-        await expect(component).toContainText(`Hello, ${size} ${variant} Button!`);
-      }
-    }
-  });
-})
+                await expect(component).toContainText(`Hello, ${size} ${variant} Button!`);
+            }
+        }
+    });
+});
 
 test('should emit an event when clicked', async ({ mount }) => {
-  const messages: string[] = [];
-  const component = await mount(PieButton,
-    {
-      props: {
-        size: BUTTON_SIZE.LARGE,
-        variant: BUTTON_VARIANT.PRIMARY
-      },
-      slots: {
-        default: 'Click me!'
-      },
-      on: {
-        CustomEvent: (data: string) => messages.push(data),
-      },
-    });
+    const messages: string[] = [];
+    const component = await mount(
+        PieButton,
+        {
+            props: {
+                size: BUTTON_SIZE.LARGE,
+                variant: BUTTON_VARIANT.PRIMARY,
+            },
+            slots: {
+                default: 'Click me!',
+            },
+            on: {
+                CustomEvent: (data: string) => messages.push(data),
+            },
+        },
+    );
 
-  await component.click();
+    await component.click();
 
-  expect(messages).toEqual(['WC event dispatched'])
+    expect(messages).toEqual(['WC event dispatched']);
 });
