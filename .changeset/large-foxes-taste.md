3d4e3d28374ad62aed30328a30e165e76d4b7103
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2023-05-11 14:57:21 +0100

    feat(pie-docs): [Removed] - iconList shortcode now that it has been replaced by categorisedIconList

---
+++ .changeset/large-foxes-taste.md
@@ -0,0 +1,5 @@
+---
+"pie-docs": minor
+---
+
+[Removed] - iconList shortcode now that it has been replaced by categorisedIconList
--- apps/pie-docs/src/_11ty/shortcodes/index.js
+++ apps/pie-docs/src/_11ty/shortcodes/index.js
@@ -1,12 +1,11 @@
 const card = require('./card');
 const categorisedIconList = require('./categorisedIconList');
 const contentPageImage = require('./contentPageImage');
-const iconList = require('./iconList');
 const list = require('./list');
 const mediaElement = require('./mediaElementList');
 const notification = require('./notification');
-const tokensTable = require('./tokensTable');
 const simpleTable = require('./simpleTable');
+const tokensTable = require('./tokensTable');
 
 const { deindentHTML } = require('./shortcode-utilities');
 
@@ -18,12 +17,11 @@ const addAllShortCodes = (eleventyConfig) => {
     eleventyConfig.addShortcode('card', (shortcodeArgs) => deindentHTML(card(shortcodeArgs)));
     eleventyConfig.addShortcode('categorisedIconList', (shortcodeArgs) => deindentHTML(categorisedIconList(shortcodeArgs)));
     eleventyConfig.addShortcode('contentPageImage', (shortcodeArgs) => deindentHTML(contentPageImage(shortcodeArgs)));
-    eleventyConfig.addShortcode('iconList', (shortcodeArgs) => deindentHTML(iconList(shortcodeArgs)));
     eleventyConfig.addShortcode('list', (shortcodeArgs) => deindentHTML(list(shortcodeArgs)));
     eleventyConfig.addShortcode('mediaElementList', (shortcodeArgs) => deindentHTML(mediaElement(shortcodeArgs)));
     eleventyConfig.addShortcode('notification', (shortcodeArgs) => deindentHTML(notification(shortcodeArgs)));
-    eleventyConfig.addShortcode('tokensTable', (shortcodeArgs) => deindentHTML(tokensTable(shortcodeArgs)));
     eleventyConfig.addShortcode('simpleTable', (shortcodeArgs) => deindentHTML(simpleTable(shortcodeArgs)));
+    eleventyConfig.addShortcode('tokensTable', (shortcodeArgs) => deindentHTML(tokensTable(shortcodeArgs)));
 };
 
 module.exports = {
