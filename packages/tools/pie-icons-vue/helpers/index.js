b613e150e42ac78c51e0a6c822c5cae3dae2593d
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2023-05-18 15:09:44 +0100

    format(pie-icons-vue): [Fixed] - Linting errors

--- packages/tools/pie-icons-vue/helpers/index.js
+++ packages/tools/pie-icons-vue/helpers/index.js
@@ -1,5 +1,5 @@
 const removeHyphenBeforeDigits = (name) => name.replace(/\-(\d+)/, '$1'); // eslint-disable-line no-useless-escape
 
 module.exports = {
-    removeHyphenBeforeDigits
+    removeHyphenBeforeDigits,
 };
