61d0edbbb51ba830bb8200a4d332f5ee11adec69
Lizzie Turney <lizzie.turney@just-eat.com>
2023-05-22 13:02:49 +0100

    fix(pie-wrapper-react): resolved linting issues

--- packages/tools/pie-wrapper-react/scripts/react-gen-wrapper.js
+++ packages/tools/pie-wrapper-react/scripts/react-gen-wrapper.js
@@ -1,75 +1,75 @@
 import { createWriteStream } from 'fs';
+
 let componentSrc;
 
-export default function genReactWrapper(customElementsObject){    
-    let components = []
-    const customElements = Object.entries(customElementsObject)
+export default function genReactWrapper (customElementsObject) {
+    const components = [];
+    const customElements = Object.entries(customElementsObject);
 
     // sort through customElements array and put all components into a separate array
     customElements.forEach(([key, value]) => {
-        if ( key === 'modules') {
-            return value.forEach(k => {
+        let componentObject;
+        if (key === 'modules') {
+            value.forEach((k) => {
                 k.declarations.forEach((decl) => {
-                    decl.customElement === true ? components.push({class: decl, path: k.path.replace('index.js', 'react.ts')}) : ''
-                })
-            })
+                    componentObject = decl.customElement === true ? components.push({ class: decl, path: k.path.replace('index.js', 'react.ts') }) : '';
+                });
+            });
         }
-    })
 
-        /**
+        return componentObject;
+    });
+
+    /**
      * function to fetch events from the component
      *
      * @param {*} component object from within components array
      * @param {*} event empty array to be populated with events
      */
-    function getEvents(component) {
-        const events = []
+    function getEvents (component) {
+        const events = [];
         if (component?.events) {
-            events.push(
-                component.events
+            events.push(component.events
                     .filter((event) => !!event.name)
-                    .map((event) => {
-                        return {
-                            name: event.name,
-                            type: event.type?.text || 'Event',
-                            description: event.description,
-                        };
-                    })
-            );
+                    .map((event) => ({
+                        name: event.name,
+                        type: event.type?.text || 'Event',
+                        description: event.description,
+                    })));
         }
 
         return events;
     }
 
     // create wrapper src code and add to index.ts file
-    components.forEach(component => {
-        const events = getEvents(component.class)
+    components.forEach((component) => {
+        const events = getEvents(component.class);
 
-        componentSrc = `// @ts-nocheck 
-    import * as React from 'react';
-    import { ${component.class.name} as ${component.class.name + 'React'}} from './index';
-    import { createComponent } from '@lit-labs/react'; ${
-    component.class.events?.length > 0 ? 
-    "\n        import { EventName } from '@lit-labs/react';" : 
-    ''
-    }
+        componentSrc = `
+// eslint-disable
+import * as React from 'react';
+import { ${component.class.name} as ${`${component.class.name}React`}} from './index';
+import { createComponent } from '@lit-labs/react'; ${
+component.class.events?.length > 0
+    ? "\n        import { EventName } from '@lit-labs/react';"
+    : ''
+}
         
-        export const ${component.class.name} = createComponent({
-            displayName: '${component.class.name}',
-            elementClass: ${component.class.name + 'React'},
-            react: React,
-            tagName: '${component.class.tagName}',
-            events: {
-                ${events?.flat().reduce(
-                    (pre, event) =>
-                        pre +
-                        `${'on' + event.name}: '${event.name}' as EventName<${event.type}>, ${
-                            event.description ? `// ${event.description}` : ''
-                        }`,
-                    ''
-                )}
-            }
-        });`
+export const ${component.class.name} = createComponent({
+    displayName: '${component.class.name}',
+    elementClass: ${`${component.class.name}React`},
+    react: React,
+    tagName: '${component.class.tagName}',
+    events: {
+        ${events?.flat().reduce(
+    (pre, event) => `${pre
+                }${`on${event.name}`}: '${event.name}' as EventName<${event.type}>, ${
+                    event.description ? `// ${event.description}` : ''
+                }`,
+    '',
+)}
+    }
+});`;
 
         const reactFile = createWriteStream(
             component.path,
@@ -79,9 +79,9 @@ export default function genReactWrapper(customElementsObject){
         );
 
         if (componentSrc.length > 0) {
-            reactFile.write(componentSrc)
+            reactFile.write(componentSrc);
         }
-    })
+    });
 
     return componentSrc;
 }
--- packages/tools/pie-wrapper-react/scripts/react-remove-wrapper.js
+++ packages/tools/pie-wrapper-react/scripts/react-remove-wrapper.js
@@ -2,29 +2,32 @@ import { unlink, existsSync } from 'fs';
 
 // removes react wrapper from index.ts after dist has built
 export default function removeReactWrapper (customElementsObject) {
-    let components = []
-    const customElements = Object.entries(customElementsObject)
+    const components = [];
+    const customElements = Object.entries(customElementsObject);
 
     // sort through customElements array and put all components into a separate array
     customElements.forEach(([key, value]) => {
-        if ( key === 'modules') {
-            return value.forEach(k => {
+        let componentObject;
+        if (key === 'modules') {
+            value.forEach((k) => {
                 k.declarations.forEach((decl) => {
-                    decl.customElement === true ? components.push({class: decl, path: k.path.replace('index.js', 'react.ts')}) : ''
-                })
-            })
+                    componentObject = decl.customElement === true ? components.push({ class: decl, path: k.path.replace('index.js', 'react.ts') }) : '';
+                });
+            });
         }
-    })
+
+        return componentObject;
+    });
 
     if (components.length > 0) {
         components.forEach((component) => {
-            const data = existsSync(component.path, { encoding: 'utf8', flag: 'r' })
+            const data = existsSync(component.path, { encoding: 'utf8', flag: 'r' });
 
             if (data) {
                 unlink(component.path, (err) => {
                     if (err) throw err;
-                })
+                });
             }
-        })
+        });
     }
 }
