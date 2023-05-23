2ce30234997081d8a6248ccbf3ea5f923bc0cbeb
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-05-23 16:09:18 +0100

    wip: add lighthouse github action

---
+++ .github/lighthouserc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "ci": {
+      "assert": {
+        "assertions": {
+          "categories:performance": ["warn", {"minScore": 0.9}],
+          "categories:accessibility": ["warn", {"minScore": 0.9}],
+          "categories:best-practices": ["warn", {"minScore": 0.9}],
+          "categories:seo": ["warn", {"minScore": 0.9}],
+        }
+      }
+    }
+  }
