593b534dd8eb1a3295cfff086e24d987684e2034
Joshua Ng <joshua.ng@just-eat.com>
2023-05-25 16:14:58 +0100

    test(pie-docs): added colour page toggle system test

---
+++ apps/pie-docs/test/system/colour-toggle.spec.js
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+import { disableCookieBanner } from '../playwright/playwright-helper';
+
+test.beforeEach(async ({ page, baseURL }) => {
+    await page.goto(baseURL);
+});
+
+test.describe('PIE - colour page toggle - @desktop', () => {
+    test.beforeEach(async ({ page, context }) => {
+        await disableCookieBanner(page, context);
+    });
+
+    test('Should check the functionality of the colour page toggle', async ({ page }) => {
+        // Arrange
+        await page.goto('foundations/colour/tokens/alias/light');
+
+        const toggleOptionA = page.getByTestId('toggle-option-a');
+        const toggleOptionB = page.getByTestId('toggle-option-b');
+
+        const lightTokens = page.locator('[id="toggle-content-a"]');
+        const darkTokens = page.locator('[id="toggle-content-b"]');
+
+        // Act - Clicks the dark theme toggle
+        await toggleOptionB.click();
+
+        // Assert - Dark tokens are visible and the light tokens are not visible
+        await expect.soft(darkTokens).toBeVisible();
+        await expect.soft(lightTokens).not.toBeVisible();
+
+        // Act - Clicks the light theme toggle
+        await toggleOptionA.click();
+
+        // Assert - Light tokens are visible and the dark tokens are not visible
+        await expect.soft(lightTokens).toBeVisible();
+        await expect(darkTokens).not.toBeVisible();
+    });
+});
