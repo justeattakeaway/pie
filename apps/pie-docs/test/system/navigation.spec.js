b140a7c155a6498a44780d48c9e82d1112b0bafd
FayeCarter <faye.carter@justeattakeaway.com>
2023-06-02 11:38:28 +0100

    test(pie-docs): add system test for navigation on desktop (#484)
    * feat(pie-docs): add percy test
    
    * feat(pie-docs): update test
    
    * feat(pie-docs): update test
    
    * feat(pie-docs): switch to system test
    
    * test(pie-docs): remove visual test
    
    * test(pie-docs): improve test id name
    
    * test(pie-docs): add helper function
    
    * test(pie-docs): update changelog
    
    * test(pie-docs): split into two tests
    
    * test(pie-docs): fix linting
    
    * test(pie-docs): re order tests
    
    * test(pie-docs): use soft expect

--- apps/pie-docs/test/system/navigation.spec.js
+++ apps/pie-docs/test/system/navigation.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { disableCookieBanner } from '../playwright/playwright-helper';
 import expectedRoutesJson from '../snapshots/expected-routes.snapshot.json';
+import { isElementVisibleInViewport } from '../helpers/functions';
 
 test.beforeEach(async ({ page, baseURL }) => {
     await page.goto(baseURL);
@@ -43,5 +44,35 @@ test.describe('PIE - site nav menu - @mobile', () => {
 
         // Assert - Nav menu is closed
         await expect(navMenu).not.toBeVisible();
+    });
+});
+
+test.describe('PIE - site nav menu - @desktop', () => {
+    let siteNavigation;
+    let contentHeader;
+
+    test.beforeEach(async ({ page, context }) => {
+        await disableCookieBanner(page, context);
+        await page.goto('all-about-pie/what-is-pie/');
+
+        // Arrange
+        siteNavigation = await page.getByTestId('site_nav_section_1');
+        contentHeader = await page.getByTestId('content_header');
+    });
+
+    test('Should be visible at the top of the page', async () => {
+        // Assert - Navigation and top of the page are visible
+        await expect.soft(await isElementVisibleInViewport(siteNavigation)).toBeTruthy();
+        await expect(await isElementVisibleInViewport(contentHeader)).toBeTruthy();
+    });
+
+    test('Should still be visible when the page has scrolled down', async ({ page }) => {
+        // Act - Scroll to the bottom of the page
+        await page.keyboard.press('End');
+        await page.waitForTimeout(1000);
+
+        // Assert - Navigation is still visible, but top of the page is not
+        await expect.soft(await isElementVisibleInViewport(contentHeader)).not.toBeTruthy();
+        await expect(await isElementVisibleInViewport(siteNavigation)).toBeTruthy();
     });
 });
