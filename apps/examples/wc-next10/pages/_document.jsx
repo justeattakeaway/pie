09a7b4b5684e54d9f12a9854a99493536f1004e2
Lizzie Turney <49618712+LTurns@users.noreply.github.com>
2023-06-05 16:56:49 +0100

    fix(pie-monorepo): tag name and styling fix for example apps (#507)
    * fix(pie-monorepo): fix for example apps
    
    * fix(pie-wrapper-react): updated readme
    
    * fix(pie-monorepo): added changesets
    
    * fix(pie-wrapper-react): small sentence change in readme
    
    * cosmetic(pie-monorepo): added CSS variables to example apps
    
    * fix(pie-button): encapsulated styles and removed reflect
    
    ---------
    
    Co-authored-by: Lizzie Turney <lizzie.turney@just-eat.com>
    Co-authored-by: Ashley Watson-Nolan <nolly00@gmail.com>

--- apps/examples/wc-next10/pages/_document.jsx
+++ apps/examples/wc-next10/pages/_document.jsx
@@ -1,21 +1,25 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
-import Document, { Html, Main, NextScript, Head } from 'next/document';
+import Document, {
+    Html, Main, NextScript, Head,
+} from 'next/document';
 
 export default class CustomDocument extends Document {
-  render() {
-    return (
-      <Html>
-            <Head>
-                <title>Create Next App</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render () {
+        return (
+            <Html>
+                <Head>
+                    <title>Create Next App</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="stylesheet" type="text/css" href="https://unpkg.com/@justeat/pie-design-tokens/dist/jet.css" />
+                    <link rel="stylesheet" type="text/css" href="https://unpkg.com/@justeat/pie-design-tokens/dist/jet-hsl-colors.css" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
