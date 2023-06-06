f5b83885a64ed7818672d18164b3b9d64a5ba2f6
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2023-06-06 13:18:22 +0100

    test(pie-icons-vue): expand test coverage for pie-icons-vue

--- packages/tools/pie-icons-vue/test/IconAlcohol.test.js
+++ packages/tools/pie-icons-vue/test/IconAlcohol.test.js
@@ -1,6 +1,15 @@
 import { mount } from '@vue/test-utils';
+/* eslint-disable import/no-extraneous-dependencies */
+import {
+    describe,
+    expect,
+    test,
+    vi,
+} from 'vitest';
+/* eslint-enable import/no-extraneous-dependencies */
 
 import IconAlcohol from '../icons/IconAlcohol';
+import { regularIconSizeDefault } from '../icons/configs';
 
 describe('IconAlcohol', () => {
     test('should exist', () => {
@@ -28,5 +37,42 @@ describe('IconAlcohol', () => {
         // Assert
         expect(wrapper.classes()).toContain(`c-pieIcon--${iconSize}`);
         expect(wrapper.classes()).toHaveLength(3);
+    });
+
+    test.each([
+        'xxs',
+        'xxxl',
+        '',
+    ])('should not allow invalid sizes - %s', (iconSize) => {
+        // Arrange
+        const consoleError = console.error;
+        const errorMock = vi.fn();
+        console.error = errorMock;
+        const propsData = { iconSize };
+
+        // Act
+        const wrapper = mount(IconAlcohol, { propsData });
+
+        // Assert
+        expect(wrapper.classes()).not.toContain(`c-pieIcon--${iconSize}`);
+        expect(wrapper.classes()).toContain(`c-pieIcon--${regularIconSizeDefault}`);
+        expect(errorMock.mock.calls[0][0].startsWith('[Vue warn]: Invalid prop')).toBeTruthy();
+
+        console.error = consoleError;
+    });
+
+    test('should ignore custom height and width', () => {
+        // Arrange
+        const propsData = {
+            height: '40px',
+            width: '100px',
+        };
+
+        // Act
+        const wrapper = mount(IconAlcohol, { propsData });
+
+        // Assert
+        expect(wrapper.html()).not.toContain('width');
+        expect(wrapper.html()).not.toContain('height');
     });
 });
--- packages/tools/pie-icons-vue/test/IconAlcoholLarge.test.js
+++ packages/tools/pie-icons-vue/test/IconAlcoholLarge.test.js
@@ -1,6 +1,8 @@
 import { mount } from '@vue/test-utils';
+import { describe, expect, test } from 'vitest'; // eslint-disable-line import/no-extraneous-dependencies
 
 import IconAlcoholLarge from '../icons/IconAlcoholLarge';
+import { largeIconSizeDefault } from '../icons/configs';
 
 describe('IconAlcoholLarge', () => {
     test('should exist', () => {
@@ -11,16 +13,69 @@ describe('IconAlcoholLarge', () => {
         expect(wrapper.exists()).toBe(true);
     });
 
-    test.each([
-        32, 40, 48, 120
-    ])('should not apply additional classes for large icon of size "%s"', (iconSize) => {
-        // Arrange
-        const propsData = { iconSize };
+    describe('iconSize prop', () => {
+        describe('validator', () => {
+            test.each([
+                32,
+                40,
+                120
+            ])('should allow valid sizes - %s', (iconSize) => {
+                // Arrange
+                const { validator } = IconAlcoholLarge.props.iconSize;
 
-        // Act
-        const wrapper = mount(IconAlcoholLarge, { propsData });
+                // Act
+                const isValid = validator(iconSize);
 
-        // Assert
-        expect(wrapper.classes()).toEqual(['c-pieIcon', 'c-pieIcon--alcoholLarge']);
+                // Assert
+                expect(isValid).toBe(true);
+            });
+
+            test.each([
+                24, // Too small
+                36, // Not a multiple of 8
+            ])('should not allow invalid sizes - %s', (iconSize) => {
+                // Arrange
+                const { validator } = IconAlcoholLarge.props.iconSize;
+
+                // Act
+                const isValid = validator(iconSize);
+
+                // Assert
+                expect(isValid).toBe(false);
+            });
+        });
+    });
+
+    describe('when mapping to attributes', () => {
+        test.each([
+            32,
+            40,
+            120
+        ])('should be correctly mapped to width and height - %s', (iconSize) => {
+            // Arrange
+            const propsData = { iconSize };
+
+            // Act
+            const wrapper = mount(IconAlcoholLarge, { propsData });
+
+            // Assert
+            expect(wrapper.html()).toContain(`width="${iconSize}"`);
+            expect(wrapper.html()).toContain(`height="${iconSize}"`);
+        });
+
+        test.each([
+            24, // Too small
+            36, // Not a multiple of 8
+        ])('should fall back to the default size - %s', (iconSize) => {
+            // Arrange
+            const propsData = { iconSize };
+
+            // Act
+            const wrapper = mount(IconAlcoholLarge, { propsData });
+
+            // Assert
+            expect(wrapper.html()).toContain(`width="${largeIconSizeDefault}"`);
+            expect(wrapper.html()).toContain(`height="${largeIconSizeDefault}"`);
+        });
     });
 });
