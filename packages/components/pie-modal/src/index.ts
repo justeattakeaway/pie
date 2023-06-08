4bf71f630721e660f89e14343701c86505d3cc40
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2023-06-08 16:03:55 +0200

    Add callbacks, event listener and query

--- packages/components/pie-modal/src/index.ts
+++ packages/components/pie-modal/src/index.ts
@@ -1,5 +1,5 @@
 import { LitElement, html, unsafeCSS } from 'lit'; // eslint-disable-line import/no-extraneous-dependencies
-import { property } from 'lit/decorators.js';
+import { property, query } from 'lit/decorators.js';
 import { RtlMixin } from '@justeattakeaway/pie-webc-core';
 
 import styles from './modal.scss?inline';
@@ -8,16 +8,64 @@ export class PieModal extends RtlMixin(LitElement) {
     @property({ type: Boolean })
         isOpen = false;
 
+    // Note: Using Underscores in Lit querys is some sort of Lit standard or requirement? Perhaps ask the Lit team.
+    @query('dialog')
+        _dialog;
+
+    connectedCallback () {
+        super.connectedCallback();
+
+        // Note: If run inline, querySelector won't find the modal
+        // TODO: Check if there is other way to avoid this workaround?
+        setTimeout(() => {
+            const dialog = this._dialog;
+
+            if (dialog) {
+                if (this.isOpen) dialog.showModal();
+                dialog.addEventListener('close', this.handleDialogClose);
+            }
+        }, 0);
+    }
+
+    disconnectedCallback () {
+        super.disconnectedCallback();
+
+        const dialog = this._dialog;
+
+        if (dialog) {
+            dialog.removeEventListener('close', this.handleDialogClose);
+        }
+    }
+
+    // Note: This wouldn't be necessary if we could use Dialog as modal only by using attributes
+    willUpdate (changedProperties: PropertyValues<this>) {
+        const dialog = this._dialog;
+
+        if (changedProperties.has('isOpen') && dialog) {
+            const isClosed = changedProperties.get('isOpen') === false;
+
+            if (isClosed) {
+                dialog.showModal();
+            } else {
+                dialog.close();
+            }
+        }
+    }
+
     // eslint-disable-next-line class-methods-use-this
     render () {
         return html`
-            <dialog class="c-modal" ?open=${this.isOpen}>
-                <h3 class="c-modal-heading">Modal header####</h3>
+            <dialog class="c-modal" ?open=${false}>
+                <h3 class="c-modal-heading">Modal header</h3>
                 <div class="c-modal-contentWrapper">
                     <slot></slot>
                 </div>
             </dialog>
         `;
+    }
+
+    handleDialogClose () {
+        this.dispatchEvent(new CustomEvent('onClose'));
     }
 
     // Renders a `CSSResult` generated from SCSS by Vite
