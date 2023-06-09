46d53b78815efda4cc7d74e3920a185b701031e3
Raouf <raouf.sawehli@justeattakeaway.com>
2023-06-09 15:58:03 +0200

    refactor(pie-modal): update percy snapshot" git commit -m "refactor(pie-modal): update percy snapshot"
    :wq
    `

--- packages/components/pie-modal/test/visual/pie-modal.spec.ts
+++ packages/components/pie-modal/test/visual/pie-modal.spec.ts
@@ -2,21 +2,21 @@ import { test } from '@sand4rt/experimental-ct-web';
 import percySnapshot from '@percy/playwright';
 import { PieModal } from '@/index';
 
-const propIsOpenValues = [true, false];
+const propIsOpenValues = [
+    { heading: 'Modal Header / isOpen = true', isOpen: true },
+    { heading: 'Modal Header / isOpen = false', isOpen: false }
+];
 
-propIsOpenValues.forEach(propValue =>
-    test(`should render Modal correctly when prop isOpen is set to ${propValue}`, async ({ page, mount }) => {
-        await mount(
-            PieModal,
-            {
-                props: {
-                    isOpen: propValue
-                },
-                slots: {
-                    default: 'Hello, this is the Pie Modal!',
-                },
+propIsOpenValues.forEach((props) => test(`should render Modal correctly when prop isOpen is set to ${props.isOpen}`, async ({ page, mount }) => {
+    await mount(
+        PieModal,
+        {
+            props,
+            slots: {
+                default: 'Hello, this is the Pie Modal!',
             },
-        );
+        },
+    );
 
-        await percySnapshot(page, `PIE Modal when isOpen is set to ${propValue}`);
+    await percySnapshot(page, `PIE Modal when isOpen is set to ${props.isOpen}`);
 }));
