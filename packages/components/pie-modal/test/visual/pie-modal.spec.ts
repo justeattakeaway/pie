ae278f2c15f5f5ae9cabc0e28a2e0343ff66a725
Raouf <raouf.sawehli@justeattakeaway.com>
2023-06-09 22:37:51 +0200

    fix(pie-modal): rename a test var

--- packages/components/pie-modal/test/visual/pie-modal.spec.ts
+++ packages/components/pie-modal/test/visual/pie-modal.spec.ts
@@ -2,9 +2,9 @@ import { test } from '@sand4rt/experimental-ct-web';
 import percySnapshot from '@percy/playwright';
 import { PieModal } from '@/index';
 
-const componentProps = [{ heading: 'Modal Heading', isOpen: true }, { isOpen: false }];
+const propIsOpenValues = [{ heading: 'Modal Heading', isOpen: true }, { isOpen: false }];
 
-componentProps.forEach((props) => test(`should render Modal correctly when prop isOpen is set to ${props.isOpen}`, async ({ page, mount }) => {
+propIsOpenValues.forEach((props) => test(`should render Modal correctly when prop isOpen is set to ${props.isOpen}`, async ({ page, mount }) => {
     await mount(
         PieModal,
         {
