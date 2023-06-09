63caf5f68c66e6831ad56a6772e95dae68a53dc4
Raouf <raouf.sawehli@justeattakeaway.com>
2023-06-09 22:06:02 +0200

    refactor(pie-modal): use test wrapper for tests

--- packages/components/pie-modal/test/visual/pie-modal.spec.ts
+++ packages/components/pie-modal/test/visual/pie-modal.spec.ts
@@ -1,22 +1,60 @@
 import { test } from '@sand4rt/experimental-ct-web';
 import percySnapshot from '@percy/playwright';
+import type {
+    PropObject, WebComponentPropValues, WebComponentTestInput,
+} from '@justeattakeaway/pie-webc-core/src/test-helpers/defs.ts';
+import {
+    getAllPropCombinations, splitCombinationsByPropertyValue,
+} from '@justeattakeaway/pie-webc-core/src/test-helpers/get-all-prop-combos.ts';
+import {
+    createTestWebComponent,
+} from '@justeattakeaway/pie-webc-core/src/test-helpers/rendering.ts';
+import {
+    WebComponentTestWrapper,
+} from '@justeattakeaway/pie-webc-core/src/test-helpers/components/web-component-test-wrapper/WebComponentTestWrapper.ts';
 import { PieModal } from '@/index';
+import { HEADING_LEVELS } from '@/defs';
 
-const propIsOpenValues = [
-    { heading: 'Modal Header / isOpen = true', isOpen: true },
-    { heading: 'Modal Header / isOpen = false', isOpen: false }
-];
+const props: PropObject = {
+    heading: 'Modal heading',
+    headingLevel: Object.values(HEADING_LEVELS),
+    isOpen: [true, false],
+};
 
-propIsOpenValues.forEach((props) => test(`should render Modal correctly when prop isOpen is set to ${props.isOpen}`, async ({ page, mount }) => {
+// Renders a <pie-modal> HTML string with the given prop values
+const renderTestPieModal = (propVals: WebComponentPropValues) => `<pie-modal isOpen="${propVals.isOpen}" heading="${propVals.heading}" headingLevel="${propVals.headingLevel}">Hello, this is the Pie Modal!</pie-modal>`;
+
+const componentPropsMatrix : WebComponentPropValues[] = getAllPropCombinations(props);
+const componentPropsMatrixByIsOpenValues: Record<string, WebComponentPropValues[]> = splitCombinationsByPropertyValue(componentPropsMatrix, 'isOpen');
+const componentIsOpenValues: string[] = Object.keys(componentPropsMatrixByIsOpenValues);
+
+// This ensures the component is registered in the DOM for each test
+// This is not required if your tests mount the web component directly in the tests
+test('Component registered in the DOM', async ({ mount }) => {
     await mount(
         PieModal,
         {
-            props,
-            slots: {
-                default: 'Hello, this is the Pie Modal!',
-            },
+            props: {},
+            slots: {},
         },
     );
+});
 
-    await percySnapshot(page, `PIE Modal when isOpen is set to ${props.isOpen}`);
+componentIsOpenValues.forEach((isOpen) => test(`Render all prop variations for isOpen: ${isOpen}`, async ({ page, mount }) => {
+    await Promise.all(componentPropsMatrixByIsOpenValues[isOpen].map(async (combo: WebComponentPropValues) => {
+        const testComponent: WebComponentTestInput = createTestWebComponent(combo, renderTestPieModal);
+        const propKeyValues = `heading: ${testComponent.propValues.heading}, headingLevel: ${testComponent.propValues.headingLevel}`;
+
+        await mount(
+            WebComponentTestWrapper,
+            {
+                props: { propKeyValues },
+                slots: {
+                    default: testComponent.renderedString.trim(),
+                },
+            },
+        );
+    }));
+
+    await percySnapshot(page, `PIE Modal - isOpen: ${isOpen}`);
 }));
