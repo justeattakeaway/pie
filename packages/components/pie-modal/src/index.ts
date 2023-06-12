905fb641bedb1ccbb24bdd3e368887f8d02af213
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2023-06-12 14:55:43 +0200

    spike(pie-modal): test dialog as modal

--- packages/components/pie-modal/src/index.ts
+++ packages/components/pie-modal/src/index.ts
@@ -1,6 +1,6 @@
 import { LitElement, unsafeCSS } from 'lit'; // eslint-disable-line import/no-extraneous-dependencies
 import { html, unsafeStatic } from 'lit/static-html.js';
-import { property } from 'lit/decorators.js'; // eslint-disable-line import/no-extraneous-dependencies
+import { property, query } from 'lit/decorators.js'; // eslint-disable-line import/no-extraneous-dependencies
 import { RtlMixin, validPropertyValues, requiredProperty } from '@justeattakeaway/pie-webc-core';
 
 import styles from './modal.scss?inline';
@@ -23,9 +23,55 @@ export class PieModal extends RtlMixin(LitElement) {
     @validPropertyValues(componentSelector, Object.values(HEADING_LEVELS), HEADING_LEVELS.H2)
         headingLevel : HEADING_LEVELS = HEADING_LEVELS.H2;
 
+    // Note: Using Underscores in Lit querys is some sort of Lit standard or requirement? Perhaps ask the Lit team.
+    @query('dialog')
+        _dialog: HTMLDialogElement;
+
+    connectedCallback () {
+        super.connectedCallback();
+
+        // Note: If run inline, querySelector won't find the modal
+        // TODO: Check if there is other way to avoid this workaround?
+        setTimeout(() => {
+            const dialog = this._dialog;
+
+            if (dialog) {
+                if (this.isOpen) dialog.showModal();
+
+                dialog.addEventListener('click', this.handleBackdropClick);
+                dialog.addEventListener('close', this.handleDialogClose);
+            }
+        }, 0);
+    }
+
+    disconnectedCallback () {
+        super.disconnectedCallback();
+
+        const dialog = this._dialog;
+
+        if (dialog) {
+            dialog.removeEventListener('click', this.handleBackdropClick);
+            dialog.removeEventListener('close', this.handleDialogClose);
+        }
+    }
+
+    // Note: This wouldn't be necessary if we could use Dialog as modal only by using attributes
+    willUpdate (changedProperties: PropertyValues<this>) {
+        const dialog = this._dialog;
+
+        if (changedProperties.has('isOpen') && dialog) {
+            const isClosed = changedProperties.get('isOpen') === false;
+
+            if (isClosed) {
+                dialog.showModal();
+            } else {
+                dialog.close();
+            }
+        }
+    }
+
     render () {
         const {
-            isOpen,
             heading,
             headingLevel,
         } = this;
@@ -33,14 +79,32 @@ export class PieModal extends RtlMixin(LitElement) {
         const headingTag = unsafeStatic(headingLevel);
 
         return html`
-            <dialog class="c-modal" ?open="${isOpen}">
-                <${headingTag} class="c-modal-heading">${heading}</${headingTag}>
+            <dialog class="c-modal">
+                <${headingTag} class="c-modal-heading">
+                    ${heading}
+                    <button @click=${this.handleCloseButtonClick}>close</button>
+                </${headingTag}>
                 <div class="c-modal-contentWrapper">
                     <slot></slot>
                 </div>
             </dialog>
         `;
     }
+
+    handleDialogClose () {
+        this.dispatchEvent(new CustomEvent('onClose'));
+    }
+
+    handleBackdropClick = (event) => {
+        // Note: This is a bit hackish, we need to find the most suitable solution for detecting clicks outside the Dialog. Another possible solution is using event.target.getBoundingClientRect() and event.clientX/Y
+        if (event.target && event.target.nodeName === 'DIALOG') {
+            this._dialog.close();
+        }
+    };
+
+    handleCloseButtonClick = () => {
+        this._dialog.close();
+    };
 
     // Renders a `CSSResult` generated from SCSS by Vite
     static styles = unsafeCSS(styles);
