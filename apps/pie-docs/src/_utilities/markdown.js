f9534b614407f13100c7d1ba021a8cf94368fa45
FayeCarter <faye.carter@justeattakeaway.com>
2023-06-12 10:25:18 +0100

    Revert "feat(pie-docs): add clicked"
    This reverts commit 5ee62047485bccd2e735bc94b9a3d8b9f0bc7017.

--- apps/pie-docs/src/_utilities/markdown.js
+++ apps/pie-docs/src/_utilities/markdown.js
@@ -1,27 +1,8 @@
 const MarkdownIt = require('markdown-it');
 const markdownItAttrs = require('markdown-it-attrs');
-const slugify = require('slugify');
-const pieIconsSvg = require('../_11ty/filters/pieIconsSvg');
-
-const anchor = pieIconsSvg({
-    name: 'link',
-    attrs: {
-        'aria-hidden': 'true',
-        height: 16,
-        width: 16,
-    },
-});
 
 const md = new MarkdownIt({
     html: true,
-})
-    .use(require('markdown-it-anchor'), {
-        level: [2, 3, 4],
-        permalink: true,
-        permalinkClass: 'c-heading-anchor',
-        permalinkSymbol: anchor,
-        slugify: (s) => slugify(s, { lower: true, remove: /[$*_+~.()'"!/\-:@?]+/g }),
-    })
-    .use(markdownItAttrs);
+}).use(markdownItAttrs);
 
 module.exports = md;
