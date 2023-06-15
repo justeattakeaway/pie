f243ed073e38248325e989d433793586f1c8ddd8
FayeCarter <faye.carter@justeattakeaway.com>
2023-06-15 16:15:09 +0100

    feat(pie-docs): add markdown anchors

--- apps/pie-docs/src/_utilities/markdown.js
+++ apps/pie-docs/src/_utilities/markdown.js
@@ -1,8 +1,33 @@
 const MarkdownIt = require('markdown-it');
 const markdownItAttrs = require('markdown-it-attrs');
+const slugify = require('slugify');
+const anchor = require('markdown-it-anchor');
+const pieIconsSvg = require('../_11ty/filters/pieIconsSvg');
+
+const anchorIcon = pieIconsSvg({
+    name: 'link',
+    attrs: {
+        'aria-hidden': 'true',
+        height: 16,
+        width: 16,
+        fill: 'interactive-brand',
+    },
+});
 
 const md = new MarkdownIt({
     html: true,
-}).use(markdownItAttrs);
+}).use(markdownItAttrs)
+    .use(anchor, {
+        level: [2, 3, 4],
+        permalink: [
+            anchor.permalink.ariaHidden({
+                placement: 'before',
+            }),
+        ],
+        permalinkClass: 'c-anchor-icon',
+        permalinkSymbol: anchorIcon,
+        permalinkBefore: true,
+        slugify: (s) => slugify(s, { lower: true, remove: /[$*_+~.()'"!/\-:@?]+/g }),
+    });
 
 module.exports = md;
