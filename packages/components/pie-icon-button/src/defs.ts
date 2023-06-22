c54a8e723ff6de45d2dda65e435d8af7587dabdf
Ashley Watson-Nolan <ashley.watson-nolan@justeattakeaway.com>
2023-06-22 12:36:33 +0100

    feat(pie-icon-button): size prop added to component and storybook (#557)

--- packages/components/pie-icon-button/src/defs.ts
+++ packages/components/pie-icon-button/src/defs.ts
@@ -1,5 +1,10 @@
 export interface IconButtonProps {
     /**
+     * the size of the icon button.
+     * @default medium
+     */
+    size: 'xsmall' | 'small' | 'medium' | 'large';
+    /**
      * the variant of the icon button.
      * @default primary
      */
@@ -10,6 +15,16 @@ export interface IconButtonProps {
      */
     disabled?: boolean;
 }
+
+/**
+ * Icon Button size variants
+ */
+export const iconButtonSizes: IconButtonProps['size'][] = [
+    'xsmall',
+    'small',
+    'medium',
+    'large'
+];
 
 /**
  * Icon Button style variants
