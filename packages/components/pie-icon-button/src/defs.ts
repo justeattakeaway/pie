2fe58d7051420bd4bbcd73a1b4239b6069e0bb3b
Jamie Maguire <jamieolivermaguire@googlemail.com>
2023-06-28 16:51:36 +0100

    docs(pie-monorepo): add descriptive jsdoc comments to WC props (#578)
    * docs(pie-monorepo): add descriptive jsdoc comments to componet props
    
    * docs(pie-monorepo): add defaults to prop jsdoc comments
    
    * docs(pie-monorepo): add full stops
    
    * docs(pie-monorepo): mark props as optional or required

--- packages/components/pie-icon-button/src/defs.ts
+++ packages/components/pie-icon-button/src/defs.ts
@@ -2,7 +2,19 @@ export const sizes = ['xsmall', 'small', 'medium', 'large'] as const;
 export const variants = ['primary', 'secondary', 'outline', 'ghost', 'ghost-tertiary'] as const;
 
 export interface IconButtonProps {
+    /**
+     * (Optional) What size the button should be.
+     * @default "medium"
+     */
     size: typeof sizes[number];
+    /**
+     * (Optional) What style variant the button should be such as primary, outline or ghost.
+     * @default "primary"
+     */
     variant: typeof variants[number];
-    disabled?: boolean;
+    /**
+     * (Optional) When true, the button element is disabled.
+     * @default false
+     */
+    disabled: boolean;
 }
