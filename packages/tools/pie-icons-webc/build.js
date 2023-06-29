ecb825912a005cdb1b35ac90100ce3cc6460d8dd
Lizzie Turney <lizzie.turney@just-eat.com>
2023-06-29 15:17:40 +0100

    fix(pie-monorepo): resolved issue with rebasing

--- packages/tools/pie-icons-webc/build.js
+++ packages/tools/pie-icons-webc/build.js
@@ -79,7 +79,7 @@ function copyIconsConfigFiles () {
     ];
     const destFilePath = path.resolve(process.cwd(), './icons');
 
-    srcFilePaths.forEach((srcFilePath) => execSync(`copy ${srcFilePath} ${destFilePath}`));
+    srcFilePaths.forEach((srcFilePath) => execSync(`cp ${srcFilePath} ${destFilePath}`));
 }
 
 async function build () {
