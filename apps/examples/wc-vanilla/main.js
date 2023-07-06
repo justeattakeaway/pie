13eaeca1d8bfc5e15968c218cfc3c5c73e986212
Ashley Watson-Nolan <nolly00@gmail.com>
2023-07-06 10:34:37 +0100

    feat(pie-monorepo): added icon-button to vanilla js example app

--- apps/examples/wc-vanilla/main.js
+++ apps/examples/wc-vanilla/main.js
@@ -5,6 +5,7 @@ document.querySelector('#app').innerHTML = `
     <div>
         <h1>Hello World!</h1>
         <pie-button id="counter" type="button"></pie-button>
+        <pie-icon-button size='medium' type='button' variant='primary'></pie-icon-button>
 </div>
 `;
 
