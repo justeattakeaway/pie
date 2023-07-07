584942e6cb06e64a9e0c132917378ee36db9297c
Ashley Watson-Nolan <nolly00@gmail.com>
2023-07-07 15:31:48 +0100

    feat(pie-codemods): merged inquirer and cli code together

--- packages/tools/pie-codemods/codemod-gui.js
+++ packages/tools/pie-codemods/codemod-gui.js
@@ -1,34 +1,328 @@
 #!/usr/bin/env node
 
-// load the jscodeshift runner
-import * as jscodeshift from 'jscodeshift/src/Runner.js';
+// import { select, Separator } from '@inquirer/prompts';
+
 import * as path from 'path';
+import { fileURLToPath } from 'url';
+import inquirer from 'inquirer';
+import meow from 'meow';
+import chalk from 'chalk';
+import * as execPkg from 'execa';
+import { createRequire } from 'node:module';
 
-import { select, Separator } from '@inquirer/prompts';
+import * as globbyPkg from 'globby';
+import * as isGitClean from 'is-git-clean';
 
-const answer = await select({
-    message: 'Select a migration',
-    choices: [
+const { execa } = execPkg;
+const { globby } = globbyPkg;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const require = createRequire(import.meta.url);
+const transformerDirectory = path.join(__dirname, '../', 'transforms');
+const jscodeshiftExecutable = require.resolve('.bin/jscodeshift');
+
+function checkGitStatus (force) {
+    let clean = false;
+    let errorMessage = 'Unable to determine if git directory is clean';
+    try {
+        clean = isGitClean.sync(process.cwd());
+        errorMessage = 'Git directory is not clean';
+    } catch (err) {
+        if (err && err.stderr && err.stderr.indexOf('Not a git repository') >= 0) {
+            clean = true;
+        }
+    }
+
+    if (!clean) {
+        if (force) {
+            console.log(`WARNING: ${errorMessage}. Forcibly continuing.`);
+        } else {
+            console.log('Thank you for using pie-codemods!');
+            console.log(chalk.yellow('\nBut before we continue, please stash or commit your git changes.'));
+            console.log('\nYou may use the --force flag to override this safety check.');
+            process.exit(1);
+        }
+    }
+}
+
+function runTransform ({
+    files, flags, parser, transformer, answers,
+}) {
+    const transformerPath = path.join(transformerDirectory, `${transformer}.js`);
+
+    let args = [];
+
+    const { dry, print, explicitRequire } = flags;
+
+    if (dry) {
+        args.push('--dry');
+    }
+    if (print) {
+        args.push('--print');
+    }
+
+    if (explicitRequire === 'false') {
+        args.push('--explicit-require=false');
+    }
+
+    args.push('--verbose=2');
+
+    args.push('--ignore-pattern=**/node_modules/**');
+
+    args.push('--parser', parser);
+
+    if (parser === 'tsx') {
+        args.push('--extensions=tsx,ts,jsx,js');
+    } else {
+        args.push('--extensions=jsx,js');
+    }
+
+    args = args.concat(['--transform', transformerPath]);
+
+    if (transformer === 'pure-component') {
+        if (answers.pureComponentUseArrows) {
+            args.push('--useArrows=true');
+        }
+        if (answers.pureComponentDestructuring) {
+            args.push('--destructuring=true');
+        }
+    }
+
+    if (flags.jscodeshift) {
+        args = args.concat(flags.jscodeshift);
+    }
+
+    args = args.concat(files);
+
+    console.log(`Executing command: jscodeshift ${args.join(' ')}`);
+
+    const result = execa.sync(jscodeshiftExecutable, args, {
+        stdio: 'inherit',
+        stripEof: false,
+    });
+
+    if (result.error) {
+        throw result.error;
+    }
+}
+
+const TRANSFORMER_INQUIRER_CHOICES = [
+    {
+        name: 'pie-design-tokens@v5.0.0',
+        value: 'pie-design-tokens@v5.0.0',
+        description: 'Update to version 5.0.0 of pie-design-tokens',
+    },
+    {
+        name: 'pie-button@v2.0.0',
+        value: 'pie-button@v2.0.0',
+        description: 'Update to version 2.0.0 of pie-button',
+    }
+];
+
+// const answer = await select({
+//     message: 'Select a migration',
+//     choices: [
+
+//     ],
+// });
+
+function expandFilePathsIfNeeded (filesBeforeExpansion) {
+    const shouldExpandFiles = filesBeforeExpansion.some((file) => file.includes('*'));
+    return shouldExpandFiles
+        ? globby.sync(filesBeforeExpansion)
+        : filesBeforeExpansion;
+}
+
+function run () {
+    const cli = meow(
         {
-            name: 'pie-button@v2',
-            value: 'pie-button@v2',
-            description: 'Some stuff here',
+            description: 'Codemods for updating React APIs.',
+            help: `
+      Usage
+        $ npx react-codemod <transform> <path> <...options>
+
+          transform    One of the choices from https://github.com/reactjs/react-codemod
+          path         Files or directory to transform. Can be a glob like src/**.test.js
+
+      Options
+        --force            Bypass Git safety checks and forcibly run codemods
+        --dry              Dry run (no changes are made to files)
+        --print            Print transformed files to your terminal
+        --explicit-require Transform only if React is imported in the file (default: true)
+
+        --jscodeshift  (Advanced) Pass options directly to jscodeshift
+      `,
+            importMeta: import.meta,
         },
         {
-            name: 'pie-design-tokens@v5',
-            value: 'pie-design-tokens@v5',
-            description: 'Some stuff here',
+            boolean: ['force', 'dry', 'print', 'explicit-require', 'help'],
+            string: ['_'],
+            alias: {
+                h: 'help',
+            },
         },
-        new Separator(),
-        {
-            name: 'v1asasd',
-            value: 'v1asdasdas',
-            description: 'Some stuff here',
-        },
-        {
-            name: 'vasaasdas1',
-            value: 'vasdasd1',
-            description: 'Some stuff here',
-        },
-    ],
-});
+    );
+    if (!cli.flags.dry) {
+        checkGitStatus(cli.flags.force);
+    }
+
+    if (
+        cli.input[0] &&
+        !TRANSFORMER_INQUIRER_CHOICES.find((x) => x.value === cli.input[0])
+    ) {
+        console.error('Invalid transform choice, pick one of:');
+        console.error(TRANSFORMER_INQUIRER_CHOICES.map((x) => `- ${x.value}`).join('\n'));
+        process.exit(1);
+    }
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'files',
+                message: 'On which files or directory should the codemods be applied?',
+                when: !cli.input[1],
+                default: '.',
+                // validate: () =>
+                filter: (files) => files.trim(),
+            },
+            {
+                type: 'list',
+                name: 'parser',
+                message: 'Which dialect of JavaScript do you use?',
+                default: 'babel',
+                when: !cli.flags.parser,
+                pageSize: PARSER_INQUIRER_CHOICES.length,
+                choices: PARSER_INQUIRER_CHOICES,
+            },
+            {
+                type: 'list',
+                name: 'transformer',
+                message: 'Which transform would you like to apply?',
+                when: !cli.input[0],
+                pageSize: TRANSFORMER_INQUIRER_CHOICES.length,
+                choices: TRANSFORMER_INQUIRER_CHOICES,
+            },
+            // if transformer === 'class'
+            {
+                type: 'confirm',
+                name: 'classFlow',
+                when: (answers) => cli.input[0] === 'class' || answers.transformer === 'class',
+                message: 'Generate Flow annotations from propTypes?',
+                default: true,
+            },
+            {
+                type: 'confirm',
+                name: 'classRemoveRuntimePropTypes',
+                when: (answers) => answers.classFlow === true,
+                message: 'Remove runtime PropTypes?',
+                default: false,
+            },
+            {
+                type: 'confirm',
+                name: 'classPureComponent',
+                when: (answers) => cli.input[0] === 'class' || answers.transformer === 'class',
+                message:
+              'replace react-addons-pure-render-mixin with React.PureComponent?',
+                default: true,
+            },
+            {
+                type: 'input',
+                name: 'classMixinModuleName',
+                when: (answers) => answers.classPureComponent === true,
+                // validate: () =>
+                message: 'What module exports this mixin?',
+                default: 'react-addons-pure-render-mixin',
+                filter: (x) => x.trim(),
+            },
+            // if transformer === 'pure-render-mixin'
+            {
+                type: 'input',
+                name: 'pureRenderMixinMixinName',
+                when: (answers) => (
+                    cli.input[0] === 'pure-render-mixin' ||
+                answers.transformer === 'pure-render-mixin'
+                ),
+                message: 'What is the name of the mixin?',
+                default: 'PureRenderMixin',
+                filter: (x) => x.trim(),
+            },
+            // if transformer === 'pure-component'
+            {
+                type: 'confirm',
+                name: 'pureComponentUseArrows',
+                when: (answers) => (
+                    cli.input[0] === 'pure-component' ||
+                answers.transformer === 'pure-component'
+                ),
+                message: 'Use arrow functions?',
+                default: false,
+            },
+            {
+                type: 'confirm',
+                name: 'pureComponentDestructuring',
+                when: (answers) => (
+                    cli.input[0] === 'pure-component' ||
+                answers.transformer === 'pure-component'
+                ),
+                message: 'Destructure props?',
+                default: false,
+            },
+            {
+                type: 'confirm',
+                name: 'destructureNamespaceImports',
+                when: (answers) => (
+                    cli.input[0] === 'update-react-imports' ||
+                answers.transformer === 'update-react-imports'
+                ),
+                message: 'Destructure namespace imports (import *) too?',
+                default: false,
+            }
+        ])
+        .then((answers) => {
+            const { files, transformer, parser } = answers;
+
+            const filesBeforeExpansion = cli.input[1] || files;
+            const filesExpanded = expandFilePathsIfNeeded([filesBeforeExpansion]);
+
+            const selectedTransformer = cli.input[0] || transformer;
+            const selectedParser = cli.flags.parser || parser;
+
+            if (!filesExpanded.length) {
+                console.log(`No files found matching ${filesBeforeExpansion.join(' ')}`);
+                return null;
+            }
+
+            return runTransform({
+                files: filesExpanded,
+                flags: cli.flags,
+                parser: selectedParser,
+                transformer: selectedTransformer,
+                answers,
+            });
+        });
+}
+
+// console.log(answer);
+
+// switch (answer) {
+//     case 'pie-design-tokens@v5.0.0':
+//         console.log('tokens');
+
+//         break;
+//     case 'pie-button@v2.0.0':
+//         console.log('button');
+//         // do some stuff
+//         break;
+//     default:
+//         break;
+// }
+
+export {
+    run,
+    runTransform,
+    checkGitStatus,
+    jscodeshiftExecutable,
+    transformerDirectory,
+};
---
+++ packages/tools/pie-codemods/codemod.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+// react-codemod optional-name-of-transform optional/path/to/src [...options]
+
+import { run } from './codemod-gui.js';
+
+run();
