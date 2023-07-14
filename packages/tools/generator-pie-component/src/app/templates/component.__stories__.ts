5af3ee7ebd1b3455c758d5f3529fdcf5959e3e1f
Raouf <raouf.sawehli@justeattakeaway.com>
2023-07-14 15:29:43 +0200

    refactor(generator-pie-component): add component reference to avoid treeshaking

--- packages/tools/generator-pie-component/src/app/templates/component.__stories__.ts
+++ packages/tools/generator-pie-component/src/app/templates/component.__stories__.ts
@@ -1,7 +1,10 @@
 import type { Meta, StoryObj as Story } from '@storybook/web-components';
-import { <%= componentName%>Props } from '@justeattakeaway/pie-<%= fileName %>';
+import { Pie<%= componentName%>, <%= componentName%>Props } from '@justeattakeaway/pie-<%= fileName %>';
 import { html, TemplateResult } from 'lit';
 
+// TODO: Remove this const when other exports from PieModal are used on Stories, 
+// otherwise tree-shaking will get rid of the web component definition
+const keptReference = Pie<%= componentName%>; 
 export default {
     title: '<%= readme %>',
     component: 'pie-<%= fileName %>',
