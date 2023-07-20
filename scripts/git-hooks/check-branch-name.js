5e1d3e10a5465f16001f040a51cba1a96e0b845b
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2023-07-20 10:13:19 +0200

    feat(pie-monorepo): DSW-801 update scripts structure

--- scripts/git-hooks/check-branch-name.js
+++ scripts/git-hooks/check-branch-name.js
@@ -1,18 +1,29 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { validateBranchName } = require('./git-hooks-utils.js');
 
-// This script is ran by the "pre-commit" git hook and checks if the current branch name has a Jira ticket id
+/**
+ * This script is ran by the "pre-commit" git hook and checks if the current branch name has a Jira ticket id
+ */
+function checkBranchName () {
+    try {
+        // This argument might be empty during a interactive rebase
+        if (!process.argv[2]) process.exit(0);
 
-// This argument might be empty during a interactive rebase
-if (!process.argv[2]) process.exit(0);
+        const branchName = process.argv[2].trim();
+        const isValid = validateBranchName(branchName);
 
-const branchName = process.argv[2].trim();
-const isValid = validateBranchName(branchName);
-
-if (!isValid) {
-    console.error(`
+        if (!isValid) {
+            const errorMessage = `
 The branch name "${branchName}" is not valid.
 It should either start with a Jira ticket id like "dsw-123-..." or "dsw-000-..." if it doesn't have a related ticket.
-`);
-    process.exit(1);
+`;
+            throw new Error(errorMessage);
+        }
+    } catch (error) {
+        console.error('checkBranchName() failed:');
+        console.error(error.message);
+        process.exit(1);
+    }
 }
+
+checkBranchName();
--- scripts/git-hooks/check-ticket-id.js
+++ scripts/git-hooks/check-ticket-id.js
@@ -2,27 +2,38 @@
 const fs = require('fs');
 const { getTicketIdFromBranchName, verifyCommitMessage } = require('./git-hooks-utils.js');
 
-// This script is ran by the "commit-msg" git hook and verifies if the commit message contains the expected Jira ticket id
+/**
+ * This script is ran by the "commit-msg" git hook and verifies if the commit message contains the expected Jira ticket id
+ */
+function checkTicketId () {
+    try {
+        // This argument might be empty during a interactive rebase
+        if (!process.argv[3]) process.exit(0);
 
-// This argument might be empty during a interactive rebase
-if (!process.argv[3]) process.exit(0);
+        const fileName = process.argv[2].trim();
+        const branchName = process.argv[3].trim();
 
-const fileName = process.argv[2].trim();
-const branchName = process.argv[3].trim();
+        // Check if the branch name has a Jira ticket
+        const ticketId = getTicketIdFromBranchName(branchName);
 
-// Check if the branch name has a Jira ticket
-const ticketId = getTicketIdFromBranchName(branchName);
+        if (!ticketId) {
+            const errorMessage = 'A proper Jira ticket id couldn\'t be inferred from the branch name.';
+            throw new Error(errorMessage);
+        }
 
-if (!ticketId) {
-    console.error('A proper Jira ticket id couldn\'t be inferred from the branch name.');
-    process.exit(1);
+        // Check if commit message has the expected Jira ticket id inferred from the branch name
+        const originalCommitMessage = fs.readFileSync(fileName, 'utf-8');
+        const expectedCommitMessage = verifyCommitMessage(originalCommitMessage, ticketId);
+
+        if (expectedCommitMessage !== originalCommitMessage) {
+            // Update commit message
+            fs.writeFileSync(fileName, expectedCommitMessage, 'utf-8');
+        }
+    } catch (error) {
+        console.error('checkTicketId() failed:');
+        console.error(error.message);
+        process.exit(1);
+    }
 }
 
-// Update commit message with the expected Jira ticket id
-const originalCommitMessage = fs.readFileSync(fileName, 'utf-8');
-const expectedCommitMessage = verifyCommitMessage(originalCommitMessage, ticketId);
-
-if (expectedCommitMessage !== originalCommitMessage) {
-    // Update commit message
-    fs.writeFileSync(fileName, expectedCommitMessage, 'utf-8');
-}
+checkTicketId();
