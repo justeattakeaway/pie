2fd1523e91726524ce97a1bb108bd1962e562a94
Jamie Maguire <jamieolivermaguire@googlemail.com>
2023-07-20 15:12:14 +0100

    chore(pie-icons-react): remove icons from source control (#633)
    * remove eslint file
    
    * chore(pie-icons-react): remove icons from source control
    
    * remove from src control

--- packages/tools/pie-icons-react/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    extends: [require.resolve('@justeattakeaway/eslint-config-pie/strict')],
-    rules: {
-        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    },
-};
