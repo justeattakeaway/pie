7ccec308f515d99abeb79a495f2176992bf91be5
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2023-07-27 16:08:50 +0100

    feat(pie-modal): DSW-729 added modal aria labels to close, back and loading e… (#652)
    * feat(pie-modal): added modal aria labels to close, back and loading elements
    
    * feat(pie-modal): DSW-729 pR comment
    
    * feat(pie-modal): DSW-729 pr comment
    
    * feat(pie-modal): DSW-729 pr comments
    
    * feat(pie-modal): DSW-729 pr comment
    
    * feat(pie-modal): DSW-729 pr comment
    
    * feat(pie-modal): DSW-729 pr comments
    
    * feat(pie-modal): DSW-729 pr comment

--- apps/pie-storybook/stories/pie-modal.stories.ts
+++ apps/pie-storybook/stories/pie-modal.stories.ts
@@ -41,6 +41,11 @@ const defaultArgs: ModalProps = {
         variant: 'ghost',
         ariaLabel: 'Descriptive cancellation text',
     },
+    aria: {
+        close: 'Close',
+        back: 'Back',
+        loading: 'Loading',
+    },
 };
 
 const modalStoryMeta: ModalStoryMeta = {
@@ -90,6 +95,9 @@ const modalStoryMeta: ModalStoryMeta = {
         supportingAction: {
             control: 'object',
         },
+        aria: {
+            control: 'object',
+        },
     },
     args: defaultArgs,
     parameters: {
@@ -134,6 +142,7 @@ const createFocusableElementsPageHTML = () : TemplateResult => html`
 
 const BaseStoryTemplate = (props: ModalProps): TemplateResult => {
     const {
+        aria,
         heading,
         headingLevel,
         isDismissible,
@@ -164,6 +173,7 @@ const BaseStoryTemplate = (props: ModalProps): TemplateResult => {
             size="${size}"
             .leadingAction="${leadingAction}"
             .supportingAction="${supportingAction}"
+            .aria="${aria}"
             position="${position}">
             ${slot}
         </pie-modal>`;
