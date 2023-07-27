716af9b90f9e6a0cdaca7ef75198403d2ac5bbd3
Jamie Maguire <jamieolivermaguire@googlemail.com>
2023-07-27 08:39:51 +0100

    chore(pie-monorepo): DSW-1022 add js linting to components (#653)

--- packages/components/pie-webc-core/src/mixins/rtl/rtlMixin.ts
+++ packages/components/pie-webc-core/src/mixins/rtl/rtlMixin.ts
@@ -3,6 +3,7 @@ import { LitElement } from 'lit';
 import { property } from 'lit/decorators/property.js';
 
 // According to TS, "A mixin class must have a constructor with a single rest parameter of type 'any[]'."
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Constructor<T> = new (...args: any[]) => T;
 
 type htmlDirAttribute = 'ltr' | 'rtl' | 'auto';
