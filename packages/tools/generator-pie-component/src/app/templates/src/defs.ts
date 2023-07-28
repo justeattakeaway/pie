9d8ee72e87337eef62285786ee4aad751bdad0ce
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-07-28 12:09:11 +0100

    add todo comments to lint disable rules

--- packages/tools/generator-pie-component/src/app/templates/src/defs.ts
+++ packages/tools/generator-pie-component/src/app/templates/src/defs.ts
@@ -1,2 +1,4 @@
+// TODO - please remove the eslint disable comment below when you add props to this interface
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface <%= componentName %>Props {}
+
