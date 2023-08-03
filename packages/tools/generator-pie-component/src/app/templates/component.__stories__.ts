67594e0eef0be41c7fdd090cfe6fd56d359b3619
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2023-08-03 14:57:16 +0100

    feat(pie-toggle-switch): DSW-991 revert generator changes

--- packages/tools/generator-pie-component/src/app/templates/component.__stories__.ts
+++ packages/tools/generator-pie-component/src/app/templates/component.__stories__.ts
@@ -1,10 +1,9 @@
 import { html, TemplateResult } from 'lit';
 import type { StoryObj as Story } from '@storybook/web-components';
-import '@justeattakeaway/pie-<%= fileName %>';
-import { <%= componentName %>Props } from '@justeattakeaway/pie-<%= fileName %>/src/defs';
+import { Pie<%= componentName %>, <%= componentName %>Props } from '@justeattakeaway/pie-<%= fileName %>';
 import { StoryMeta } from '../types';
 
-// TODO: Remove this const when other exports from Pie<%= componentName %> are used on Stories,
+// TODO: Remove this const when other exports from Pie<%= componentName %> are used on Stories, 
 // otherwise tree-shaking will get rid of the web component definition
 const keptReference = Pie<%= componentName %>;
 
@@ -31,7 +30,7 @@ const <%= componentNameCamelCase %>StoryMeta: <%= componentName %>StoryMeta = {
 export default <%= componentNameCamelCase %>StoryMeta;
 
 const Template = ({}: <%= componentName %>Props): TemplateResult => html`
-        <pie-<%= fileName %>><pie-<%= fileName %>/>
+        <pie-<%= fileName %>/>
         `;
 
 export const Default: Story<<%= componentName %>Props> = (args: <%= componentName %>Props) => Template(args);
