b56832c3846c5588439d1a754abb78f58a282d67
Raouf <raouf.sawehli@justeattakeaway.com>
2023-08-03 17:15:05 +0200

    feat(generator-pie-component): DSW-989 add testing starting point (#669)
    * feat(generator-pie-component): DSW-989 add testing templates for the pie component generator
    
    * feat(generator-pie-component): DSW-989 add testing templates for the pie component generator
    
    * feat(generator-pie-component): DSW-989 add testing templates for the pie component generator
    
    * fix(generator-pie-component): DSW-989 lint
    
    * fix(generator-pie-component): DSW-989 changeset typo
    
    * refactor(generator-pie-component): DSW-989 update testing starting point
    
    * refactor(generator-pie-component): DSW-989 update template file names to placeholder
    
    * refactor(pie-webc-testing): DSW-989 use a shared axe builder across accessbility tests
    
    * refactor(pie-webc-testing): DSW-989 disable cache
    
    * refactor(pie-webc-testing): DSW-989 enable cache
    
    * chore(pie-monorepo): DSW-989 update yarn lock
    
    * chore(pie-monorepo): DSW-989 update cw-vailla icons import

---
+++ .changeset/perfect-clocks-beg.md
@@ -0,0 +1,7 @@
+---
+"@justeattakeaway/generator-pie-component": minor
+"pie-storybook": minor
+---
+[Changed] - renamed the toggle switch story filename
+
+[Added] - testing starting templates for the generator-pie-component
---
+++ packages/tools/generator-pie-component/src/app/templates/test/accessibility/pie-placeholder.__spec__.ts
@@ -0,0 +1,18 @@
+
+import { test, expect } from '@justeattakeaway/pie-webc-testing/src/playwright/fixtures.ts';
+import { Pie<%= componentName %>, <%= componentName %>Props } from '@/index';
+
+test.describe('Pie<%= componentName %> - Accessibility tests', () => {
+    test('a11y - should test the Pie<%= componentName %> component WCAG compliance', async ({ makeAxeBuilder, mount }) => {
+        await mount(
+            Pie<%= componentName %>,
+            {
+                props: {} as <%= componentName %>Props,
+            },
+        );
+
+        const results = await makeAxeBuilder().analyze();
+
+        expect(results.violations).toEqual([]);
+    });
+});
