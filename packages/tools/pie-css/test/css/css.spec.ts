7c57d68422fa4e7442ce5425a25064671fdad981
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-08-03 14:53:26 +0100

    update pie-css tests

--- packages/tools/pie-css/test/css/css.spec.ts
+++ packages/tools/pie-css/test/css/css.spec.ts
@@ -1,6 +1,5 @@
-import * as fs from 'fs';
-import * as util from 'util';
-import * as path from 'path';
+import fs from 'fs/promises';
+import path from 'path';
 import cssValidator from 'w3c-css-validator';
 import {
     describe,
@@ -8,24 +7,13 @@ import {
     expect,
 } from 'vitest';
 
-const readFile = util.promisify(fs.readFile);
-
-async function getCssContent (filePath: string): Promise<string> {
-    try {
-        return readFile(filePath, 'utf8');
-    } catch (err) {
-        console.error(`Error reading file from path: ${filePath}`, err);
-        return '';
-    }
-}
-
 // Requires the CSS to have been built before running these tests
 const builtCssFilePath = path.join(__dirname, '../../dist/index.css');
 
 describe('index.css', () => {
     it('should not throw any unexpected W3C CSS validation errors', async () => {
         // Arrange
-        const css = await getCssContent(builtCssFilePath);
+        const css = await fs.readFile(builtCssFilePath, 'utf8');
 
         // text-rendering is not supported by W3C CSS validator however it is allowed to be set on HTML elements in various browsers
         // therefore we will allow for this error to be ignored
@@ -41,7 +29,7 @@ describe('index.css', () => {
 
     it('should render the expected CSS content', async () => {
         // Arrange
-        const css = await getCssContent(builtCssFilePath);
+        const css = await fs.readFile(builtCssFilePath, 'utf8');
 
         // Act & Assert
         expect(css).toMatchSnapshot();
