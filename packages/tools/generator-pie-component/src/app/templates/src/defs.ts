c204853d9209942804d12f6d03539196d3adb2de
Raouf <raouf.sawehli@justeattakeaway.com>
2023-08-04 16:51:05 +0200

    feat(generator-pie-component): DSW-1054 add rtl  support to the generator

--- packages/tools/generator-pie-component/src/app/templates/src/defs.ts
+++ packages/tools/generator-pie-component/src/app/templates/src/defs.ts
@@ -1,4 +1,6 @@
+<% if (needsRTL) { %>import { RTLComponentProps } from '@justeattakeaway/pie-webc-core';<% } %>
+
 // TODO - please remove the eslint disable comment below when you add props to this interface
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface <%= componentName %>Props {}
+export interface <%= componentName %>Props <% if (needsRTL) { %>extends RTLComponentProps<% } %> {}
 
