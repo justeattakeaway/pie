6ae14774691a6c939e2e2b7ac42657e22663e8b1
Raouf <raouf.sawehli@justeattakeaway.com>
2023-08-07 12:37:53 +0200

    feat(generator-pie-component): DSW-1054 add rtl  support to the gener… (#690)
    * feat(generator-pie-component): DSW-1054 add rtl  support to the generator
    
    * feat(generator-pie-component): DSW-1054 add rtl  support to the generator
    
    * feat(generator-pie-component): DSW-1054 fix format
    
    * feat(generator-pie-component): DSW-1054 fix format
    
    * fix(generator-pie-component): DSW-1054 update storybook configs

--- packages/components/pie-webc-testing/src/playwright/fixtures.ts
+++ packages/components/pie-webc-testing/src/playwright/fixtures.ts
@@ -13,8 +13,8 @@ interface ExtendedTestContext {
 export const test = baseTest.extend<ExtendedTestContext>({
     makeAxeBuilder: [async ({ page }: { page: Page }, use: (builder: () => AxeBuilder) => Promise<void>) => {
         const makeAxeBuilder = () => new AxeBuilder({ page })
-        .withTags(['wcag21a', 'wcag21aa', 'wcag143', 'cat.color', 'cat.aria'])
-        .disableRules(['color-contrast-enhanced']);
+          .withTags(['wcag21a', 'wcag21aa', 'wcag143', 'cat.color', 'cat.aria'])
+          .disableRules(['color-contrast-enhanced']);
 
         await use(makeAxeBuilder);
     }, {
