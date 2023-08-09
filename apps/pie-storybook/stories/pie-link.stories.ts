4e1f9e530f05bb88e7c32647fb2b7cc35eaedc74
Raouf <raouf.sawehli@justeattakeaway.com>
2023-08-09 11:01:06 +0200

    feat(DSW-1056): DSW-1056 added basic functionality of pie link

--- apps/pie-storybook/stories/pie-link.stories.ts
+++ apps/pie-storybook/stories/pie-link.stories.ts
@@ -1,23 +1,83 @@
 import { html, TemplateResult } from 'lit';
 import { type StoryObj as Story } from '@storybook/web-components';
-import { PieLink, LinkProps } from '@justeattakeaway/pie-link';
-import { type StoryMeta } from '../types';
+import {
+    PieLink, LinkProps as LinkBaseProps, sizes, variants,
+} from '@justeattakeaway/pie-link';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import type { StoryMeta, SlottedComponentProps } from '../types';
 
 // This prevents storybook from tree shaking the components
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const keptReferences = [PieLink];
 
+type LinkProps = SlottedComponentProps<LinkBaseProps>;
 type LinkStoryMeta = StoryMeta<LinkProps>;
 
 const defaultArgs: LinkProps = {
-
+    variant: 'default',
+    size: 'medium',
+    href: 'https://pie.design',
+    isBold: false,
+    isStandalone: false,
+    slot: 'This is Lit!',
 };
 
 const linkStoryMeta: LinkStoryMeta = {
     title: 'Link',
     component: 'pie-link',
     argTypes: {
-
+        variant: {
+            description: 'Set the variant of the link.',
+            control: 'select',
+            options: variants,
+            defaultValue: {
+                summary: 'default',
+            },
+        },
+        size: {
+            description: 'Set the size of the link.',
+            control: 'select',
+            options: sizes,
+            defaultValue: {
+                summary: 'medium',
+            },
+        },
+        href: {
+            description: 'What the URL that the hyperlink should point to',
+            control: 'text',
+            defaultValue: {
+                summary: 'https://pie.design',
+            },
+        },
+        target: {
+            description: 'Set where to display the linked URL',
+            control: 'text',
+        },
+        rel: {
+            description: 'Set what the relationship of the linked URL is',
+            control: 'text',
+        },
+        isBold: {
+            description: 'If `true`, makes the link text bold',
+            control: 'boolean',
+            defaultValue: {
+                summary: false,
+            },
+        },
+        isStandalone: {
+            description: 'If `true`, the link will be treated as a block box',
+            control: 'boolean',
+            defaultValue: {
+                summary: false,
+            },
+        },
+        slot: {
+            description: 'The default slot is used to pass the button text into the component.',
+            control: 'text',
+            defaultValue: {
+                summary: '',
+            },
+        },
     },
     args: defaultArgs,
     parameters: {
@@ -30,13 +90,40 @@ const linkStoryMeta: LinkStoryMeta = {
 
 export default linkStoryMeta;
 
-// TODO: remove the eslint-disable rule when props are added
-// eslint-disable-next-line no-empty-pattern
-const Template = ({ }: LinkProps): TemplateResult => html`
-  <pie-link></pie-link>
-`;
+const Template = ({
+    href, target, rel, size, variant, isBold, isStandalone, slot,
+}: LinkProps): TemplateResult => html`
+        <pie-link
+            variant="${variant}"
+            size="${size}"
+            href=${ifDefined(href)}
+            target=${ifDefined(target)}
+            rel=${ifDefined(rel)}
+            ?isBold="${isBold}"
+            ?isStandalone="${isStandalone}">
+            ${slot}
+        </pie-link>
+        `;
 
 export const Default: Story<LinkProps> = (args: LinkProps) => Template(args);
 Default.args = {
     ...defaultArgs,
+};
+
+export const HighVisibility: Story<LinkProps> = (args: LinkProps) => Template(args);
+HighVisibility.args = {
+    ...defaultArgs,
+    variant: 'high-visibility',
+};
+
+export const Inverse: Story<LinkProps> = (args: LinkProps) => Template(args);
+Inverse.args = {
+    ...defaultArgs,
+    variant: 'inverse',
+};
+
+Inverse.parameters = {
+    backgrounds: {
+        default: 'dark',
+    },
 };
--- packages/components/pie-link/src/index.ts
+++ packages/components/pie-link/src/index.ts
@@ -1,18 +1,61 @@
 import { LitElement, html, unsafeCSS } from 'lit';
-
+import { property } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { validPropertyValues } from '@justeattakeaway/pie-webc-core';
 import styles from './link.scss?inline';
-import { LinkProps } from './defs';
+import { LinkProps, variants, sizes } from './defs';
 
 // Valid values available to consumers
 export {
     type LinkProps,
+    variants,
+    sizes,
 };
 
 const componentSelector = 'pie-link';
 
 export class PieLink extends LitElement implements LinkProps {
+    @property()
+    @validPropertyValues(componentSelector, variants, 'default')
+    public variant: LinkProps['variant'] = 'default';
+
+    @property()
+    @validPropertyValues(componentSelector, sizes, 'medium')
+    public size: LinkProps['size'] = 'medium';
+
+    @property({ type: String, reflect: true })
+    public href?: string;
+
+    @property({ type: String, reflect: true })
+    public target?: string;
+
+    @property({ type: String, reflect: true })
+    public rel?: string;
+
+    @property({ type: Boolean })
+    public isBold = false;
+
+    @property({ type: Boolean })
+    public isStandalone = false;
+
     render () {
-        return html`<h1 data-test-id="pie-link">Hello world!</h1>`;
+        const {
+            variant, size, href, target, rel, isBold, isStandalone,
+        } = this;
+
+        return html`
+            <a  
+                data-test-id="pie-link"
+                class="c-link"
+                variant=${variant}
+                size=${size}
+                href=${ifDefined(href)}
+                target=${ifDefined(target)}
+                rel=${ifDefined(rel)}
+                ?isBold=${isBold}
+                ?isStandalone=${isStandalone}>
+                <slot></slot>
+            </a>`;
     }
 
     // Renders a `CSSResult` generated from SCSS by Vite
