050faa12171ed1939d5d5225a40def28895e2754
Raouf <raouf.sawehli@justeattakeaway.com>
2023-08-09 17:55:35 +0200

    feat(pie-divider): DSW-1012 add a pie divider component (#704)
    * feat(DSW-1012): DSW-1012 add a pie divider component
    
    * feat(DSW-1012): DSW-1012 fix style lints
    
    * fix(DSW-1012): DSW-1012 update package.json
    
    * fix(DSW-1012): DSW-1012 use relative imports to extend tsconfig
    
    * feat(pie-divider): DSW-1012 address PR comments

---
+++ .changeset/tender-lemons-sleep.md
@@ -0,0 +1,7 @@
+---
+"@justeattakeaway/generator-pie-component": minor
+"@justeattakeaway/pie-toggle-switch": minor
+"@justeattakeaway/pie-divider": minor
+---
+
+[Changed] extend ts config using relative paths
--- packages/components/pie-divider/src/defs.ts
+++ packages/components/pie-divider/src/defs.ts
@@ -1,3 +1,13 @@
-// TODO - please remove the eslint disable comment below when you add props to this interface
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface DividerProps {}
+export const variants = ['default', 'inverse'] as const;
+export const orientations = ['horizontal', 'vertical'] as const;
+
+export interface DividerProps {
+   /**
+     * What style variant the divider should be such as default or vertical.
+     */
+   variant: typeof variants[number];
+    /**
+     * What orientation the divider should be such as horizontal or inverse.
+     */
+   orientation: typeof orientations[number];
+}
