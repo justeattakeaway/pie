9cbdd9dd89478f62536fa39389b4361cb6875770
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-08-22 11:13:10 +0100

    ci(pie-monorepo): DSW-1074 syntax fix - test

--- packages/components/pie-button/declaration.d.ts
+++ packages/components/pie-button/declaration.d.ts
@@ -2,6 +2,7 @@ declare module '*.scss' {
     const content: Record<string, string>;
     export default content;
 }
+// dummy change
 
 declare module '*.scss?inline' {
     const content: Record<string, string>;
