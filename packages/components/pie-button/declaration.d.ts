a6a6c9893f5ccfce1c15a41a67083b738f3433b3
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-08-22 14:49:41 +0100

    ci(pie-monorepo): DSW-1074 dummy change for pie-storybook

--- packages/components/pie-button/declaration.d.ts
+++ packages/components/pie-button/declaration.d.ts
@@ -2,7 +2,6 @@ declare module '*.scss' {
     const content: Record<string, string>;
     export default content;
 }
-// dummy change
 
 declare module '*.scss?inline' {
     const content: Record<string, string>;
