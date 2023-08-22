de229f2e0cfbb518146511ffcdbd7ff1b2261241
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-08-22 11:54:56 +0100

    lint issues

--- apps/pie-storybook/stories/pie-cookie-banner.stories.ts
+++ apps/pie-storybook/stories/pie-cookie-banner.stories.ts
@@ -3,7 +3,6 @@ import { type StoryObj as Story } from '@storybook/web-components';
 import { PieCookieBanner, CookieBannerProps } from '@justeattakeaway/pie-cookie-banner';
 import { type StoryMeta } from '../types';
 
-
 // This prevents storybook from tree shaking the components
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const keptReferences = [PieCookieBanner];
@@ -11,14 +10,14 @@ const keptReferences = [PieCookieBanner];
 type CookieBannerStoryMeta = StoryMeta<CookieBannerProps>;
 
 const defaultArgs: CookieBannerProps = {
-    
+
 };
 
 const cookieBannerStoryMeta: CookieBannerStoryMeta = {
     title: 'Cookie Banner',
     component: 'pie-cookie-banner',
     argTypes: {
-    
+
     },
     args: defaultArgs,
     parameters: {
@@ -33,8 +32,8 @@ export default cookieBannerStoryMeta;
 
 // TODO: remove the eslint-disable rule when props are added
 // eslint-disable-next-line no-empty-pattern
-const Template = ({  }: CookieBannerProps): TemplateResult => html`
-  <pie-cookie-banner></pie-cookie-banner>
+const Template = ({ }: CookieBannerProps): TemplateResult => html`
+    <pie-cookie-banner></pie-cookie-banner>
 `;
 
 export const Default: Story<CookieBannerProps> = (args: CookieBannerProps) => Template(args);
