b0cd2f0eb2de4449e4320373faba8fe2a9998c0d
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-08-22 15:22:52 +0100

    add responsive positioning

--- apps/pie-storybook/stories/pie-cookie-banner.stories.ts
+++ apps/pie-storybook/stories/pie-cookie-banner.stories.ts
@@ -1,4 +1,4 @@
-import { html, TemplateResult } from 'lit';
+import { html, TemplateResult, nothing } from 'lit';
 import { type StoryObj as Story } from '@storybook/web-components';
 import { PieCookieBanner, CookieBannerProps } from '@justeattakeaway/pie-cookie-banner';
 import { PieButton } from '@justeattakeaway/pie-button';
@@ -32,10 +32,38 @@ const cookieBannerStoryMeta: CookieBannerStoryMeta = {
             type: 'figma',
             url: '',
         },
+        backgrounds: {
+            default: 'dark',
+        },
     },
 };
 
 export default cookieBannerStoryMeta;
+
+const BaseStoryTemplate = (props: CookieBannerProps) : TemplateResult => html`
+        <pie-cookie-banner></pie-cookie-banner>`;
+
+/**
+ * Creates a 'page' of scrollable HTML. Useful for testing scroll behaviours in a Story.
+ * @param inverseColors Sets the font colors to light (use when the Story background is dark)
+ * @returns
+ */
+const createScrollablePageHTML = (inverseColors = false) => {
+    const items = [];
+    for (let i = 0; i < 200; i++) {
+        items.push(html`<li>Item ${i}</li>`);
+    }
+
+    return html`
+        ${inverseColors ? html`<style>
+            h1, p, ul {
+                color: var(--dt-color-content-light);
+            }
+        </style>` : nothing}
+        <h1>Test Page</h1>
+        <p>Test copy</p>
+        <ul>${items}</ul>`;
+};
 
 // TODO: remove the eslint-disable rule when props are added
 // eslint-disable-next-line no-empty-pattern
@@ -47,3 +75,10 @@ export const Default: Story<CookieBannerProps> = (args: CookieBannerProps) => Te
 Default.args = {
     ...defaultArgs,
 };
+
+const ScrollablePageStoryTemplate = (props: CookieBannerProps) : TemplateResult => html`
+    ${BaseStoryTemplate(props)}
+    ${createScrollablePageHTML(true)}`;
+
+export const ScrollablePage: Story<CookieBannerProps> = (args: CookieBannerProps) => ScrollablePageStoryTemplate(args);
+ScrollablePage.args = defaultArgs;
