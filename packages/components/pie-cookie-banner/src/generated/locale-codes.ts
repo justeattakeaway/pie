90ec9c7bec3d7e3948600fd9dc726e28c08d5e99
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2023-09-14 17:42:12 +0200

    DSW-1195: Add locale codes

---
+++ packages/components/pie-cookie-banner/src/generated/locale-codes.ts
@@ -0,0 +1,18 @@
+/**
+ * The locale code that templates in this source code are written in.
+ */
+export const sourceLocale = `en`;
+
+/**
+ * The other locale codes that this application is localized into.
+ */
+export const targetLocales = [
+  'bg-BG',
+  'da-DK',
+  'de-DE',
+  'es-ES',
+  'fr-FR',
+  'it-IT',
+  'nl-NL',
+  'pl-PL'
+] as const;
