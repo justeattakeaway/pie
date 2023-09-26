9b5468f820ea3511df020e904da9a6f7558e1db0
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-09-26 12:28:56 +0100

    cosmetic(pie-css): DSW-1194 reusable focus styles

---
+++ packages/tools/pie-css/test/scss/unit/mixins/focus.spec.ts
@@ -0,0 +1,35 @@
+import {
+    describe,
+    it,
+    expect,
+} from 'vitest';
+
+import { compileCss, stripCSSWhitespace } from '../../../../utilities/compileCss';
+
+describe('mixins.focus', () => {
+    it('should render the expected CSS content', async () => {
+        // Arrange
+        const scssToTest = `
+          @use 'mixins';
+
+          .foo {
+            &:focus-visible {
+              @include mixins.focus();
+            }
+          }
+        `;
+
+        const expectedCss = stripCSSWhitespace(`
+          .foo:focus-visible {
+            box-shadow: 0 0 0 2px var(--dt-color-focus-inner), 0 0 0 4px var(--dt-color-focus-outer);
+          }
+        `);
+
+        // Act
+        const css = stripCSSWhitespace(compileCss(scssToTest));
+
+        // Assert
+        expect(css).toBe(expectedCss);
+    });
+});
+
