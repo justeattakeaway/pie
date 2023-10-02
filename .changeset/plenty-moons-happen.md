ddf62c423bf94dc0827ceefb471454cb512259c0
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2023-10-02 10:16:51 +0100

    feat(browserslist-config-pie): DSW-31 add initial browserslist package and config (#841)
    * Align body-scroll-lock versions to meet peer requirements
    
    * feat(browserslist-config-pie): DSW-31 add initial browserslist package and config
    
    * Remove duplicate CODE_OF_CONDUCT.md files
    
    * Update project labeler

---
+++ .changeset/plenty-moons-happen.md
@@ -0,0 +1,7 @@
+---
+"wc-vanilla": patch
+"pie-storybook": patch
+"pie-monorepo": patch
+---
+
+[Changed] - Align body-scroll-lock versions to meet peer requirements
---
+++ packages/tools/browserslist-config-pie/index.js
@@ -0,0 +1,9 @@
+module.exports = [
+    'last 3 Chrome versions',
+    'last 3 ChromeAndroid versions',
+    'last 4 iOS major versions',
+    'last 4 Safari major versions',
+    'last 4 Firefox versions',
+    'last 4 Edge versions',
+    'last 3 Samsung versions',
+];
