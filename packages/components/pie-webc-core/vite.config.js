7d6d6e0fc3f4f50f6c0f5e806a94d0eba0f4d46f
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-10-05 17:46:17 +0100

    add build step and make lib treeshakeable

--- packages/components/pie-webc-core/vite.config.js
+++ packages/components/pie-webc-core/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
-export default defineConfig({
-    test: {
-        environment: 'jsdom',
+// https://vitejs.dev/config/
+const sharedConfig = (extendedConfig = {}) => defineConfig({
+    build: {
+        lib: {
+            entry: {
+                index: path.resolve(__dirname, 'src/index.ts'),
+                'mixins/rtl/rtlMixin': path.resolve(__dirname, 'src/mixins/rtl/rtlMixin.ts'),
+                'decorators/required-property': path.resolve(__dirname, 'src/decorators/required-property.ts'),
+                'decorators/valid-property-values': path.resolve(__dirname, 'src/decorators/valid-property-values.ts'),
+            },
+            formats: ['es'],
+        },
+        rollupOptions: {
+            external: ['react', /^lit/],
+        },
     },
+    ...extendedConfig,
 });
+
+export default sharedConfig;
