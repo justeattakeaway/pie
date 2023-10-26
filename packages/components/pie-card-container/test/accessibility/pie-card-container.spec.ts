d0e56baa5d75791f681ee7db7a17431ce7592bb7
Raouf <raouf.sawehli@justeattakeaway.com>
2023-10-26 15:23:10 +0200

    fix(pie-card-container): DSW-1289 add more accessibility tests

--- packages/components/pie-card-container/test/accessibility/pie-card-container.spec.ts
+++ packages/components/pie-card-container/test/accessibility/pie-card-container.spec.ts
@@ -3,11 +3,31 @@ import { test, expect } from '@justeattakeaway/pie-webc-testing/src/playwright/f
 import { PieCardContainer, CardContainerProps } from '@/index';
 
 test.describe('PieCardContainer - Accessibility tests', () => {
+    test('a11y - should test the PieCardContainer component WCAG compliance if tag is "button"', async ({ makeAxeBuilder, mount }) => {
+        await mount(
+            PieCardContainer,
+            {
+                props: {
+                    tag: 'button',
+                    aria: {
+                        label: 'card-container',
+                    },
+                } as CardContainerProps,
+            },
+        );
+
+        const results = await makeAxeBuilder().analyze();
+
+        expect(results.violations).toEqual([]);
+    });
+
     test('a11y - should test the PieCardContainer component WCAG compliance', async ({ makeAxeBuilder, mount }) => {
         await mount(
             PieCardContainer,
             {
-                props: {} as CardContainerProps,
+                props: {
+                    tag: 'a',
+                } as CardContainerProps,
             },
         );
 
