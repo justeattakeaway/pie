7f175a65540418b6ee0879a61e65fd65b9dc2119
FayeCarter <faye.carter@justeattakeaway.com>
2023-11-06 14:34:01 +0000

    feat(pie-docs): DSW-1324 add column component (#961)
    * feat(pie-docs): DSW-000 add component template
    
    * feat(pie-docs): DSW-000 add draft
    
    * feat(pie-docs): DSW-1324 add column component
    
    * feat(pie-docs): DSW-1324 add changelog
    
    * feat(pie-docs): DSW-1324 lint
    
    * feat(pie-docs): DSW-1324 routes
    
    * feat(pie-docs): DSW-1324 update margin
    
    * feat(pie-docs): DSW-1324 update layout
    
    * feat(pie-docs): DSW-1324 remove components for design review
    
    * feat(pie-docs): DSW-1324 generate routes
    
    * feat(pie-docs): DSW-1324 correct spelling mistakes

--- apps/pie-docs/src/_11ty/shortcodes/index.js
+++ apps/pie-docs/src/_11ty/shortcodes/index.js
@@ -1,6 +1,7 @@
 const card = require('./card');
 const categorisedIconList = require('./categorisedIconList');
 const contentPageImage = require('./contentPageImage');
+const { contentLayout, contentItem } = require('./contentLayout');
 const globalTokensWarning = require('./notifications/globalTokensWarning');
 const link = require('./link');
 const list = require('./list');
@@ -21,6 +22,8 @@ const addAllShortCodes = (eleventyConfig) => {
     eleventyConfig.addShortcode('card', (shortcodeArgs) => deindentHTML(card(shortcodeArgs)));
     eleventyConfig.addShortcode('categorisedIconList', (shortcodeArgs) => deindentHTML(categorisedIconList(shortcodeArgs)));
     eleventyConfig.addShortcode('contentPageImage', (shortcodeArgs) => deindentHTML(contentPageImage(shortcodeArgs)));
+    eleventyConfig.addPairedShortcode('contentLayout', (shortcodeArgs) => deindentHTML(contentLayout(shortcodeArgs)));
+    eleventyConfig.addPairedShortcode('contentItem', (shortcodeArgs) => deindentHTML(contentItem(shortcodeArgs)));
     eleventyConfig.addShortcode('globalTokensWarning', (shortcodeArgs) => deindentHTML(globalTokensWarning(shortcodeArgs)));
     eleventyConfig.addShortcode('link', (shortcodeArgs) => deindentHTML(link(shortcodeArgs)));
     eleventyConfig.addShortcode('list', (shortcodeArgs) => deindentHTML(list(shortcodeArgs)));
