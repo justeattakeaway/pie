686eb5a6b69fe8a9c9fd0b32f6aa43cf25122f41
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-11-07 15:48:29 +0000

    feat(pie-monorepo): DSW-000 get all example apps (except next13) working with Lit 3

--- apps/examples/wc-next10/next.config.js
+++ apps/examples/wc-next10/next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const transpileModules = [
     '@justeattakeaway/pie-button',
     '@lit-labs/react',
@@ -7,4 +9,29 @@ const transpileModules = [
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withTM = require('next-transpile-modules')(transpileModules);
 
-module.exports = withTM();
+const nextConfig = {
+    // Required for Lit 3 support with Webpack 4 - https://lit.dev/docs/releases/upgrade/#using-lit-3-with-webpack-4
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.js$/,
+            include: ['@lit', 'lit-element', 'lit-html'].map((p) => path.resolve(__dirname, `../../../node_modules/${p}`)),
+            use: [
+                options.defaultLoaders.babel,
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            '@babel/plugin-transform-optional-chaining',
+                            '@babel/plugin-transform-nullish-coalescing-operator',
+                            '@babel/plugin-transform-logical-assignment-operators'
+                        ],
+                    },
+                },
+            ],
+        });
+
+        return config;
+    },
+};
+
+module.exports = withTM(nextConfig);
