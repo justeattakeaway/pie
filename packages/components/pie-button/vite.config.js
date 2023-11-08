bdb25dd9515f80a86ecd78541466faa8e34b6c41
Raouf <raouf.sawehli@justeattakeaway.com>
2023-11-08 11:06:45 +0100

    fix(pie-button): DSW-1430 update vite.config

--- packages/components/pie-button/vite.config.js
+++ packages/components/pie-button/vite.config.js
@@ -1,3 +1,20 @@
 import viteConfig from '@justeattakeaway/pie-components-config/vite.config';
 
-export default viteConfig;
+export default viteConfig({
+    build: {
+        lib: {
+            entry: {
+                index: 'src/index.ts',
+                react: 'src/react.ts',
+            },
+            formats: ['es'],
+        },
+        rollupOptions: {
+            external: [
+                'react',
+                /^lit/,
+                /@justeattakeaway/
+            ],
+        },
+    },
+});
