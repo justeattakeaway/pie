f19e6850f9e5c7e8098e533818389d62f3a3e899
Raouf <raouf.sawehli@justeattakeaway.com>
2023-11-08 12:40:57 +0100

    fix(pie-button): DSW-1430 revert some changes

--- .changeset/three-dots-prove.md
+++
@@ -1,9 +0,0 @@
----
-"@justeattakeaway/pie-cookie-banner": minor
-"@justeattakeaway/pie-button": minor
-"@justeattakeaway/pie-components-config": minor
-"@justeattakeaway/pie-modal": minor
-"pie-monorepo": minor
----
-
-[Changed] - use the shared vite config file
--- configs/pie-components-config/vite.config.js
+++ configs/pie-components-config/vite.config.js
@@ -12,7 +12,7 @@ const sharedConfig = (extendedConfig = {}) => defineConfig({
             formats: ['es'],
         },
         rollupOptions: {
-            external: ['react', /^lit/, /@justeattakeaway/],
+            external: ['react', /^lit/],
         },
     },
     plugins: [dts({
