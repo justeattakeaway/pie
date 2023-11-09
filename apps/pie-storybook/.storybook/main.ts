26341a2d39e003a219bdf147bc9eeecab0f12695
Sébastien Crapoulet <2960408+sebastienrocks@users.noreply.github.com>
2023-11-09 13:43:40 +0000

    feat(pie-storybook): DSW-000 add dark mode (#951)
    Co-authored-by: Sébastien Crapoulet <sebastien.crapoulet@justeattakeaway.com>

--- apps/pie-storybook/.storybook/main.ts
+++ apps/pie-storybook/.storybook/main.ts
@@ -11,6 +11,7 @@ const config: StorybookConfig = {
         "@storybook/addon-designs",
         "@storybook/addon-essentials",
         "@storybook/addon-links",
+        "storybook-dark-mode",
     ],
     framework: {
         name: "@storybook/web-components-vite",
