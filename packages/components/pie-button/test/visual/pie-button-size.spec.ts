49f7365a9252eed71d7e2a36f3a48b0452a483f7
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2023-11-15 09:31:27 +0100

    feat(pie-button): DSW-1293 implement responsive mode (#986)
    * feat(pie-button): DSW-1293 add isResponsive prop
    
    * feat(pie-button): DSW-1293 add browser tests
    
    * feat(pie-button): DSW-1293 update visual tests
    
    * feat(pie-button): DSW-1293 update docs
    
    * feat(pie-button): DSW-1293 code review updates
    
    * feat(pie-button): DSW-1293 code review updates
    
    * feat(pie-button): DSW-1293 code review updates
    
    * feat(pie-button): DSW-1293 code review updates
    
    Co-authored-by: Xander Marjoram <xander.marjoram@justeattakeaway.com>
    
    * feat(pie-button): DSW-1293 code review updates
    
    Co-authored-by: Xander Marjoram <xander.marjoram@justeattakeaway.com>
    
    * feat(pie-button): DSW-1293 code review updates
    
    * chore(pie-button): DSW-1293 fix tests
    
    * chore(pie-button): DSW-1293 extract responsize size tests into a dedicated test
    
    * feat(pie-button): DSW-1293 code review updates
    
    * chore(pie-button): DSW-1293 avoid extra vrt changes
    
    ---------
    
    Co-authored-by: Xander Marjoram <xander.marjoram@justeattakeaway.com>
    Co-authored-by: Ben Siggery <ben.siggery@justeattakeaway.com>

---
+++ packages/components/pie-button/test/visual/pie-button-size.spec.ts
@@ -0,0 +1,52 @@
+import { test } from '@sand4rt/experimental-ct-web';
+import percySnapshot from '@percy/playwright';
+import type {
+    PropObject, WebComponentPropValues, WebComponentTestInput,
+} from '@justeattakeaway/pie-webc-testing/src/helpers/defs.ts';
+import {
+    getAllPropCombinations,
+} from '@justeattakeaway/pie-webc-testing/src/helpers/get-all-prop-combos.ts';
+import {
+    createTestWebComponent,
+} from '@justeattakeaway/pie-webc-testing/src/helpers/rendering.ts';
+import {
+    WebComponentTestWrapper,
+} from '@justeattakeaway/pie-webc-testing/src/helpers/components/web-component-test-wrapper/WebComponentTestWrapper.ts';
+import { percyWidths } from '@justeattakeaway/pie-webc-testing/src/percy/breakpoints.ts';
+import { sizes } from '@/defs';
+
+const props: PropObject = {
+    variant: ['primary'],
+    size: sizes,
+    isResponsive: [true, false],
+    responsiveSize: ['productive', 'expressive'],
+};
+
+// Renders a <pie-button> HTML string with the given prop values
+const renderTestPieButton = (propVals: WebComponentPropValues) => `<pie-button size="${propVals.size}" ${propVals.isResponsive ? 'isResponsive' : ''} responsiveSize="${propVals.responsiveSize ? propVals.responsiveSize : ''}">Hello world</pie-button>`;
+
+const componentPropsMatrix : WebComponentPropValues[] = getAllPropCombinations(props);
+
+// eslint-disable-next-line no-empty-pattern
+test.beforeEach(async ({ }, testInfo) => {
+    testInfo.setTimeout(testInfo.timeout + 40000);
+});
+
+test('should render all size variations', async ({ page, mount }) => {
+    await Promise.all(componentPropsMatrix.map(async (combo: WebComponentPropValues) => {
+        const testComponent: WebComponentTestInput = createTestWebComponent(combo, renderTestPieButton);
+        const propKeyValues = `size: ${testComponent.propValues.size}, isResponsive: ${testComponent.propValues.isResponsive}, responsiveSize: ${testComponent.propValues.responsiveSize}`;
+
+        await mount(
+            WebComponentTestWrapper,
+            {
+                props: { propKeyValues },
+                slots: {
+                    component: testComponent.renderedString.trim(),
+                },
+            },
+        );
+    }));
+
+    await percySnapshot(page, 'PIE Button - sizes/isResponsive/responsiveSize', percyWidths);
+});
