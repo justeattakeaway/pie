3b665d0f33fd85200f0a9f2893e035bd6654fcc0
Raouf <raouf.sawehli@justeattakeaway.com>
2023-11-21 16:06:45 +0100

    fix(pie-monorepo): DSW-000 fix react app

--- apps/examples/wc-react17/src/index.js
+++ apps/examples/wc-react17/src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
-render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
