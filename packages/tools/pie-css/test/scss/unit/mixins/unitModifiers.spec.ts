2c9f04e1e36cfa4c41716ef9eb4f54905b46072c
Ashley Watson-Nolan <ashley.watson-nolan@justeattakeaway.com>
2023-11-24 12:53:47 +0000

    feat(pie-css): DSW-1166 scss mixins and functions for line height (#1048)
    * feat(pie-css): DSW-1166 scss mixins and functions for line height
    
    * feat(pie-css): DSW-1166 remove async from css tests

--- packages/tools/pie-css/test/scss/unit/mixins/unitModifiers.spec.ts
+++ packages/tools/pie-css/test/scss/unit/mixins/unitModifiers.spec.ts
@@ -7,7 +7,7 @@ import {
 import { compileCss, stripCSSWhitespace } from '../../../../utilities/compileCss';
 
 describe('mixins.font-size', () => {
-    it('should render the expected CSS content', async () => {
+    it('should render the expected CSS content', () => {
         // Arrange
         const scssToTest = `
           @use 'mixins';
@@ -39,3 +39,35 @@ describe('mixins.font-size', () => {
     });
 });
 
+describe('mixins.line-height', () => {
+    it('should render the expected CSS content', () => {
+        // Arrange
+        const scssToTest = `
+          @use 'mixins';
+
+          :root {
+            --line-height: 12;
+          }
+
+          .foo {
+            @include mixins.line-height(--line-height);
+          }
+        `;
+
+        const expectedCss = stripCSSWhitespace(`
+          :root {
+            --line-height: 12;
+          }
+
+          .foo {
+            line-height: calc(var(--line-height) * 1px);
+          }
+        `);
+
+        // Act
+        const css = stripCSSWhitespace(compileCss(scssToTest));
+
+        // Assert
+        expect(css).toBe(expectedCss);
+    });
+});
