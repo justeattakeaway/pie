37f2ccb42ff3748d8659d222104d1647cf387687
personal <jamieolivermaguire@googlemail.com>
2023-11-28 16:36:32 +0000

    update root eslint and gitignore

--- packages/tools/pie-icons-webc/playwright-react.config.ts
+++ packages/tools/pie-icons-webc/playwright-react.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: '50%',
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [['html', { outputFolder: 'lit-browsers-report' }]],
+    reporter: [['html', { outputFolder: 'react-browsers-report' }]],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         ctViteConfig: {
