104c3adcab8d69b1b002c1985ed5ed48b044041a
Jose De Freitas Jr <jose.defreitasjr@justeattakeaway.com>
2023-12-05 10:39:23 -0700

    feat(pie-notification): DSW-1348 creating classes and placeholders

--- packages/components/pie-notification/src/defs.ts
+++ packages/components/pie-notification/src/defs.ts
@@ -1,3 +1,40 @@
-// TODO - please remove the eslint disable comment below when you add props to this interface
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface NotificationProps {}
+import { Variant as PieButtonVariant } from '@justeattakeaway/pie-button/src/defs.ts';
+
+export const variants = ['neutral', 'neutral-alternative', 'info', 'positive', 'warning', 'error'] as const;
+
+export type ActionProps = {
+  /**
+   * The text to display inside the button.
+   */
+  text: string;
+
+  /**
+   * The button variant.
+   */
+  variant?: PieButtonVariant;
+
+  /**
+   * The ARIA label for the button.
+   */
+  ariaLabel?: string;
+};
+
+export interface NotificationProps {
+  // variant: typeof variants;
+  isCompact?: boolean;
+  // icon // there is a default icon
+  // hideIcon
+
+  /**
+   * When true, the notification will be open.
+   */
+  isOpen: boolean;
+}
+
+// heading {
+//   headingLevel
+// }
+
+// leadingAction
+
+// supportingAction
--- packages/components/pie-notification/src/index.ts
+++ packages/components/pie-notification/src/index.ts
@@ -1,6 +1,9 @@
 import { LitElement, html, unsafeCSS } from 'lit';
 
 import { defineCustomElement } from '@justeattakeaway/pie-webc-core';
+import { property } from 'lit/decorators.js';
+import '@justeattakeaway/pie-icons-webc/IconClose';
+
 import styles from './notification.scss?inline';
 import { NotificationProps } from './defs';
 
@@ -8,13 +11,34 @@ import { NotificationProps } from './defs';
 export * from './defs';
 
 const componentSelector = 'pie-notification';
+const componentClass = 'c-notification';
 
 /**
  * @tagname pie-notification
  */
 export class PieNotification extends LitElement implements NotificationProps {
+    @property({ type: Boolean })
+    public isOpen = false;
+
     render () {
-        return html`<h1 data-test-id="pie-notification">Hello world!</h1>`;
+        return html`
+        <div data-test-id="${componentSelector}" class="${componentClass}">
+            <section>
+                <header class="${componentClass}-header">
+                    <!-- Icon -->
+                    <h2>Title</h2>
+                </header>
+                <article>
+                    Content
+                    <!-- <slot></slot> -->
+                </article>
+            </section>
+            
+            <footer class="${componentClass}-footer">
+                <!-- Action -->
+                Footer
+            </footer>
+        </div>`;
     }
 
     // Renders a `CSSResult` generated from SCSS by Vite
