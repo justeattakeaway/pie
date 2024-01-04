cb74abb77c560a71a2bf611429a122e027636f36
personal <jamieolivermaguire@googlemail.com>
2024-01-04 11:21:02 +0000

    update test

--- packages/components/pie-webc-core/src/test/mixins/formControlMixin/formControlMixin.browser.spec.ts
+++ packages/components/pie-webc-core/src/test/mixins/formControlMixin/formControlMixin.browser.spec.ts
@@ -9,67 +9,73 @@ test.describe('FormControlMixin', () => {
     });
 
     test.describe('form property', () => {
-        test('should not have an associated form when no form exists', async ({ page, mount }) => {
-            // Arrange
-            await mount(MockComponent);
+        test.describe('when no form exists', () => {
+            test('should not have an associated form', async ({ page, mount }) => {
+                // Arrange
+                await mount(MockComponent);
 
-            const isFormAssociated = await page.evaluate(() => {
-                const component = document.querySelector('form-control-mixin-mock');
-                const form = component?.form;
+                const isFormAssociated = await page.evaluate(() => {
+                    const component = document.querySelector('form-control-mixin-mock');
+                    const form = component?.form;
 
-                return !!form;
+                    return !!form;
+                });
+
+                // Assert
+                expect(isFormAssociated).toBe(false);
             });
-
-            // Assert
-            expect(isFormAssociated).toBe(false);
         });
 
-        test('should return the associated form when inside of a form', async ({ page }) => {
-            // Arrange
-            await page.evaluate(() => {
-                const formHTML = `
-                <form id="testForm" action="/foo" method="POST">
-                    <input type="text" id="username" name="username" required>
-                    <input type="password" id="password" name="password" required>
+        test.describe('when inside of a form', () => {
+            test('should return the associated form', async ({ page }) => {
+                // Arrange
+                await page.evaluate(() => {
+                    const formHTML = `
+                    <form id="testForm" action="/foo" method="POST">
+                        <input type="text" id="username" name="username" required>
+                        <input type="password" id="password" name="password" required>
+                        <form-control-mixin-mock></form-control-mixin-mock>
+                    </form>
+                    `;
+                    document.body.innerHTML = formHTML;
+                });
+
+                const formId = await page.evaluate(() => {
+                    const component = document.querySelector('form-control-mixin-mock');
+                    const form = component?.form;
+
+                    return form?.id;
+                });
+
+                // Assert
+                expect(formId).toContain('testForm');
+            });
+        });
+
+        test.describe('when not inside of an existing form', () => {
+            test('should not have an associated form', async ({ page }) => {
+                // Arrange
+                await page.evaluate(() => {
+                    const formHTML = `
+                    <form id="siblingForm" action="/foo" method="POST">
+                        <input type="text" id="username" name="username" required>
+                        <input type="password" id="password" name="password" required>
+                    </form>
                     <form-control-mixin-mock></form-control-mixin-mock>
-                </form>
-                `;
-                document.body.innerHTML = formHTML;
+                    `;
+                    document.body.innerHTML = formHTML;
+                });
+
+                const isFormAssociated = await page.evaluate(() => {
+                    const component = document.querySelector('form-control-mixin-mock');
+                    const form = component?.form;
+
+                    return !!form;
+                });
+
+                // Assert
+                expect(isFormAssociated).toBe(false);
             });
-
-            const formId = await page.evaluate(() => {
-                const component = document.querySelector('form-control-mixin-mock');
-                const form = component?.form;
-
-                return form?.id;
-            });
-
-            // Assert
-            expect(formId).toContain('testForm');
-        });
-
-        test('should not have an associated form when it is not inside of a form', async ({ page }) => {
-            // Arrange
-            await page.evaluate(() => {
-                const formHTML = `
-                <form id="siblingForm" action="/foo" method="POST">
-                    <input type="text" id="username" name="username" required>
-                    <input type="password" id="password" name="password" required>
-                </form>
-                <form-control-mixin-mock></form-control-mixin-mock>
-                `;
-                document.body.innerHTML = formHTML;
-            });
-
-            const isFormAssociated = await page.evaluate(() => {
-                const component = document.querySelector('form-control-mixin-mock');
-                const form = component?.form;
-
-                return !!form;
-            });
-
-            // Assert
-            expect(isFormAssociated).toBe(false);
         });
     });
 });
