f5aee523ebcd16e6801d32103760ffa05ccaf6af
personal <jamieolivermaguire@googlemail.com>
2024-01-11 10:11:09 +0000

    use storybook action for form submit

--- apps/pie-storybook/stories/pie-input.stories.ts
+++ apps/pie-storybook/stories/pie-input.stories.ts
@@ -78,51 +78,42 @@ const Template = ({ type, value, name }: InputProps) => {
     `;
 };
 
-const FormTemplate: TemplateFunction<InputProps> = (props: InputProps) => html`
-<h2>Fake form</h2>
-<form id="testForm">
-        <p>Required fields are followed by <strong><span aria-label="required">*</span></strong>.</p>
-        <section>
-            <h2>Contact information</h2>
-            <p>
-                <label for="name">
-                    <span>Name: </span>
-                    <strong><span aria-label="required">*</span></strong>
-                </label>
-                ${Template({ ...props, type: 'text' })}
-            </p>
-        </section>
-        <section style="display: flex; gap: var(--dt-spacing-a); justify-content: flex-end; flex-wrap: wrap; margin-top: var(--dt-spacing-b);">
-            <pie-button type="reset" variant="secondary">Reset</pie-button>
-            <pie-button type="submit" variant="primary">Submit</pie-button>
-        </section>
-    </form>
-    <p id="formLog" style="display: none; font-size: 2rem; color: var(--dt-color-support-positive);"></p>
-    <script>
-        // Display a success message to the user when they submit the form
-        const form = document.querySelector('#testForm');
-        const formLog = document.querySelector('#formLog');
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
+const FormTemplate: TemplateFunction<InputProps> = (props: InputProps) => {
+    function onSubmit (event: SubmitEvent) {
+        event.preventDefault();
 
-            formLog.innerHTML = 'Form submitted!';
-            formLog.style.display = 'block';
+        const form = event.target as HTMLFormElement;
+        const formData = new FormData(form);
 
-            const formData = new FormData(form);
-
-            for (const entry of formData.entries()) {
-                console.table(entry);
-            }
-
-            // Reset the success message after roughly 8 seconds
-            setTimeout(() => {
-                formLog.innerHTML = '';
-                formLog.style.display = 'none';
-            }, 8000);
+        const data: { [key: string]: string } = {};
+        formData.forEach((value, key) => {
+            data[key] = value.toString();
         });
 
-    </script>
-`;
+        action('submit')({
+            ...data,
+        });
+    }
+
+    return html`
+        <h2>Fake form</h2>
+        <form id="testForm" @submit="${onSubmit}">
+                <section>
+                    <h2>Contact information</h2>
+                    <p>
+                        <label for="name">
+                            <span>Name: </span>
+                        </label>
+                        ${Template({ ...props, type: 'text' })}
+                    </p>
+                </section>
+                <section style="display: flex; gap: var(--dt-spacing-a); justify-content: flex-end; flex-wrap: wrap; margin-top: var(--dt-spacing-b);">
+                    <pie-button type="reset" variant="secondary">Reset</pie-button>
+                    <pie-button type="submit" variant="primary">Submit</pie-button>
+                </section>
+            </form>
+    `;
+};
 
 const createInputStoryWithForm = createStory<InputProps>(FormTemplate, defaultArgs);
 
