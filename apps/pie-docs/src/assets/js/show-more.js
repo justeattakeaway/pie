01ef3a5dc93b13abce284d509f8cdad43c841f55
Lizzie Turney <lizzie.turney@justeattakeaway.com>
2024-01-12 16:28:58 +0000

    feat(pie-docs): DSW-1565 added show more button

---
+++ apps/pie-docs/src/assets/js/show-more.js
@@ -0,0 +1,58 @@
+let shown = true;
+let showMoreButtons;
+
+const initialiseShowMore = () => {
+    showMoreButtons = document.querySelectorAll('[data-js="show-more"]');
+    const componentStatusTables = document.querySelectorAll('[class="c-componentStatusTable "]');
+
+    if (!showMoreButtons) return;
+
+    if (componentStatusTables) {
+        componentStatusTables.forEach((el) => {
+            el.querySelectorAll('tbody tr').forEach((tr, index) => {
+                const rows = index >= 6 ? tr.style.display = 'none' : '';
+
+                return rows;
+            });
+        });
+    }
+
+    showMoreButtons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            tableReveal(event.target);
+        });
+    });
+};
+
+const tableReveal = (elm) => {
+    const componentStatusTable = document.querySelector(`[data-js="${elm.id}"]`);
+    const trs = componentStatusTable.querySelectorAll('tbody tr');
+
+    const hide = () => {
+        trs.forEach((tr, index) => {
+            const rows = index >= 6 ? tr.style.display = 'none' : '';
+
+            return rows;
+        });
+
+        elm.innerHTML = 'Show More <svg xmlns="http://www.w3.org/2000/svg" role="presentation" focusable="false" fill="#125fca" viewBox="0 0 16 16" height="16" width="16" class="c-pieIcon c-pieIcon--chevronDown u-iconFilled" aria-hidden="true"><path d="M2.82 5.044 8 10.399 13.197 5l.963.875-5.364 5.565a1.164 1.164 0 0 1-1.636 0L1.875 5.945l.945-.901Z"></path></svg>';
+        shown = false;
+    };
+
+    const show = () => {
+        trs.forEach((tr) => {
+            tr.style.display = 'table-row';
+
+            return tr;
+        });
+
+        elm.innerHTML = 'Show Less <svg xmlns="http://www.w3.org/2000/svg" role="presentation" focusable="false" fill="#125fca" viewBox="0 0 16 16" height="16" width="16" class="c-pieIcon c-pieIcon--chevronUp u-iconFilled" aria-hidden="true"><path d="M13.18 10.97 8 5.615l-5.18 5.399-.962-.875 5.346-5.565a1.164 1.164 0 0 1 1.671 0l5.25 5.495-.945.901Z"></path></svg>';
+        shown = true;
+    };
+
+    const table = shown ? hide() : show();
+
+    return table;
+};
+
+window.addEventListener('DOMContentLoaded', initialiseShowMore);
