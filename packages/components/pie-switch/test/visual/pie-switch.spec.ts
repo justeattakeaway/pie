9960bf5d0d2df1b5430ba13b25c40d303a995fb5
Josh Ng <joshua.ng2332@gmail.com>
2024-01-19 12:48:00 +0000

    test(pie-switch): DSW-1621 initial add of rtl visual test

--- packages/components/pie-switch/test/visual/pie-switch.spec.ts
+++ packages/components/pie-switch/test/visual/pie-switch.spec.ts
@@ -1,8 +1,16 @@
 import { test } from '@sand4rt/experimental-ct-web';
 import percySnapshot from '@percy/playwright';
+import type { WebComponentPropValues } from '@justeattakeaway/pie-webc-testing/src/helpers/defs.ts';
 import { percyWidths } from '@justeattakeaway/pie-webc-testing/src/percy/breakpoints.ts';
 import { PieSwitch } from '../../src/index.ts';
 import { SwitchProps, labelPlacements } from '../../src/defs.ts';
+
+// const renderRTLPieSwitch = (propVals: WebComponentPropValues) => {
+//     const { checked } = propVals;
+//     `<div ?isRTL=true, ?checked=${checked}><pie-switch ></pie-switch></div>`;
+// };
+
+const renderRTLPieSwitch = '<div ?isRTL=true><pie-switch ></pie-switch></div>';
 
 [
     [false, false],
@@ -36,5 +44,20 @@ test.describe('Prop: `Label`', () => {
                 await percySnapshot(page, `Switch - label placement: ${placement}`, percyWidths);
             });
         });
+    });
+});
+
+[
+    [false],
+    [true],
+].forEach(([checked]) => {
+    test.only(`should render RTL correctly with checked = ${checked}`, async ({ page, mount }) => {
+        await mount(renderRTLPieSwitch, {
+            props: {
+                checked,
+            },
+        });
+
+        await percySnapshot(page, `Switch - RTL - checked = ${checked}`, percyWidths);
     });
 });
