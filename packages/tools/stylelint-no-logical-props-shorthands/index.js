b180b7e4d03a566d60f3a8aed7895f1402e18a25
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2024-01-19 15:52:05 +0100

    feat(stylelint-config-pie): DSW-1113 provide custom rule for css logical props shorthands
    feat(stylelint-config-pie): DSW-1113 provide custom rule for css logical props shorthands
    
    feat(stylelint-config-pie): DSW-1113 provide custom rule for css logical props shorthands
    
    feat(stylelint-config-pie): DSW-1113 rename
    
    feat(stylelint-config-pie): DSW-1113 move
    
    feat(stylelint-config-pie): DSW-1113 add dependencies

---
+++ packages/tools/stylelint-no-logical-props-shorthands/index.js
@@ -0,0 +1,80 @@
+const stylelint = require('stylelint');
+
+const { report, ruleMessages, validateOptions } = stylelint.utils;
+
+const ruleName = '@justeattakeaway/stylelint-no-logical-props-shorthands';
+const messages = ruleMessages(ruleName, {
+    rejected: (unfixed) => `Disallowed usage of CSS logical property shorthand "${unfixed}"`,
+});
+
+const disallowedShorthands = [
+    'padding-block',
+    'padding-inline',
+    'margin-block',
+    'margin-inline',
+];
+
+function getReplacementNodes (node) {
+    // Split shorthand values
+    const values = node.value.split(' ');
+
+    // Create distinct declarations for start and end
+    const nodeStart = node.clone({
+        prop: `${node.prop}-start`,
+        value: values[0],
+    });
+
+    const nodeEnd = node.clone({
+        prop: `${node.prop}-end`,
+        value: values[1] || values[0],
+        raws: {
+            ...node.raws,
+            before: '', // prevent line break duplication
+        },
+    });
+
+    return [nodeStart, nodeEnd];
+}
+
+// TODO: Add documentation or readme
+function ruleFunction (primaryOption, secondaryOption, context) {
+    return function lint (root, result) {
+        // Validate options
+        const validOptions = validateOptions(
+            result,
+            ruleName,
+            {
+                // TODO: Provide options schema
+            },
+        );
+        if (!validOptions) return;
+
+        const isAutoFixing = Boolean(context.fix);
+
+        // Iterate over CSS declarations
+        root.walkDecls((decl) => {
+            const hasDisallowedShorthand = disallowedShorthands.includes(decl.prop);
+
+            // The expectation is met, there's nothing to be done
+            if (!hasDisallowedShorthand) return;
+
+            if (isAutoFixing) {
+                const nodes = getReplacementNodes(decl);
+
+                decl.replaceWith(...[nodes]);
+            } else {
+                report({
+                    ruleName,
+                    result,
+                    message: messages.rejected(decl.prop),
+                    node: decl,
+                    word: decl.prop, // The exact word caused the error
+                });
+            }
+        });
+    };
+}
+
+module.exports.ruleName = ruleName;
+module.exports.messages = messages;
+module.exports = stylelint.createPlugin(ruleName, ruleFunction);
