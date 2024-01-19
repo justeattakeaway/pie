6227849a974a51d848246f794839bfd147c7e4cc
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2024-01-19 17:04:45 +0100

    feat(stylelint-config-pie): DSW-1113 add tests

---
+++ packages/tools/stylelint-no-logical-props-shorthands/__tests__/plugin.test.js
@@ -0,0 +1,104 @@
+import {
+    describe,
+    it,
+    expect,
+    beforeEach,
+} from 'vitest';
+
+const stylelint = require('stylelint');
+
+const config = {
+    extends: 'stylelint-config-standard-scss',
+    plugins: [
+        '@justeattakeaway/stylelint-no-logical-props-shorthands',
+    ],
+    rules: {
+        '@justeattakeaway/stylelint-no-logical-props-shorthands': true,
+    },
+};
+
+describe('stylelint-no-logical-props-shorthands', () => {
+    let result;
+
+    describe('when given valid CSS', () => {
+        const validCSS = `a { margin-block-start: 10px; }
+`;
+        beforeEach(() => {
+            result = stylelint.lint({
+                code: validCSS,
+                config,
+            });
+        });
+
+        it('doesn\'t raises an error', () => result.then((data) => {
+            expect(data.errored).toBeFalsy();
+        }));
+    });
+
+    describe('when given invalid CSS', () => {
+        const invalidCSS = `a { margin-block: 10px; }
+`;
+        beforeEach(() => {
+            result = stylelint.lint({
+                code: invalidCSS,
+                config,
+            });
+        });
+
+        it('raises an error', () => result.then((data) => {
+            expect(data.errored).toBeTruthy();
+        }));
+
+        describe('when autofix is true', () => {
+            beforeEach(() => {
+                result = stylelint.lint({
+                    code: invalidCSS,
+                    config,
+                    fix: true,
+                });
+            });
+
+            describe('when a single value is given', () => {
+                it('provides the expected suggestion', async () => {
+                    const code = `a { margin-block: 10px; }
+`;
+                    const result = await stylelint.lint({
+                        code,
+                        config,
+                        fix: true,
+                    });
+
+                    expect(result.output).toMatchSnapshot();
+                });
+            });
+
+            describe('when multiple values are given', () => {
+                it('provides the expected suggestion', async () => {
+                    const code = `a { margin-block: 12px 34px; }
+`;
+                    const result = await stylelint.lint({
+                        code,
+                        config,
+                        fix: true,
+                    });
+
+                    expect(result.output).toMatchSnapshot();
+                });
+
+                describe('and values are complex', () => {
+                    it('provides the expected suggestion', async () => {
+                        const code = `a { padding-inline: calc(var(--dt-spacing-d) + $nav-item-inline-start-margin) var(--dt-spacing-d); }
+    `;
+                        const result = await stylelint.lint({
+                            code,
+                            config,
+                            fix: true,
+                        });
+
+                        expect(result.output).toMatchSnapshot();
+                    });
+                });
+            });
+        });
+    });
+});
