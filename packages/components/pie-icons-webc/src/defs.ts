7dd2f538847986888a1a58507844cde4a3fcb74c
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2024-01-24 17:19:33 +0000

    Add missing dependency

--- packages/components/pie-icons-webc/src/defs.ts
+++ packages/components/pie-icons-webc/src/defs.ts
@@ -1,5 +1,3 @@
-// TODO - please remove the eslint disable comment below when you add props to this interface
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 import { type RegularIconSize } from '@justeattakeaway/pie-icons-configs';
 
 export interface IconCloseProps {
