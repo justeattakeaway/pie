d9b4bc2d2f5f4298d46d9b9a5eb79c4ee7673eb5
Ashley Watson-Nolan <nolly00@gmail.com>
2024-01-29 09:51:42 +0000

    fix(pie-tag): DSW-000 percy visual test fix

--- packages/components/pie-tag/test/visual/pie-tag.spec.ts
+++ packages/components/pie-tag/test/visual/pie-tag.spec.ts
@@ -14,6 +14,7 @@ import {
     WebComponentTestWrapper,
 } from '@justeattakeaway/pie-webc-testing/src/helpers/components/web-component-test-wrapper/WebComponentTestWrapper.ts';
 import { percyWidths } from '@justeattakeaway/pie-webc-testing/src/percy/breakpoints.ts';
+import { PieTag } from '@/index';
 import { sizes, variants } from '../../src/defs.ts';
 
 // TODO: Currently setting the slot to use a straight up SVG
@@ -36,8 +37,20 @@ const componentPropsMatrix: WebComponentPropValues[] = getAllPropCombinations(pr
 const componentPropsMatrixByVariant: Record<string, WebComponentPropValues[]> = splitCombinationsByPropertyValue(componentPropsMatrix, 'variant');
 const componentVariants: string[] = Object.keys(componentPropsMatrixByVariant);
 
-// eslint-disable-next-line no-empty-pattern
-test.beforeEach(async ({ }, testInfo) => {
+// This ensures the component is registered in the DOM for each test
+// This is not required if your tests mount the web component directly in the tests
+test.beforeEach(async ({ page, mount }, testInfo) => {
+    await mount(
+        PieTag,
+        {},
+    );
+
+    // Removing the element so it's not present in the tests (but is still registered in the DOM)
+    await page.evaluate(() => {
+        const element : Element | null = document.querySelector('pie-tag');
+        element?.remove();
+    });
+
     testInfo.setTimeout(testInfo.timeout + 40000);
 });
 
