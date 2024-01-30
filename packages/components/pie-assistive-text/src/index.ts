e6e805dc028b8acef67f830f7151d4815272b94f
Lizzie Turney <lizzie.turney@justeattakeaway.com>
2024-01-30 17:42:56 +0000

    feat(pie-assistive-text): DSW-1590 added generated component

---
+++ packages/components/pie-assistive-text/src/index.ts
@@ -0,0 +1,30 @@
+import { LitElement, html, unsafeCSS } from 'lit';
+import { RtlMixin, defineCustomElement } from '@justeattakeaway/pie-webc-core';
+
+import styles from './assistive-text.scss?inline';
+import { AssistiveTextProps } from './defs';
+
+// Valid values available to consumers
+export * from './defs';
+
+const componentSelector = 'pie-assistive-text';
+
+/**
+ * @tagname pie-assistive-text
+ */
+export class PieAssistiveText extends RtlMixin(LitElement) implements AssistiveTextProps {
+    render () {
+        return html`<h1 data-test-id="pie-assistive-text">Hello world!</h1>`;
+    }
+
+    // Renders a `CSSResult` generated from SCSS by Vite
+    static styles = unsafeCSS(styles);
+}
+
+defineCustomElement(componentSelector, PieAssistiveText);
+
+declare global {
+    interface HTMLElementTagNameMap {
+        [componentSelector]: PieAssistiveText;
+    }
+}
