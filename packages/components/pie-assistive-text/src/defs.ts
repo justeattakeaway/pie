137ef753f8e5bbfa4d9c3b082a1e7a558ca91fde
Lizzie Turney <lizzie.turney@justeattakeaway.com>
2024-02-01 17:06:55 +0000

    feat(pie-assistive-text): DSW-1590 removed unneeded space

--- packages/components/pie-assistive-text/src/defs.ts
+++ packages/components/pie-assistive-text/src/defs.ts
@@ -1,6 +1,5 @@
 export const variants = ['default', 'error', 'success'] as const;
 
-
 export interface AssistiveTextProps {
      /**
      * What variant the assistive text should be such as default, error or success.
