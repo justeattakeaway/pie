81ed6696bb12341f89810ec63b89a1f808033344
Josh Ng <joshua.ng2332@gmail.com>
2024-02-07 11:06:15 +0000

    fix(pie-docs): DSW-1721 Privacy policy 200 fix (#1253)
    * fix(pie-docs): DSW-1721 un-skipped footer privacy policy test
    
    * chore(pie-docs): DSW-1721 changeset added

--- apps/pie-docs/test/system/footer.spec.js
+++ apps/pie-docs/test/system/footer.spec.js
@@ -7,7 +7,7 @@ test.beforeEach(async ({ page, context, baseURL }) => {
 });
 
 test.describe('PIE - Footer - @desktop', () => {
-    test.skip('Should ensure links return correct status code', async ({ page }) => {
+    test('Should ensure links return correct status code', async ({ page }) => {
         /* eslint-disable no-await-in-loop, no-restricted-syntax */
         for await (const link of await page.getByTestId('footer_link').all()) {
             const href = await link.getAttribute('href');
