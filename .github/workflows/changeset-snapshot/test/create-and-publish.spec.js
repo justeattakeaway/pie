e64c6423478a31785835d79f5a1b40ee84f32291
Ben Siggery <bensiggery28@gmail.com>
2024-02-09 15:37:34 +0000

    refactor syntax

--- .github/workflows/changeset-snapshot/test/create-and-publish.spec.js
+++ .github/workflows/changeset-snapshot/test/create-and-publish.spec.js
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, test, vi } from 'vitest';
-import createAndPublish from '../create-and-publish.mjs';
+import createAndPublish from '../create-and-publish';
 import { Readable } from 'stream';
 
 let context;
