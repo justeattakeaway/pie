f4b2f74acaf26b5e0c4030030611cf106a4b8864
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2024-02-13 14:09:16 +0100

    feat(pie-form-label): DSW-1585 add component tests

--- packages/components/pie-form-label/test/component/pie-form-label.spec.ts
+++ packages/components/pie-form-label/test/component/pie-form-label.spec.ts
@@ -1,5 +1,7 @@
 
 import { test, expect, MountOptions } from '@sand4rt/experimental-ct-web';
+import { PieInput } from '@justeattakeaway/pie-input';
+import { PieSwitch } from '@justeattakeaway/pie-switch';
 import { PieFormLabel, FormLabelProps } from '../../src/index.ts';
 
 const componentSelector = '[data-test-id="pie-form-label"]';
@@ -23,5 +25,45 @@ test.describe('PieFormLabel - Component tests', () => {
 
         // Assert
         expect(formLabel).toBeVisible();
+    });
+    test.describe('should focus the component assigned with "for" when clicked', () => {
+        // IMPORTANT: Mounting and Unmounting the component before each test ensures that any tests that do not explicitly
+        // mount the component will still have it available in Playwright's cache (loaded and registered in the test browser)
+        test.beforeEach(async ({ mount }) => {
+            const pieInput = await mount(PieInput);
+            await pieInput.unmount();
+
+            const pieSwitch = await mount(PieSwitch);
+            await pieInput.unmount();
+
+            const pieFormLabel = await mount(PieFormLabel);
+            await pieFormLabel.unmount();
+        });
+
+        test('when used with a text input', async ({ page }) => {
+            // Arrange
+            await page.setContent('<pie-form-label for="email">Email:</pie-form-label><pie-input id="email"></pie-input>');
+
+            // Act
+            const target = page.locator('#email');
+            const label = page.locator('pie-form-label');
+            await label.click();
+
+            // Assert
+            expect(target).toBeFocused();
+        });
+
+        test('when used with a switch', async ({ page }) => {
+            // Arrange
+            await page.setContent('<pie-form-label for="approveSettings">Approve settings</pie-form-label><pie-switch id="approveSettings"></pie-switch>');
+
+            // Act
+            const target = page.locator('#approveSettings');
+            const label = page.locator('pie-form-label');
+            await label.click();
+
+            // Assert
+            expect(target).toBeFocused();
+        });
     });
 });
