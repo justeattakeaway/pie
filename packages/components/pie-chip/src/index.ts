538ed6495eef2a2ffb5e7e64bf8a6e34ae1c2e6f
maryia.radchuk <maryia.radchuk@justeattakeaway.com>
2024-02-14 13:51:57 +0100

    feat(pie-chip): DSW-1732 dispatching pie-chip-close event

--- packages/components/pie-chip/src/index.ts
+++ packages/components/pie-chip/src/index.ts
@@ -3,9 +3,11 @@ import {
 } from 'lit';
 import { property } from 'lit/decorators.js';
 
-import { validPropertyValues, defineCustomElement } from '@justeattakeaway/pie-webc-core';
+import {
+    validPropertyValues, defineCustomElement, dispatchCustomEvent,
+} from '@justeattakeaway/pie-webc-core';
 import styles from './chip.scss?inline';
-import { ChipProps, variants } from './defs';
+import { ChipProps, variants, ON_CHIP_CLOSE_EVENT } from './defs';
 import '@justeattakeaway/pie-icons-webc/IconCloseCircleFilled';
 import '@justeattakeaway/pie-spinner';
 
@@ -18,6 +20,7 @@ const componentSelector = 'pie-chip';
  * @tagname pie-chip
  * @slot icon - The icon slot
  * @slot - Default slot
+ * @event {CustomEvent} pie-chip-close - when a user clicks close button.
  */
 export class PieChip extends LitElement implements ChipProps {
     @property()
@@ -54,6 +57,15 @@ export class PieChip extends LitElement implements ChipProps {
     }
 
     /**
+     * Handles click on a close button by dispatching a custom event
+     *
+     * @private
+     */
+    private _handleCloseButtonClick () : void {
+        dispatchCustomEvent(this, ON_CHIP_CLOSE_EVENT);
+    }
+
+    /**
      * Template for the dismissible state
      *
      * @private
@@ -61,6 +73,8 @@ export class PieChip extends LitElement implements ChipProps {
     private renderCloseButton (): TemplateResult {
         return html`
                     <button
+                        @click="${this._handleCloseButtonClick}"
+                        ?disabled=${this.disabled}
                         class="c-chip-closeBtn"
                         data-test-id="chip-close-button">
                         <icon-close-circle-filled size="m"></icon-close-circle-filled>
