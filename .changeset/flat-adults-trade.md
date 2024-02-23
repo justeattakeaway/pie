2b83c01af902ef86b3716ff01d92554325a28c6a
J. de Freitas <juniormfreitas@gmail.com>
2024-02-23 06:21:44 -0700

    feat(pie-notification): DSW-1348 Build the basic functionality and styles of the Pie Notification web component (#1254)
    * feat(pie-notification): DSW-1348 creating classes and placeholders
    
    * feat(pie-notification): DSW-1348 wip add storybook buttons
    
    * feat(pie-notification): DSW-1348 wip add icons
    
    * feat(pie-notification): DSW-1348 implement icon logic and close button
    
    * feat(pie-notification): DSW-1348 add values to props table in the readme file
    
    * feat(pie-notification): DSW-1348 implement unit tests
    
    * feat(pie-notification): DSW-1348 fix styling
    
    * feat(pie-notification): DSW-1348 implement isCompact property
    
    * feat(pie-notification): DSW-1348 remove unused code
    
    * feat(pie-notification): DSW-1348 create util methods for renderIconVariant
    
    * feat(pie-notification): DSW-1348 set default properties and make mandatory properties as optional
    
    * feat(pie-notification): DSW-1348 optimize headingTag generation
    
    * feat(pie-notification): DSW-1348 apply unit tests as the same format as pie-input
    
    * feat(pie-notification): DSW-1348 cover edge case when variant does not have icon
    
    * feat(pie-notification): DSW-1348 add icons assertions in the unit tests
    
    * feat(pie-notification): DSW-1348 fix lifecycle issue with icons and variants
    
    * feat(pie-notification): DSW-1348 fix test name
    
    * feat(pie-notification): DSW-1348 use state decorators instead private properties
    
    * feat(pie-notification): DSW-1348 use PropertyValues type
    
    * feat(pie-notification): DSW-1348 use logical properties in css
    
    * feat(pie-notification): DSW-1348 do not destruct methods in the render function
    
    * feat(pie-notification): DSW-1348 fix documentation
    
    * feat(pie-notification): DSW-1348 adjust internal types and remove static methods
    
    * feat(pie-notification): DSW-1348 adjust _iconSlot formatting
    
    * feat(pie-notification): DSW-1348 rollback _iconSlot formatting
    
    * feat(pie-notification): DSW-1348 publish pie-notification minor release including basic properties
    
    * Update .changeset/flat-adults-trade.md
    
    ---------
    
    Co-authored-by: Xander Marjoram <xander.marjoram@justeattakeaway.com>

---
+++ .changeset/flat-adults-trade.md
@@ -0,0 +1,9 @@
+---
+"@justeattakeaway/pie-notification": patch
+"pie-storybook": patch
+"pie-monorepo": patch
+---
+
+[Added] - isOpen, variant, isCompact, heading, headingLevel, hideIcon properties
+[Added] - styles, basic interactions and unit tests
+[Changed] - Storybook's playground
--- packages/components/pie-notification/test/component/pie-notification.spec.ts
+++ packages/components/pie-notification/test/component/pie-notification.spec.ts
@@ -1,14 +1,36 @@
 
 import { test, expect } from '@sand4rt/experimental-ct-web';
-import { PieNotification, NotificationProps } from '../../src/index.ts';
+import { PieNotification } from '../../src/index.ts';
+import { variants, headingLevels, type NotificationProps } from '../../src/defs.ts';
 
-const componentSelector = '[data-test-id="pie-notification"]';
+const rootSelector = 'pie-notification';
+const componentSelector = `[data-test-id="${rootSelector}"]`;
+const iconCloseSelector = `[data-test-id="${rootSelector}-icon-close"]`;
+const slottedIconSelector = `[data-test-id="${rootSelector}-icon-slotted"]`;
+const iconAreaSelector = `[data-test-id="${rootSelector}-heading-icon-area"]`;
+const headingIconInfoSelector = `[data-test-id="${rootSelector}-heading-icon-info"]`;
+const headingIconSuccessSelector = `[data-test-id="${rootSelector}-heading-icon-success"]`;
+const headingIconWarningSelector = `[data-test-id="${rootSelector}-heading-icon-warning"]`;
+const headingIconErrorSelector = `[data-test-id="${rootSelector}-heading-icon-error"]`;
+const headerSelector = `[data-test-id="${rootSelector}-header"]`;
+const headingSelector = `[data-test-id="${rootSelector}-heading"]`;
+
+const slotContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet tincidunt est, vitae vulputate turpis. Cras pretium venenatis elementum. Duis tristique neque non varius tempor. In hac habitasse platea dictumst. Aenean accumsan vehicula urna. Cras fringilla sed ipsum nec dignissim. Aliquam sit amet ullamcorper ligula.';
+const mockSlottedIcon = `<div slot="icon" data-test-id="${rootSelector}-icon-slotted">Mocked Icon Slot</div>`;
 
 test.describe('PieNotification - Component tests', () => {
+    // IMPORTANT: Mounting and Unmounting the component before each test ensures that any tests that do not explicitly
+    // mount the component will still have it available in Playwright's cache (loaded and registered in the test browser)
+    test.beforeEach(async ({ mount }) => {
+        const component = await mount(PieNotification);
+        await component.unmount();
+    });
+
     test('should render successfully', async ({ mount, page }) => {
         // Arrange
         await mount(PieNotification, {
             props: {} as NotificationProps,
+            slots: { default: slotContent },
         });
 
         // Act
@@ -16,5 +38,223 @@ test.describe('PieNotification - Component tests', () => {
 
         // Assert
         expect(notification).toBeVisible();
+    });
+
+    test.describe('Props', () => {
+        test.describe('variants', () => {
+            variants.forEach((variant) => {
+                test(`should render when the variant is ${variant}`, async ({ mount, page }) => {
+                    // Arrange
+                    await mount(PieNotification, {
+                        props: {
+                            variant,
+                        },
+                    });
+
+                    // Act
+                    const notification = page.locator(componentSelector);
+
+                    // Assert
+                    expect(notification).toBeVisible();
+                    expect(notification).toHaveAttribute('variant', variant);
+                });
+            });
+
+            test('should render icon-info when variant is info', async ({ mount, page }) => {
+                // Arrange
+                await mount(PieNotification, {
+                    props: {
+                        variant: 'info',
+                    },
+                });
+
+                // Act
+                const notification = page.locator(componentSelector);
+                const icon = page.locator(headingIconInfoSelector);
+
+                // Assert
+                expect(notification).toBeVisible();
+                expect(icon).toBeVisible();
+            });
+
+            test('should render icon-success when variant is success', async ({ mount, page }) => {
+                // Arrange
+                await mount(PieNotification, {
+                    props: {
+                        variant: 'success',
+                    },
+                });
+
+                // Act
+                const notification = page.locator(componentSelector);
+                const icon = page.locator(headingIconSuccessSelector);
+
+                // Assert
+                expect(notification).toBeVisible();
+                expect(icon).toBeVisible();
+            });
+
+            test('should render icon-warning when variant is warning', async ({ mount, page }) => {
+                // Arrange
+                await mount(PieNotification, {
+                    props: {
+                        variant: 'warning',
+                    },
+                });
+
+                // Act
+                const notification = page.locator(componentSelector);
+                const icon = page.locator(headingIconWarningSelector);
+
+                // Assert
+                expect(notification).toBeVisible();
+                expect(icon).toBeVisible();
+            });
+
+            test('should render icon-error when variant is error', async ({ mount, page }) => {
+                // Arrange
+                await mount(PieNotification, {
+                    props: {
+                        variant: 'error',
+                    },
+                });
+
+                // Act
+                const notification = page.locator(componentSelector);
+                const icon = page.locator(headingIconErrorSelector);
+
+                // Assert
+                expect(notification).toBeVisible();
+                expect(icon).toBeVisible();
+            });
+        });
+
+        test.describe('isCompact', () => {
+            test('should not render the close icon if isCompact is true ', async ({ mount, page }) => {
+                // Arrange
+                await mount(PieNotification, {
+                    props: { isCompact: true },
+                });
+
+                // Act
+                const notification = page.locator(componentSelector);
+                const iconClose = page.locator(iconCloseSelector);
+
+                // Assert
+                expect(notification).toBeVisible();
+                expect(iconClose).not.toBeVisible();
+            });
+        });
+
+        test.describe('heading', () => {
+            test('should not render the header if heading is not provided', async ({ mount, page }) => {
+                // Arrange
+                await mount(PieNotification, {
+                    props: { heading: undefined },
+                });
+
+                // Act
+                const notification = page.locator(componentSelector);
+                const iconClose = page.locator(iconCloseSelector);
+                const header = page.locator(headerSelector);
+
+                // Assert
+                expect(notification).toBeVisible();
+                expect(iconClose).toBeVisible();
+                expect(header).not.toBeVisible();
+            });
+
+            test('should render the header when heading is provided', async ({ mount, page }) => {
+                // Arrange
+                await mount(PieNotification, {
+                    props: { heading: 'Title' },
+                });
+
+                // Act
+                const notification = page.locator(componentSelector);
+                const iconClose = page.locator(iconCloseSelector);
+                const header = page.locator(headerSelector);
+                const heading = page.locator(`h2${headingSelector}`);
+
+                // Assert
+                expect(notification).toBeVisible();
+                expect(iconClose).toBeVisible();
+                expect(header).toBeVisible();
+                expect(heading).toBeVisible();
+                expect(heading).toHaveText('Title');
+            });
+        });
+
+        test.describe('headingLevel', () => {
+            test.describe('heading levels', () => {
+                headingLevels.forEach((headingLevel) => {
+                    test(`should render successfully when heading level is ${headingLevel}`, async ({ mount, page }) => {
+                        // Arrange
+                        await mount(PieNotification, {
+                            props: {
+                                headingLevel,
+                                heading: `Title using ${headingLevel}`,
+                            },
+                        });
+
+                        // Act
+                        const heading = page.locator(`${headingLevel}${headingSelector}`);
+                        const header = page.locator(headerSelector);
+
+                        // Assert
+                        expect(header).toBeVisible();
+                        expect(heading).toBeVisible();
+                        expect(heading).toHaveText(`Title using ${headingLevel}`);
+                    });
+                });
+            });
+        });
+
+        test.describe('hideIcon', () => {
+            const variantsWithDefaultIcon = ['info', 'success', 'warning', 'error'];
+            variants.filter((variant) => variantsWithDefaultIcon.includes(variant)).forEach((variant) => {
+                test(`should not render the default icon when the variant is ${variant} and hideIcon is true`, async ({ mount, page }) => {
+                    // Arrange
+                    await mount(PieNotification, {
+                        props: {
+                            variant,
+                            hideIcon: true,
+                        },
+                    });
+
+                    // Act
+                    const notification = page.locator(componentSelector);
+                    const iconArea = page.locator(iconAreaSelector);
+                    const iconSelector = page.locator(`[data-test-id="${rootSelector}-heading-icon-${variant}"]`);
+
+                    // Assert
+                    expect(notification).toBeVisible();
+                    expect(notification).toHaveAttribute('variant', variant);
+                    expect(iconSelector).not.toBeVisible();
+                    expect(iconArea).not.toBeVisible();
+                });
+            });
+        });
+    });
+
+    test.describe('Slots', () => {
+        test('should correctly render the slot content and an icon as named slot', async ({ mount, page }) => {
+            // Arrange
+            await mount(PieNotification, {
+                slots: {
+                    default: slotContent,
+                    icon: mockSlottedIcon,
+                },
+            });
+
+            // Act
+            const notification = page.locator(componentSelector);
+            const slottedIcon = page.locator(slottedIconSelector);
+
+            // Assert
+            expect(notification).toBeVisible();
+            expect(slottedIcon).toBeVisible();
+            expect(slottedIcon).toHaveText('Mocked Icon Slot');
+        });
     });
 });
