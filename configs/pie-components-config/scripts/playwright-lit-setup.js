33757088bcd32192f7910ec1a74770ca5c113e11
Ben Siggery <bensiggery28@gmail.com>
2024-03-07 10:50:08 +0000

    chore(pie-monorepo): DSW-1783 fix tests

--- configs/pie-components-config/scripts/playwright-lit-setup.js
+++ configs/pie-components-config/scripts/playwright-lit-setup.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 const destinationDir = path.resolve(process.cwd(), 'playwright');
 
-async function copyPlaywrightHTML () {
+async function copyPlaywrightFiles () {
     try {
         try {
             await fs.access(destinationDir);
@@ -16,14 +16,17 @@ async function copyPlaywrightHTML () {
             await fs.mkdir(destinationDir, { recursive: true });
         }
 
-        const sourceFile = path.resolve(__dirname, '..', 'playwright/index.html');
-        const destinationFile = path.resolve(destinationDir, 'index.html');
+        const playwrightFiles = ['index.html', 'index.ts'];
+        playwrightFiles.forEach(async (file) => {
+            const sourceFile = path.resolve(__dirname, '..', `playwright/${file}`);
+            const destinationFile = path.resolve(destinationDir, file);
 
-        await fs.copyFile(sourceFile, destinationFile);
-        console.log('Playwright HTML file copied successfully.');
+            await fs.copyFile(sourceFile, destinationFile);
+            console.log(`Playwright ${file} copied successfully.`);
+        });
     } catch (err) {
         console.error('Error copying Playwright HTML:', err);
     }
 }
 
-copyPlaywrightHTML();
+copyPlaywrightFiles();
