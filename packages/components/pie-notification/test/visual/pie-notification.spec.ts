2ac2ba9a0168d4a258e95c65795dc8f06f748477
Jose De Freitas Jr <jose.defreitasjr@justeattakeaway.com>
2024-03-15 17:09:53 -0600

    feat(pie-notification): DSW-1351 additional visual tests

--- packages/components/pie-notification/test/visual/pie-notification.spec.ts
+++ packages/components/pie-notification/test/visual/pie-notification.spec.ts
@@ -1,14 +1,91 @@
-
-import { test, expect } from '@sand4rt/experimental-ct-web';
+import { test } from '@sand4rt/experimental-ct-web';
 import percySnapshot from '@percy/playwright';
-import { PieNotification, type NotificationProps } from '../../src/index.ts';
+import type {
+    PropObject, WebComponentPropValues, WebComponentTestInput,
+} from '@justeattakeaway/pie-webc-testing/src/helpers/defs.ts';
+import {
+    getAllPropCombinations, splitCombinationsByPropertyValue,
+} from '@justeattakeaway/pie-webc-testing/src/helpers/get-all-prop-combos.ts';
+import {
+    createTestWebComponent,
+} from '@justeattakeaway/pie-webc-testing/src/helpers/rendering.ts';
+import {
+    WebComponentTestWrapper,
+} from '@justeattakeaway/pie-webc-testing/src/helpers/components/web-component-test-wrapper/WebComponentTestWrapper.ts';
+import { IconHeartFilled } from '@justeattakeaway/pie-icons-webc';
 import {
     variants,
     headingLevels,
 } from '../../src/defs.ts';
+import { PieNotification, type NotificationProps } from '../../src/index.ts';
 
-const rootSelector = 'pie-notification';
-const componentSelector = `[data-test-id="${rootSelector}"]`;
+export const screenWidths = {
+    widths: [1450, 375],
+};
+
+const slotContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet tincidunt est, vitae vulputate turpis. Cras pretium venenatis elementum. Duis tristique neque non varius tempor. In hac habitasse platea dictumst. Aenean accumsan vehicula urna. Cras fringilla sed ipsum nec dignissim. Aliquam sit amet ullamcorper ligula.';
+
+const props: PropObject = {
+    variant: variants,
+    heading: ['Title', ''],
+    isDismissible: [false, true],
+    isCompact: [false, true],
+    hideIcon: [false, true],
+};
+
+// Renders a <pie-notification> HTML string with the given prop values
+const renderTestPieNotification = (propVals: WebComponentPropValues) => `<pie-notification 
+        variant="${propVals.variant}" 
+        ${propVals.isCompact ? 'isCompact' : ''}
+        ${propVals.isDismissible ? 'isDismissible' : ''}
+        ${propVals.hideIcon ? 'hideIcon' : ''}
+        heading="${propVals.heading}">${slotContent}</pie-notification>`;
+
+const componentPropsMatrix: WebComponentPropValues[] = getAllPropCombinations(props);
+const componentPropsMatrixByVariant: Record<string, WebComponentPropValues[]> = splitCombinationsByPropertyValue(componentPropsMatrix, 'variant');
+const componentVariants: string[] = Object.keys(componentPropsMatrixByVariant);
+
+test.beforeEach(async ({ mount }, testInfo) => {
+    testInfo.setTimeout(testInfo.timeout + 40000);
+
+    // This ensures the tag and icon components are registered in the DOM for each test.
+    // It appears to add them to a Playwright cache which we understand is required for the tests to work correctly.
+    const tagComponent = await mount(PieNotification);
+    await tagComponent.unmount();
+    const iconComponent = await mount(IconHeartFilled);
+    await iconComponent.unmount();
+});
+
+componentVariants.forEach((variant) => test(`should render all prop variations for Variant: ${variant}`, async ({ page, mount }) => {
+    await Promise.all(componentPropsMatrixByVariant[variant].map(async (combo: WebComponentPropValues) => {
+        const testComponent: WebComponentTestInput = createTestWebComponent(combo, renderTestPieNotification);
+
+        const propKeyValues = `
+            variant: ${testComponent.propValues.variant},
+            isCompact: ${testComponent.propValues.isCompact},
+            isDismissible: ${testComponent.propValues.isDismissible},
+            hideIcon: ${testComponent.propValues.hideIcon},
+            heading: ${testComponent.propValues.heading}
+        `;
+
+        const darkMode = ['neutral-alternative'].includes(variant);
+
+        await mount(
+            WebComponentTestWrapper,
+            {
+                props: { propKeyValues, darkMode },
+                slots: {
+                    component: testComponent.renderedString.trim(),
+                },
+            },
+        );
+    }));
+
+    // Follow up to remove in Jan
+    await page.waitForTimeout(5000);
+
+    await percySnapshot(page, `PIE Notification - Variant: ${variant}`, screenWidths);
+}));
 
 const initialValues: NotificationProps = {
     isOpen: true,
@@ -20,24 +97,6 @@ const initialValues: NotificationProps = {
         onClick: () => { console.info('confirmAction'); },
     },
 };
-const slotContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet tincidunt est, vitae vulputate turpis. Cras pretium venenatis elementum. Duis tristique neque non varius tempor. In hac habitasse platea dictumst. Aenean accumsan vehicula urna. Cras fringilla sed ipsum nec dignissim. Aliquam sit amet ullamcorper ligula.';
-
-test.describe('PieNotification - Visual tests`', () => {
-    test('should display the PieNotification component successfully', async ({ page, mount }) => {
-        // Arrange
-        await mount(PieNotification, {
-            props: initialValues as NotificationProps,
-            slots: { default: slotContent },
-        });
-
-        // Act
-        const notification = page.locator(componentSelector);
-
-        // Assert
-        await expect.soft(notification).toBeVisible();
-        await percySnapshot(page, 'PieNotification - Visual Test');
-    });
-});
 
 test.describe('Props', () => {
     const confirmAction = () => { console.info('confirmAction'); };
@@ -53,42 +112,6 @@ test.describe('Props', () => {
         onClick: cancelAction,
     };
 
-    test.describe('isOpen', () => {
-        test('should not display the PieNotification component when isOpen is false', async ({ page, mount }) => {
-            // Arrange
-            await mount(PieNotification, {
-                props: {
-                    ...initialValues,
-                    isOpen: false,
-                } as NotificationProps,
-                slots: { default: slotContent },
-            });
-
-            // Act
-            const notification = page.locator(componentSelector);
-
-            // Assert
-            await expect.soft(notification).not.toBeVisible();
-            await percySnapshot(page, 'PieNotification - isOpen = false');
-        });
-    });
-
-    test.describe('PieNotification variants', () => {
-        variants.forEach((variant) => {
-            test(`should render correctly the ${variant} variant`, async ({ page, mount }) => {
-                await mount(PieNotification, {
-                    props: {
-                        ...initialValues,
-                        variant,
-                    } as NotificationProps,
-                    slots: { default: slotContent },
-                });
-
-                await percySnapshot(page, `PieNotification - variant = ${variant}`);
-            });
-        });
-    });
-
     test.describe('PieNotification headingLevels', () => {
         headingLevels.forEach((headingLevel) => {
             test(`should render correctly the ${headingLevel} headingLevel`, async ({ page, mount }) => {
@@ -102,110 +125,6 @@ test.describe('Props', () => {
 
                 await percySnapshot(page, `PieNotification - headingLevel = ${headingLevel} but font size remains the same`);
             });
-        });
-    });
-
-    test.describe('isCompact', () => {
-        test('should not render the close icon if isCompact is true ', async ({ mount, page }) => {
-            // Arrange
-            await mount(PieNotification, {
-                props: {
-                    ...initialValues,
-                    isCompact: true,
-                } as NotificationProps,
-                slots: { default: slotContent },
-            });
-
-            await percySnapshot(page, 'PieNotification - isCompact = true');
-        });
-
-        test('action buttons should render in the same row as the title and content when isCompact is true', async ({ mount, page }) => {
-            // Arrange
-            await mount(PieNotification, {
-                props: {
-                    ...initialValues,
-                    leadingAction: mainAction,
-                    supportingAction: secondaryAction,
-                    isCompact: true,
-                } as NotificationProps,
-                slots: { default: slotContent },
-            });
-
-            await percySnapshot(page, 'PieNotification - isCompact = true - Action buttons');
-        });
-    });
-
-    test.describe('hideIcon', () => {
-        const variantsWithDefaultIcon = ['info', 'success', 'warning', 'error'];
-        variants.filter((variant) => variantsWithDefaultIcon.includes(variant)).forEach((variant) => {
-            test(`should not render the default icon when the variant is ${variant} and hideIcon is true`, async ({ mount, page }) => {
-                // Arrange
-                await mount(PieNotification, {
-                    props: {
-                        ...initialValues,
-                        hideIcon: true,
-                    } as NotificationProps,
-                    slots: { default: slotContent },
-                });
-
-                await percySnapshot(page, `PieNotification - hideIcon = true, variant = ${variant}`);
-            });
-        });
-    });
-
-    test.describe('isDismissible', () => {
-        test('should not show the close icon if isDismissible is false', async ({ mount, page }) => {
-            // Arrange
-            await mount(PieNotification, {
-                props: {
-                    ...initialValues,
-                    isDismissible: false,
-                } as NotificationProps,
-                slots: { default: slotContent },
-            });
-
-            await percySnapshot(page, 'PieNotification - isDismissible = false - Should not show the close icon');
-        });
-
-        test('should not show the close icon if isDismissible is false and isCompact is false', async ({ mount, page }) => {
-            // Arrange
-            await mount(PieNotification, {
-                props: {
-                    ...initialValues,
-                    isDismissible: false,
-                    isCompact: false,
-                } as NotificationProps,
-                slots: { default: slotContent },
-            });
-
-            await percySnapshot(page, 'PieNotification - isDismissible = false, isCompact = false - Should not show the close icon');
-        });
-
-        test('should not show the close icon if isDismissible is true and isCompact is true', async ({ mount, page }) => {
-            // Arrange
-            await mount(PieNotification, {
-                props: {
-                    ...initialValues,
-                    isDismissible: true,
-                    isCompact: true,
-                } as NotificationProps,
-                slots: { default: slotContent },
-            });
-
-            await percySnapshot(page, 'PieNotification - isDismissible = true, isCompact = true - Should not show the close icon');
-        });
-
-        test('should show the close icon if isDismissible is true', async ({ mount, page }) => {
-            // Arrange
-            await mount(PieNotification, {
-                props: {
-                    ...initialValues,
-                    isDismissible: true,
-                } as NotificationProps,
-                slots: { default: slotContent },
-            });
-
-            await percySnapshot(page, 'PieNotification - isDismissible = true - Should show the close icon');
         });
     });
 
