fb640a310dfa8a3cd1f73cdd32a7585f5e31f9fb
personal <jamieolivermaguire@googlemail.com>
2024-03-28 09:37:03 +0000

    remove unnecessary variable

--- apps/pie-storybook/docblocks/component-status.jsx
+++ apps/pie-storybook/docblocks/component-status.jsx
@@ -3,10 +3,8 @@ import '@justeattakeaway/pie-tag';
 import componentStatusesJson from '../component-statuses.json';
 import { tagVariantToStatusMap } from '../data/tag-variants-to-statuses-map';
 
-const componentStatuses = componentStatusesJson;
-
 export const ComponentStatus = ({ component }) => {
-    const componentStatus = componentStatuses[component];
+    const componentStatus = componentStatusesJson[component];
     const tagVariant = tagVariantToStatusMap[componentStatus];
 
     return (
