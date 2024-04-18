7251a9bc70382fc095372b4875d3e04baed6cbef
personal <jamieolivermaguire@googlemail.com>
2024-04-18 10:03:50 +0100

    slot tests

--- packages/components/pie-input/test/visual/pie-input.spec.ts
+++ packages/components/pie-input/test/visual/pie-input.spec.ts
@@ -1,11 +1,9 @@
 import { test } from '@sand4rt/experimental-ct-web';
 import percySnapshot from '@percy/playwright';
 import type {
-    PropObject, WebComponentPropValues, WebComponentTestInput,
+    WebComponentPropValues, WebComponentTestInput,
 } from '@justeattakeaway/pie-webc-testing/src/helpers/defs.ts';
-import {
-    getAllPropCombinations, splitCombinationsByPropertyValue,
-} from '@justeattakeaway/pie-webc-testing/src/helpers/get-all-prop-combos.ts';
+
 import {
     createTestWebComponent,
 } from '@justeattakeaway/pie-webc-testing/src/helpers/rendering.ts';
@@ -13,32 +11,44 @@ import {
     WebComponentTestWrapper,
 } from '@justeattakeaway/pie-webc-testing/src/helpers/components/web-component-test-wrapper/WebComponentTestWrapper.ts';
 import { percyWidths } from '@justeattakeaway/pie-webc-testing/src/percy/breakpoints.ts';
-import { statusTypes } from '../../src/defs.ts';
-import { PieInput, InputProps } from '../../src/index.ts';
+import { IconPlaceholder } from '@justeattakeaway/pie-icons-webc/dist/IconPlaceholder';
+import { PieAssistiveText } from '@justeattakeaway/pie-assistive-text';
+import { PieInput } from '../../src/index.ts';
 
-// const props: PropObject = {
-//     assistiveText:  ['', 'Assistive Text'],
-//     status: statusTypes,
-//     disabled: [true, false],
-//     readonly: [true, false],
-//     value: ['', 'String'],
-//     placeholder: ['', 'Placeholder'],
-// };
+// Renders a <pie-input> HTML string with the given prop values
+type InputSlotOptions = {
+    leadingIcon?: boolean;
+    trailingIcon?: boolean;
+    leadingCharacter?: boolean;
+    trailingCharacter?: boolean;
+};
 
-// // Renders a <pie-input> HTML string with the given prop values
 const renderTestPieInput = (propVals: WebComponentPropValues) => {
     let attributes = '';
+
     if (propVals.size) attributes += ` size="${propVals.size}"`;
     if (propVals.placeholder) attributes += ` placeholder="${propVals.placeholder}"`;
     if (propVals.value) attributes += ` value="${propVals.value}"`;
     if (propVals.status) attributes += ` status="${propVals.status}"`;
     if (propVals.assistiveText) attributes += ` assistiveText="${propVals.assistiveText}"`;
+    // if (propVals.disabled) attributes += ' disabled';
+    // if (propVals.readonly) attributes += ' readonly';
 
     return `<pie-input${attributes}></pie-input>`;
 };
-// const componentPropsMatrix: WebComponentPropValues[] = getAllPropCombinations(props);
-// const componentPropsMatrixByStatus: Record<string, WebComponentPropValues[]> = splitCombinationsByPropertyValue(componentPropsMatrix, 'status');
-// const componentStatus: string[] = Object.keys(componentPropsMatrixByStatus);
+
+// Adds any slots to the component HTML string
+const addSlotsToComponent = (component: string, slotOptions: InputSlotOptions) => {
+    let slots = '';
+
+    if (slotOptions.leadingIcon) slots += '<icon-placeholder slot="leading"></icon-placeholder>';
+    if (slotOptions.trailingIcon) slots += '<icon-placeholder slot="trailing"></icon-placeholder>';
+    if (slotOptions.leadingCharacter) slots += '<pie-assistive-text slot="leading">#</pie-assistive-text>';
+    if (slotOptions.trailingCharacter) slots += '<pie-assistive-text slot="trailing">#</pie-assistive-text>';
+
+    // add slots between > and </pie-input>
+    return component.replace('></pie-input>', `>${slots}</pie-input>`);
+};
 
 test.beforeEach(async ({ mount }, testInfo) => {
     testInfo.setTimeout(testInfo.timeout + 40000);
@@ -47,6 +57,12 @@ test.beforeEach(async ({ mount }, testInfo) => {
     // It appears to add them to a Playwright cache which we understand is required for the tests to work correctly.
     const inputComponent = await mount(PieInput);
     await inputComponent.unmount();
+
+    const iconComponent = await mount(IconPlaceholder);
+    await iconComponent.unmount();
+
+    const assistiveTextComponent = await mount(PieAssistiveText);
+    await assistiveTextComponent.unmount();
 });
 
 test('size variants with value and placeholder', async ({ mount, page }) => {
@@ -86,7 +102,7 @@ test('size variants with value and placeholder', async ({ mount, page }) => {
 });
 
 test('assistive text and status', async ({ mount, page }) => {
-    // Test with value prop
+    // Assistive text with no status
     let testComponent: WebComponentTestInput = createTestWebComponent({ assistiveText: 'Assistive text', value: 'String' }, renderTestPieInput);
     let propKeyValues = `assistiveText: ${testComponent.propValues.value}, value: ${testComponent.propValues.value}`;
 
@@ -138,6 +154,106 @@ test('assistive text and status', async ({ mount, page }) => {
             props: { propKeyValues },
             slots: {
                 component: testComponent.renderedString.trim(),
+            },
+        },
+    );
+
+    await percySnapshot(page, 'PIE Input - Statuses and assistive text', percyWidths);
+});
+
+test('Content', async ({ mount, page }) => {
+    // Trailing + leading icon
+    let testComponent: WebComponentTestInput = createTestWebComponent({ assistiveText: 'Assistive text', value: 'String' }, renderTestPieInput);
+    let componentStringWithSlots = addSlotsToComponent(testComponent.renderedString, { leadingIcon: true, trailingIcon: true });
+
+    let propKeyValues = `assistiveText: ${testComponent.propValues.value}, value: ${testComponent.propValues.value}`;
+
+    await mount(
+        WebComponentTestWrapper,
+        {
+            props: { propKeyValues },
+            slots: {
+                component: componentStringWithSlots.trim(),
+            },
+        },
+    );
+
+    // Leading icon
+    testComponent = createTestWebComponent({ assistiveText: 'Assistive text', value: 'String' }, renderTestPieInput);
+    componentStringWithSlots = addSlotsToComponent(testComponent.renderedString, { leadingIcon: true });
+
+    propKeyValues = `assistiveText: ${testComponent.propValues.value}, value: ${testComponent.propValues.value}`;
+
+    await mount(
+        WebComponentTestWrapper,
+        {
+            props: { propKeyValues },
+            slots: {
+                component: componentStringWithSlots.trim(),
+            },
+        },
+    );
+
+    // Trailing icon
+    testComponent = createTestWebComponent({ assistiveText: 'Assistive text', value: 'String' }, renderTestPieInput);
+    componentStringWithSlots = addSlotsToComponent(testComponent.renderedString, { trailingIcon: true });
+
+    propKeyValues = `assistiveText: ${testComponent.propValues.value}, value: ${testComponent.propValues.value}`;
+
+    await mount(
+        WebComponentTestWrapper,
+        {
+            props: { propKeyValues },
+            slots: {
+                component: componentStringWithSlots.trim(),
+            },
+        },
+    );
+
+    // Leading + Trailing text
+    testComponent = createTestWebComponent({ assistiveText: 'Assistive text', value: 'String' }, renderTestPieInput);
+    componentStringWithSlots = addSlotsToComponent(testComponent.renderedString, { leadingCharacter: true, trailingCharacter: true });
+
+    propKeyValues = `assistiveText: ${testComponent.propValues.value}, value: ${testComponent.propValues.value}`;
+
+    await mount(
+        WebComponentTestWrapper,
+        {
+            props: { propKeyValues },
+            slots: {
+                component: componentStringWithSlots.trim(),
+            },
+        },
+    );
+
+    // Leading text
+    testComponent = createTestWebComponent({ assistiveText: 'Assistive text', value: 'String' }, renderTestPieInput);
+    componentStringWithSlots = addSlotsToComponent(testComponent.renderedString, { leadingCharacter: true });
+
+    propKeyValues = `assistiveText: ${testComponent.propValues.value}, value: ${testComponent.propValues.value}`;
+
+    await mount(
+        WebComponentTestWrapper,
+        {
+            props: { propKeyValues },
+            slots: {
+                component: componentStringWithSlots.trim(),
+            },
+        },
+    );
+
+    // Trailing text
+    testComponent = createTestWebComponent({ assistiveText: 'Assistive text', value: 'String' }, renderTestPieInput);
+    componentStringWithSlots = addSlotsToComponent(testComponent.renderedString, { trailingCharacter: true });
+
+    propKeyValues = `assistiveText: ${testComponent.propValues.value}, value: ${testComponent.propValues.value}`;
+
+    await mount(
+        WebComponentTestWrapper,
+        {
+            props: { propKeyValues },
+            slots: {
+                component: componentStringWithSlots.trim(),
             },
         },
     );
