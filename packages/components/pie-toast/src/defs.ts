d193da6ee60a2ae49611cf7611abbfeca117c30d
Jose De Freitas Jr <jose.defreitasjr@justeattakeaway.com>
2024-06-24 14:02:41 -0600

    feat(pie-toast): DSW-2121 implement foundational types and template functions

--- packages/components/pie-toast/src/defs.ts
+++ packages/components/pie-toast/src/defs.ts
@@ -1,3 +1,65 @@
+import { type ComponentDefaultPropsGeneric } from '@justeattakeaway/pie-webc-core';
+
 // TODO - please remove the eslint disable comment below when you add props to this interface
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ToastProps {}
+export type ActionProps = {
+  /**
+   * The text to display inside the button.
+   */
+  text: string;
+
+  /**
+   * The ARIA label for the button.
+   */
+  ariaLabel?: string;
+};
+
+export interface ToastProps {
+  /**
+   * When true, the notification is set to be open and visible.
+   */
+  isOpen?: boolean;
+  /**
+   * When true, allows dismissing the notification by clicking on the close button.
+   */
+  isDismissible?: boolean;
+  /**
+   * The message content of the toast.
+   */
+  message?: string;
+  /**
+   * The leading action for the toast.
+   */
+  leadingAction?: ActionProps;
+}
+
+export const componentSelector = 'pie-toast';
+export const componentClass = 'c-toast';
+
+/**
+ * Event name for when the toast is closed.
+ *
+ * @constant
+ */
+export const ON_TOAST_CLOSE_EVENT = `${componentSelector}-close`;
+
+/**
+ * Event name for when the toast is opened.
+ *
+ * @constant
+ */
+export const ON_TOAST_OPEN_EVENT = `${componentSelector}-open`;
+
+/**
+ * Event name for when the toast leading action is clicked.
+ *
+ * @constant
+ */
+export const ON_TOAST_LEADING_ACTION_CLICK_EVENT = `${componentSelector}-leading-action-click`;
+
+export type DefaultProps = ComponentDefaultPropsGeneric<ToastProps, 'isOpen' | 'isDismissible'>;
+
+export const defaultProps: DefaultProps = {
+    isOpen: true,
+    isDismissible: true,
+};
--- packages/components/pie-toast/src/index.ts
+++ packages/components/pie-toast/src/index.ts
@@ -1,24 +1,144 @@
-import { LitElement, html, unsafeCSS } from 'lit';
-import { RtlMixin, defineCustomElement } from '@justeattakeaway/pie-webc-core';
+import {
+    LitElement,
+    html,
+    unsafeCSS,
+    nothing,
+    type TemplateResult,
+} from 'lit';
+import { property } from 'lit/decorators.js';
+import { RtlMixin, defineCustomElement, dispatchCustomEvent } from '@justeattakeaway/pie-webc-core';
+import '@justeattakeaway/pie-button';
 
 import styles from './toast.scss?inline';
-import { ToastProps } from './defs';
+import {
+    ToastProps,
+    componentSelector,
+    componentClass,
+    ActionProps,
+    ON_TOAST_CLOSE_EVENT,
+    ON_TOAST_OPEN_EVENT,
+    ON_TOAST_LEADING_ACTION_CLICK_EVENT,
+    defaultProps,
+} from './defs';
 
 // Valid values available to consumers
 export * from './defs';
-
-const componentSelector = 'pie-toast';
 
 /**
  * @tagname pie-toast
  */
 export class PieToast extends RtlMixin(LitElement) implements ToastProps {
-    render () {
-        return html`<h1 data-test-id="pie-toast">Hello world!</h1>`;
-    }
+    @property({ type: Boolean })
+    public isOpen = defaultProps.isOpen;
+
+    @property({ type: Boolean })
+    public isDismissible = defaultProps.isDismissible;
+
+    @property({ type: String })
+    public message?: string;
+
+    @property({ type: Object })
+    public leadingAction: ToastProps['leadingAction'];
 
     // Renders a `CSSResult` generated from SCSS by Vite
     static styles = unsafeCSS(styles);
+
+    /**
+     * It handle the action button action.
+     *
+     * @private
+     */
+    private handleActionClick () {
+        dispatchCustomEvent(this, ON_TOAST_LEADING_ACTION_CLICK_EVENT, { targetNotification: this });
+    }
+
+    /**
+     * Render the action button depending on action type and its action.
+     *
+     * @param {ActionProps} action - The action properties.
+     *
+     * @returns {TemplateResult | typeof nothing} - The rendered action button or nothing if the action text is not defined.
+     * @private
+     */
+    private renderActionButton (action: ActionProps) : TemplateResult | typeof nothing {
+        const { text, ariaLabel } = action;
+
+        return html`
+            <pie-button
+                variant="ghost"
+                size="small-productive"
+                aria-label="${ariaLabel || nothing}"
+                @click="${() => this.handleActionClick()}"
+                data-test-id="${componentSelector}-leading-action"
+                type="button">
+                ${text}
+            </pie-button>
+        `;
+    }
+
+    /**
+     * Template for the footer area
+     * Called within the main render function.
+     *
+     * @private
+     */
+    private renderFooter () {
+        const { leadingAction } = this;
+        return html`
+            <footer class="${componentClass}-footer" data-test-id="${componentSelector}-footer" >
+                ${leadingAction ? this.renderActionButton(leadingAction) : nothing}
+            </footer>
+        `;
+    }
+
+    /**
+     * Template for the close button element. Called within the
+     * main render function.
+     *
+     * @private
+     */
+    private renderCloseButton (): TemplateResult {
+        return html`
+            <pie-icon-button
+                variant="ghost-secondary"
+                size="small"
+                class="${componentClass}-icon-close"
+                data-test-id="${componentSelector}-icon-close"
+                @click="${this.handleCloseButton}">
+                <icon-close></icon-close>
+            </pie-icon-button>`;
+    }
+
+    /**
+     * It handles the action when user clicks in the close button.
+     * Also triggers an event when is executed.
+     *
+     * @private
+     */
+    private handleCloseButton () {
+        this.closeToastComponent();
+        dispatchCustomEvent(this, ON_TOAST_CLOSE_EVENT, { targetNotification: this });
+    }
+
+    /**
+     * Util method responsible to close the component.
+     *
+     * @private
+     */
+    private closeToastComponent () {
+        this.isOpen = false;
+    }
+
+    render () {
+        const { message, isDismissible, leadingAction } = this;
+
+        return html`
+            <div data-test-id="${componentSelector}" class="${componentClass}">
+                ${message}
+                ${isDismissible ? this.renderCloseButton() : nothing}
+                ${leadingAction?.text ? this.renderFooter() : nothing}
+            </div>`;
+    }
 }
 
 defineCustomElement(componentSelector, PieToast);
