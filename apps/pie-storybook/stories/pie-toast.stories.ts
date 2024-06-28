e22a6313802d71d6c19c81aa667d62cca49cadcc
Jose De Freitas Jr <jose.defreitasjr@justeattakeaway.com>
2024-06-28 08:49:48 -0600

    feat(pie-toast): DSW-2121 added storybook knobs

--- apps/pie-storybook/stories/pie-toast.stories.ts
+++ apps/pie-storybook/stories/pie-toast.stories.ts
@@ -2,7 +2,8 @@ import { html } from 'lit';
 
 /* eslint-disable import/no-duplicates */
 import '@justeattakeaway/pie-toast';
-import { ToastProps } from '@justeattakeaway/pie-toast';
+import { ToastProps, defaultProps } from '@justeattakeaway/pie-toast';
+import { action } from '@storybook/addon-actions';
 /* eslint-enable import/no-duplicates */
 
 import { type StoryMeta } from '../types';
@@ -10,27 +11,85 @@ import { createStory } from '../utilities';
 
 type ToastStoryMeta = StoryMeta<ToastProps>;
 
-const defaultArgs: ToastProps = {};
+const defaultArgs: ToastProps = {
+    ...defaultProps,
+    message: 'Menu Published',
+    leadingAction: {
+        text: 'Confirm',
+        ariaLabel: 'Descriptive confirmation text',
+    },
+};
 
 const toastStoryMeta: ToastStoryMeta = {
     title: 'Toast',
     component: 'pie-toast',
-    argTypes: {},
+    argTypes: {
+        isOpen: {
+            description: 'When true, the toast is set to be open and visible.',
+            control: 'boolean',
+            defaultValue: {
+                summary: defaultProps.isOpen,
+            },
+        },
+        isDismissible: {
+            description: 'Allows dismissing the toast by clicking on the close button.',
+            control: 'boolean',
+            defaultValue: {
+                summary: defaultProps.isDismissible,
+            },
+        },
+        message: {
+            description: 'The message content of the toast.',
+            control: 'text',
+        },
+        isMultiline: {
+            description: 'It allows the message content being displayed as multiline limited to three rows.',
+            control: 'boolean',
+            defaultValue: {
+                summary: defaultProps.isOpen,
+            },
+        },
+        leadingAction: {
+            description: 'The leading action configuration for the toast.',
+            control: 'object',
+        },
+    },
     args: defaultArgs,
     parameters: {
         design: {
             type: 'figma',
-            url: '',
+            url: 'https://www.figma.com/design/pPSC73rPin4csb8DiK1CRr/branch/sjkhCbCcv7t4QkDrz7g0Xx/%E2%9C%A8-[Core]-Web-Components-[PIE-3]?node-id=7934-12060',
         },
     },
 };
 
 export default toastStoryMeta;
 
+const pieToastLeadingActionClick = action('pie-toast-leading-action-click');
+const pieToastSupportingActionClick = action('pie-toast-supporting-action-click');
+const pieToastClose = action('pie-toast-close');
+const pieToastOpen = action('pie-toast-open');
+
 // TODO: remove the eslint-disable rule when props are added
 // eslint-disable-next-line no-empty-pattern
-const Template = ({}: ToastProps) => html`
-    <pie-toast></pie-toast>
+const Template = ({
+    isOpen,
+    isDismissible,
+    message,
+    leadingAction,
+    isMultiline,
+}: ToastProps) => html`
+    <pie-toast 
+        ?isOpen="${isOpen}" 
+        ?isDismissible="${isDismissible}" 
+        message="${message}" 
+        ?isMultiline="${isMultiline}"
+        .leadingAction="${leadingAction}"
+        @pie-toast-leading-action-click="${pieToastLeadingActionClick}"
+        @pie-toast-supporting-action-click="${pieToastSupportingActionClick}"
+        @pie-toast-close="${pieToastClose}"
+        @pie-toast-open="${pieToastOpen}"
+    /></pie-toast>
 `;
 
 export const Default = createStory<ToastProps>(Template, defaultArgs)();
