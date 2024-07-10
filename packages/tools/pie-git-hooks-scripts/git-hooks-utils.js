59d9ef739c46ab88ef2fca5563817af95a9195c0
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2024-07-10 16:09:32 +0100

    chore(pie-monorepo): DSW-000 update commit message verification to allow merge commits (#1586)
    * chore(pie-monorepo): DSW-000 test message
    
    * chore(pie-monorepo): DSW-000 update commit message verification to allow merge commits
    
    * Update regex and matcher logic

--- packages/tools/pie-git-hooks-scripts/git-hooks-utils.js
+++ packages/tools/pie-git-hooks-scripts/git-hooks-utils.js
@@ -39,6 +39,9 @@ function verifyCommitMessage (commitMessage, ticketId) {
     // Process commit message and check if it already has a ticket id
     const match = commitMessage.match(/^(\w*)\((\w.*)\): (\w+?-\d{1,7}) (\w.*)/);
 
+    const isMergeCommit = commitMessage.trim().match(/^Merge branch '[\w-]+' into [\w-]+\n?/);
+    if (isMergeCommit !== null) return commitMessage.trim();
+
     // If the commit message doesn't have a ticket id, extract the existing message parts and add the ticket id
     if (!match) {
         const commitMessageParts = commitMessage.match(/^(\w*)\((\w.*)\): (\w.*)/);
