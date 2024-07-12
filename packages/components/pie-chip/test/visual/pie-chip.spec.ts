ae240d71a902569c45e33898b09c88f9ac0b5895
Josh Ng <joshua.ng2332@gmail.com>
2024-07-12 11:35:54 +0100

    refactor(pie-chip): DSW-1969 refactored promise all out

--- packages/components/pie-chip/test/visual/pie-chip.spec.ts
+++ packages/components/pie-chip/test/visual/pie-chip.spec.ts
@@ -45,7 +45,8 @@ test.beforeEach(async ({ mount }, testInfo) => {
 });
 
 componentVariants.forEach((variant) => test(`should render all prop variations for Variant: ${variant}`, async ({ page, mount }) => {
-    await Promise.all(componentPropsMatrixByVariant[variant].map(async (combo: WebComponentPropValues) => {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const combo of componentPropsMatrixByVariant[variant]) {
         const testComponent: WebComponentTestInput = createTestWebComponent(combo, renderTestPieChip);
         const propKeyValues = `
             variant: ${testComponent.propValues.variant},
@@ -55,6 +56,7 @@ componentVariants.forEach((variant) => test(`should render all prop variations f
             disabled: ${testComponent.propValues.disabled},
             iconSlot: ${testComponent.propValues.iconSlot ? 'with icon' : 'no icon'}`;
 
+        // eslint-disable-next-line no-await-in-loop
         await mount(
             WebComponentTestWrapper,
             {
@@ -64,7 +66,7 @@ componentVariants.forEach((variant) => test(`should render all prop variations f
                 },
             },
         );
-    }));
+    }
 
     // Follow up to remove in Jan
     await page.waitForTimeout(5000);
