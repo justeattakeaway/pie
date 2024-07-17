accd23f09b65e91ed3b5d0abd9dbe23fcbecf34b
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2024-07-17 14:36:16 +0100

    Update logging

--- packages/components/pie-modal/src/index.ts
+++ packages/components/pie-modal/src/index.ts
@@ -113,6 +113,7 @@ export class PieModal extends RtlMixin(LitElement) implements ModalProps {
 
     constructor () {
         super();
+        console.log('isOpen in constructor', this.isOpen);
         console.log('leadingAction in constructor', this.leadingAction);
         console.log('position in constructor', this.position);
         console.log('size in constructor', this.size);
@@ -134,7 +135,7 @@ export class PieModal extends RtlMixin(LitElement) implements ModalProps {
     }
 
     async firstUpdated (changedProperties: PropertyValues<this>) : Promise<void> {
-        console.log('initial open state in firstUpdated', this.isOpen);
+        console.log('isOpen in firstUpdated', this.isOpen);
         console.log('leadingAction in firstUpdated', this.leadingAction);
         console.log('position in firstUpdated', this.position);
         console.log('size in firstUpdated', this.size);
@@ -217,10 +218,8 @@ export class PieModal extends RtlMixin(LitElement) implements ModalProps {
     private _handleModalOpenStateOnFirstRender (changedProperties: PropertyValues<this>) : void {
         // This ensures if the modal is open on first render, the scroll lock and backdrop are applied
         const previousValue = changedProperties.get('isOpen');
-        console.log('previous value in firstUpdated', previousValue);
 
         if (previousValue === undefined && this.isOpen) {
-            console.log('Dispatching open event on first render');
             dispatchCustomEvent(this, ON_MODAL_OPEN_EVENT, { targetModal: this });
         }
     }
