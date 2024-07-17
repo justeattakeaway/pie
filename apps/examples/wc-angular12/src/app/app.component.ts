f41d35bd555ef1b8010427444c467dddecd99fc7
Kevin Rodrigues <kevrodrigues116@gmail.com>
2024-07-17 15:06:04 +0100

    feat(pie-monorepo): DSW-2056 added pie-webc integration to angular examples app

--- apps/examples/wc-angular12/src/app/app.component.ts
+++ apps/examples/wc-angular12/src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { ComponentDefaultProps } from '@justeattakeaway/pie-webc-core';
+// Or use the appropriate export if ComponentDefaultPropsGeneric was a typo
+
 import { Component } from '@angular/core';
-import { ButtonProps, sizes, variants } from '@justeattakeaway/pie-button';
+import { ButtonProps, sizes, variants } from '@justeattakeaway/pie-webc/components/button.js';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
     title = 'wc-angular12';
 
@@ -15,15 +17,15 @@ export class AppComponent {
     variantIndex = 0;
     variantName = 'primary';
 
-    increment () {
+    increment() {
         this.count++;
     }
 
-    decrement () {
+    decrement() {
         this.count--;
     }
 
-    switchVariant () {
+    switchVariant() {
         this.variantIndex += 1;
         const variant: ButtonProps['variant'] = variants[this.variantIndex % variants.length];
 
