ba2529278b05ebf3359124d3f40b4e69b26acabe
Mariya Radchuk <maryia.radchuk@justeattakeaway.com>
2024-07-18 12:17:21 +0200

    feat(pie-checkbox-group): DSW-2182 main component functionality (#1581)
    * feat(pie-checkbox-group): DSW-2182 main component functionality
    
    * feat(pie-checkbox-group): DSW-2182 removes aria prop from checkbox and checkbox-group
    
    * feat(pie-checkbox-group): DSW-2182 enabling/disabling children + aria attr updates
    
    * feat(pie-checkbox-group): DSW-2182 pie-checkbox-group-disabled custom event
    
    * feat(pie-checkbox-group): DSW-2182 name attribute + connected and disconncted callbacks changes
    
    * feat(pie-checkbox-group): DSW-2182 removes aria prop from checkbox story
    
    * Update apps/pie-storybook/stories/pie-checkbox-group.stories.ts
    
    Co-authored-by: leksaBoiko <168754213+leksaBoiko@users.noreply.github.com>
    
    * Update packages/components/pie-checkbox-group/README.md
    
    Co-authored-by: Xander Marjoram <xander.marjoram@justeattakeaway.com>
    
    * feat(pie-checkbox-group): DSW-2182 queryAssignedElements decorator
    
    ---------
    
    Co-authored-by: leksaBoiko <168754213+leksaBoiko@users.noreply.github.com>
    Co-authored-by: Xander Marjoram <xander.marjoram@justeattakeaway.com>

--- apps/pie-storybook/stories/pie-checkbox-group.stories.ts
+++ apps/pie-storybook/stories/pie-checkbox-group.stories.ts
@@ -1,36 +1,97 @@
 import { html } from 'lit';
-
+import { ifDefined } from 'lit/directives/if-defined.js';
 /* eslint-disable import/no-duplicates */
 import '@justeattakeaway/pie-checkbox-group';
-import { CheckboxGroupProps } from '@justeattakeaway/pie-checkbox-group';
+import { CheckboxGroupProps, defaultProps, statusTypes } from '@justeattakeaway/pie-checkbox-group';
 /* eslint-enable import/no-duplicates */
 
 import { type StoryMeta } from '../types';
 import { createStory } from '../utilities';
 
+import '@justeattakeaway/pie-link';
+import '@justeattakeaway/pie-checkbox';
+
 type CheckboxGroupStoryMeta = StoryMeta<CheckboxGroupProps>;
 
-const defaultArgs: CheckboxGroupProps = {};
+const defaultArgs: CheckboxGroupProps = {
+    ...defaultProps,
+};
 
 const checkboxGroupStoryMeta: CheckboxGroupStoryMeta = {
     title: 'Checkbox Group',
     component: 'pie-checkbox-group',
-    argTypes: {},
+    argTypes: {
+        name: {
+            description: 'The name associated with the group.',
+            control: 'text',
+        },
+        label: {
+            description: 'The visible label for the checkbox group.',
+            control: 'text',
+        },
+        status: {
+            description: 'The status of the checkbox group component / assistive text. Can be default, success or error.',
+            control: 'select',
+            options: statusTypes,
+            defaultValue: {
+                summary: defaultProps.status,
+            },
+        },
+        disabled: {
+            description: 'If true, disables the whole checkbox group. If you need to disable only one checkbox pass the disabled prop to the needed checkbox.',
+            control: 'boolean',
+            defaultValue: {
+                summary: defaultArgs.disabled,
+            },
+        },
+        assistiveText: {
+            description: 'An optional assistive text to display below the checkbox group.',
+            control: 'text',
+            defaultValue: {
+                summary: '',
+            },
+        },
+    },
     args: defaultArgs,
     parameters: {
         design: {
             type: 'figma',
-            url: '',
+            url: 'https://www.figma.com/file/aD4m0j97Ruw8Q4S5lED2Bl/Checkbox-audit?node-id=9938-14138&t=1V1VEYmtArFih6AX-4',
         },
     },
 };
 
 export default checkboxGroupStoryMeta;
 
-// TODO: remove the eslint-disable rule when props are added
-// eslint-disable-next-line no-empty-pattern
-const Template = ({}: CheckboxGroupProps) => html`
-    <pie-checkbox-group></pie-checkbox-group>
+const Template = ({
+    name,
+    label,
+    assistiveText,
+    status,
+    disabled,
+}: CheckboxGroupProps) => html`
+    <p>Please note, the checkboxes are separate components. See
+    <pie-link href="/?path=/story/checkbox--default">pie-checkbox</pie-link>.</p>
+    <pie-checkbox-group
+        name="${ifDefined(name)}"
+        label="${ifDefined(label)}"
+        assistiveText="${ifDefined(assistiveText)}"
+        status=${ifDefined(status)}
+        ?disabled="${disabled}"
+    >
+        <pie-checkbox
+            name="checkbox-one"
+            label="checkbox 1">
+        </pie-checkbox>
+        <pie-checkbox
+            name="checkbox-two"
+            label="checkbox 2">
+        </pie-checkbox>
+        <pie-checkbox
+            name="checkbox-three"
+            label="checkbox 3">
+        </pie-checkbox>
+    </pie-checkbox-group>
 `;
 
 export const Default = createStory<CheckboxGroupProps>(Template, defaultArgs)();
