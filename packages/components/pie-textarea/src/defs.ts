107376eea72817fd913823a0bf1ff22397d76793
leksaBoiko <oleksandra.boiko@justeattakeaway.com>
2024-07-23 14:10:42 +0300

    feat(pie-textarea): DSW-2132 add textarea form functionality

--- packages/components/pie-textarea/src/defs.ts
+++ packages/components/pie-textarea/src/defs.ts
@@ -20,17 +20,57 @@ export interface TextareaProps {
      * When set to `manual`, the textarea will not resize automatically but can be resized by the user.
      */
     resize?: typeof resizeModes[number];
+
+    /**
+     * The value of the textarea (used as a key/value pair in HTML forms with `name`).
+     */
+    value: string;
+
+    /**
+     * The name of the textarea (used as a key/value pair with `value`). This is required in order to work properly with forms.
+     */
+    name?: string;
+
+    /**
+     * Allows the user to enable or disable autocomplete functionality on the textarea field.
+     * See [MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete) for more information and values.
+     */
+    autocomplete?: string;
+
+    /**
+     * If true, the textarea will be focused on the first render.
+     * No more than one element in the document or dialog may have the autofocus attribute. If applied to multiple elements the first one will receive focus.
+     * See [MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/autofocus) for more information.
+     */
+    autoFocus?: boolean;
+
+    /**
+     * When true, the user cannot edit the control. Not the same as disabled.
+     * See [MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/readonly) for more information.
+     */
+    readonly?: boolean;
+
+    /**
+     * An optional default value to use when the textarea is reset.
+     */
+    defaultValue?: string;
+
+    /**
+     * If true, the textarea is required to have a value before submitting the form. If there is no value, then the component validity state will be invalid.
+     */
+    required?: boolean;
 }
 
 /**
  * The default values for the `TextareaProps` that are required (i.e. they have a fallback value in the component).
  */
-type DefaultProps = ComponentDefaultProps<TextareaProps>;
+type DefaultProps = ComponentDefaultProps<TextareaProps, 'value' | 'disabled' | 'size' | 'resize'>;
 
 /**
  * Default values for optional properties that have default fallback values in the component.
  */
 export const defaultProps: DefaultProps = {
+    value: '',
     disabled: false,
     size: 'medium',
     resize: 'auto',
--- packages/components/pie-textarea/src/index.ts
+++ packages/components/pie-textarea/src/index.ts
@@ -2,9 +2,13 @@ import {
     LitElement, html, unsafeCSS, PropertyValues,
 } from 'lit';
 import { property, query } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { live } from 'lit/directives/live.js';
 import throttle from 'lodash.throttle';
 
-import { validPropertyValues, RtlMixin, defineCustomElement } from '@justeattakeaway/pie-webc-core';
+import {
+    validPropertyValues, RtlMixin, defineCustomElement, FormControlMixin, wrapNativeEvent,
+} from '@justeattakeaway/pie-webc-core';
 
 import styles from './textarea.scss?inline';
 import {
@@ -18,9 +22,17 @@ const componentSelector = 'pie-textarea';
 
 /**
  * @tagname pie-textarea
+ * @event {InputEvent} input - when the textarea value is changed.
+ * @event {CustomEvent} change - when the textarea value is changed.
  */
-export class PieTextarea extends RtlMixin(LitElement) implements TextareaProps {
+export class PieTextarea extends FormControlMixin(RtlMixin(LitElement)) implements TextareaProps {
     static shadowRootOptions = { ...LitElement.shadowRootOptions, delegatesFocus: true };
+
+    @property({ type: String })
+    public value = defaultProps.value;
+
+    @property({ type: String })
+    public name?: TextareaProps['name'];
 
     @property({ type: Boolean, reflect: true })
     public disabled = defaultProps.disabled;
@@ -33,6 +45,21 @@ export class PieTextarea extends RtlMixin(LitElement) implements TextareaProps {
     @validPropertyValues(componentSelector, resizeModes, defaultProps.resize)
     public resize = defaultProps.resize;
 
+    @property({ type: String })
+    public autocomplete?: TextareaProps['autocomplete'];
+
+    @property({ type: Boolean })
+    public autoFocus?: TextareaProps['autoFocus'];
+
+    @property({ type: Boolean })
+    public readonly?: TextareaProps['readonly'];
+
+    @property({ type: String })
+    public defaultValue?: TextareaProps['defaultValue'];
+
+    @property({ type: Boolean })
+    public required?: TextareaProps['required'] = false;
+
     @query('textarea')
     private _textarea!: HTMLTextAreaElement;
 
@@ -43,21 +70,89 @@ export class PieTextarea extends RtlMixin(LitElement) implements TextareaProps {
         }
     }, 100);
 
+    /**
+     * (Read-only) returns a ValidityState with the validity states that this element is in.
+     * https://developer.mozilla.org/en-US/docs/Web/API/HTMLObjectElement/validity
+     */
+    public get validity (): ValidityState {
+        return (this._textarea as HTMLTextAreaElement).validity;
+    }
+
+    /**
+     * Called after the disabled state of the element changes,
+     * either because the disabled attribute of this element was added or removed;
+     * or because the disabled state changed on a <fieldset> that's an ancestor of this element.
+     * @param disabled - The latest disabled state of the input.
+     */
+    public formDisabledCallback (disabled: boolean): void {
+        this.disabled = disabled;
+    }
+
+    /**
+     * Called when the form that owns this component is reset.
+     * Resets the value to the default value.
+     */
+    public formResetCallback (): void {
+        this.value = this.defaultValue ?? defaultProps.value;
+
+        // This ensures the input value is updated when the form is reset.
+        // Otherwise, there is a bug where values like 'e1212' for number inputs do not correctly reset.
+        if (this._textarea) {
+            this._textarea.value = this.value;
+        }
+
+        this._internals.setFormValue(this.value);
+    }
+
+    protected firstUpdated (_changedProperties: PropertyValues<this>): void {
+        super.firstUpdated(_changedProperties);
+        this._internals.setFormValue(this.value);
+    }
+
     private handleResize () {
         this._throttledResize();
     }
 
-    updated (changedProperties: PropertyValues<this>) {
+    protected updated (changedProperties: PropertyValues<this>) {
         if (this.resize === 'auto' && (changedProperties.has('resize') || changedProperties.has('size'))) {
             this.handleResize();
         }
+
+        if (changedProperties.has('value')) {
+            this._internals.setFormValue(this.value);
+        }
     }
+
+    /**
+     * Handles data processing in response to the input event. The native input event is left to bubble up.
+     * @param event - The input event.
+     */
+    private handleInput = (event: InputEvent) => {
+        this.value = (event.target as HTMLTextAreaElement).value;
+        this._internals.setFormValue(this.value);
+    };
+
+    private handleChange = (event: Event) => {
+        // We have to create our own change event because the native one
+        // does not penetrate the shadow boundary.
+
+        // This is because some events set `composed` to `false`.
+        // Reference: https://javascript.info/shadow-dom-events#event-composed
+        const customChangeEvent = wrapNativeEvent(event);
+        this.dispatchEvent(customChangeEvent);
+    };
 
     render () {
         const {
             disabled,
             resize,
             size,
+            autocomplete,
+            autoFocus,
+            name,
+            readonly,
+            value,
+            required,
         } = this;
 
         return html`
@@ -67,9 +162,16 @@ export class PieTextarea extends RtlMixin(LitElement) implements TextareaProps {
                 data-pie-size="${size}"
                 data-pie-resize="${resize}">
                 <textarea
-                    data-test-id="pie-textarea"
-                    @input=${this.handleResize}
+                    name=${ifDefined(name)}
+                    autocomplete=${ifDefined(autocomplete)}
+                    .value=${live(value)}
+                    ?autofocus=${autoFocus}
+                    ?readonly=${readonly}
+                    ?required=${required}
                     ?disabled=${disabled}
+                    @input=${this.handleInput}
+                    @change=${this.handleChange}
+                    data-test-id="pie-textarea"
                 ></textarea>
             </div>`;
     }
