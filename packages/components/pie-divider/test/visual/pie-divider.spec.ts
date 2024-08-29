0c1c50146fa4194df7d88f0768cfe22300b4cc20
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2024-08-29 16:11:48 +0100

    Changes from self-review

--- packages/components/pie-divider/test/visual/pie-divider.spec.ts
+++ packages/components/pie-divider/test/visual/pie-divider.spec.ts
@@ -14,7 +14,7 @@ import {
     WebComponentTestWrapper,
 } from '@justeattakeaway/pie-webc-testing/src/helpers/components/web-component-test-wrapper/WebComponentTestWrapper.ts';
 import { percyWidths } from '@justeattakeaway/pie-webc-testing/src/percy/breakpoints.ts';
-import { variants, orientations, DividerProps } from '../../src/defs.ts';
+import { variants, orientations, type DividerProps } from '../../src/defs.ts';
 import { PieDivider } from '../../src/index.ts';
 
 const props: PropObject<DividerProps> = {
