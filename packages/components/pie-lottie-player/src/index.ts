a474e6f08245a8597518458743b401ac089cfb8f
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2024-08-29 10:46:35 +0200

    feat(pie-lottie-player): DSW-2365 update animation methods
    feat(pie-lottie-player): DSW-2365 update loop and play to use getters and setters
    
    feat(pie-lottie-player): DSW-2365 implement speed and direction as getters/setters
    
    feat(pie-lottie-player): DSW-2365 assign type

--- packages/components/pie-lottie-player/src/index.ts
+++ packages/components/pie-lottie-player/src/index.ts
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit';
+import { LitElement, PropertyValues, html } from 'lit';
 import { property, query } from 'lit/decorators.js';
 // @ts-expect-error - Type is defined below
 import lottieLight from 'lottie-web/build/player/esm/lottie_light_canvas.min.js';
@@ -19,22 +19,21 @@ const lottie = lottieLight as typeof Lottie;
  * @tagname pie-lottie-player
  */
 export class PieLottiePlayer extends LitElement implements LottiePlayerProps {
-    @property({ type: String })
-    private _animationSrc?: string;
-
-    @property({ type: Object })
-    private _animationData?: object;
-
-    @property({ type: Boolean })
-    public loop = defaultProps.loop;
-
-    @property({ type: Boolean })
-    public autoPlay = defaultProps.autoPlay;
-
     @query('div')
     private hostElement!: HTMLDivElement;
 
     private animationInstance?: AnimationItem;
+
+    private _autoPlay = defaultProps.autoPlay;
+    private _loop = defaultProps.loop;
+    private _speed = defaultProps.speed;
+    private _direction = defaultProps.direction;
+    private _animationSrc = '';
+    private _animationData?:object;
+
+    protected firstUpdated (_changedProperties: PropertyValues): void {
+        this._loadAnimation();
+    }
 
     disconnectedCallback () {
         super.disconnectedCallback();
@@ -58,7 +57,7 @@ export class PieLottiePlayer extends LitElement implements LottiePlayerProps {
                 path: this.animationSrc,
             });
         } catch (error:any) {
-            console.error(`The animation couldn't be played: ${error.message}`);
+            console.error(`PieLottiePlayer: the animation couldn't be played. Error: "${error.message}"`);
         }
     }
 
@@ -67,26 +66,84 @@ export class PieLottiePlayer extends LitElement implements LottiePlayerProps {
         this.animationInstance.destroy();
     }
 
+    // Getter and Setter for animationSrc
+    @property({ type: String, reflect: true })
     get animationSrc () {
         return this._animationSrc;
     }
 
-    set animationSrc (newValue) {
-        this._animationSrc = newValue;
+    set animationSrc (value) {
+        this._animationSrc = value;
+        // When animationSrc changes, reset animationData to undefined
         this._animationData = undefined;
-
         this._loadAnimation();
     }
 
+    // Getter and Setter for animationData
+    @property({ type: Object })
     get animationData () {
         return this._animationData;
     }
 
-    set animationData (newValue) {
-        this._animationData = newValue;
-        this._animationSrc = undefined;
-
+    set animationData (value) {
+        this._animationData = value;
+        // When animationData changes, reset animationSrc to an empty string
+        this._animationSrc = '';
         this._loadAnimation();
+    }
+
+    @property({ type: Boolean, reflect: true })
+    get autoPlay () {
+        return this._autoPlay;
+    }
+
+    set autoPlay (value) {
+        if (!this.animationInstance) return;
+        this._autoPlay = value;
+        this.animationInstance.autoplay = value;
+    }
+
+    @property({ type: Boolean, reflect: true })
+    get loop () {
+        return this._loop;
+    }
+
+    set loop (value) {
+        if (!this.animationInstance) return;
+        this._loop = value;
+        this.animationInstance.loop = value;
+    }
+
+    @property({ type: Number, reflect: true })
+    get speed () {
+        return this._speed;
+    }
+
+    set speed (value) {
+        if (!this.animationInstance) return;
+        this._speed = value;
+        this.animationInstance.setSpeed(value);
+    }
+
+    @property({ type: String, reflect: true })
+    get direction ():AnimationDirection {
+        return this._direction;
+    }
+
+    set direction (value) {
+        if (!this.animationInstance) return;
+        if (!value) return;
+
+        this._direction = value;
+
+        switch (this._direction) {
+            case 'reverse':
+                this.animationInstance.setDirection(-1);
+                break;
+            default:
+                this.animationInstance.setDirection(1);
+                break;
+        }
     }
 
     public play (): void {
@@ -97,25 +154,6 @@ export class PieLottiePlayer extends LitElement implements LottiePlayerProps {
     public stop (): void {
         if (!this.animationInstance) return;
         this.animationInstance.stop();
-    }
-
-    public setSpeed (speed:number): void {
-        if (!this.animationInstance) return;
-        this.animationInstance.setSpeed(speed);
-    }
-
-    public setDirection (direction:AnimationDirection): void {
-        if (!this.animationInstance) return;
-        if (!direction) return;
-
-        switch (direction) {
-            case 'reverse':
-                this.animationInstance.setDirection(-1);
-                break;
-            default:
-                this.animationInstance.setDirection(1);
-                break;
-        }
     }
 
     render () {
