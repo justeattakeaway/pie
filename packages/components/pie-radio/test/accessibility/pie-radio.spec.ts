fc7482c5240566ae0522c6a48eb1160fb7a73dc8
leksaBoiko <oleksandra.boiko@justeattakeaway.com>
2024-09-05 12:16:34 +0300

    feat(pie-radio): DSW-2284 add component skeleton

---
+++ packages/components/pie-radio/test/accessibility/pie-radio.spec.ts
@@ -0,0 +1,18 @@
+
+import { test, expect } from '@justeattakeaway/pie-webc-testing/src/playwright/webc-fixtures.ts';
+import { PieRadio, RadioProps } from '../../src/index.ts';
+
+test.describe('PieRadio - Accessibility tests', () => {
+    test('a11y - should test the PieRadio component WCAG compliance', async ({ makeAxeBuilder, mount }) => {
+        await mount(
+            PieRadio,
+            {
+                props: {} as RadioProps,
+            },
+        );
+
+        const results = await makeAxeBuilder().analyze();
+
+        expect(results.violations).toEqual([]);
+    });
+});
