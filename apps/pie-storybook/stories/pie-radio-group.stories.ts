d32c0fc82f0f8f5ea555ce3bf6abddfefb487af7
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2024-09-10 14:15:50 +0100

    feat(pie-radio-group): DSW-2285 add component skeleton

---
+++ apps/pie-storybook/stories/pie-radio-group.stories.ts
@@ -0,0 +1,34 @@
+import { html } from 'lit';
+
+import '@justeattakeaway/pie-radio-group';
+import { type RadioGroupProps } from '@justeattakeaway/pie-radio-group';
+
+import { type StoryMeta } from '../types';
+import { createStory } from '../utilities';
+
+type RadioGroupStoryMeta = StoryMeta<RadioGroupProps>;
+
+const defaultArgs: RadioGroupProps = {};
+
+const radioGroupStoryMeta: RadioGroupStoryMeta = {
+    title: 'Radio Group',
+    component: 'pie-radio-group',
+    argTypes: {},
+    args: defaultArgs,
+    parameters: {
+        design: {
+            type: 'figma',
+            url: '',
+        },
+    },
+};
+
+export default radioGroupStoryMeta;
+
+// TODO: remove the eslint-disable rule when props are added
+// eslint-disable-next-line no-empty-pattern
+const Template = ({}: RadioGroupProps) => html`
+    <pie-radio-group></pie-radio-group>
+`;
+
+export const Default = createStory<RadioGroupProps>(Template, defaultArgs)();
