fb13040358cd645c7a89b008e1c719006031924d
Ben Siggery <ben.siggery@justeattakeaway.com>
2024-10-15 12:07:01 +0100

    chore(pie-monorepo): DSW-000 remove next / nuxt3 / vanilla

--- apps/examples/wc-next10/eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    root: true,
-    extends: [
-        require.resolve('@justeattakeaway/eslint-config-pie/strict'),
-        'eslint:recommended',
-        'next',
-    ],
-};
--- apps/examples/wc-next10/pages/_app.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import '@justeattakeaway/pie-css';
-import '../styles/styles.css';
-
-export default function App ({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
