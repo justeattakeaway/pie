c8a34fb35447ed7bdc05046f7eb448f750456edd
Raouf <raouf.sawehli@justeattakeaway.com>
2024-11-05 17:39:40 +0100

    fix(pie-tag): DSW-2325 revert some changes

--- apps/pie-storybook/stories/pie-tag.stories.ts
+++ apps/pie-storybook/stories/pie-tag.stories.ts
@@ -79,7 +79,6 @@ const tagStoryMeta: TagStoryMeta = {
             },
             if: { arg: 'isInteractive', eq: true },
         },
-
     },
     args: defaultArgs,
     parameters: {
@@ -117,12 +116,17 @@ const Template : TemplateFunction<TagProps> = ({
 const createTagStory = createStory<TagProps>(Template, defaultArgs);
 
 export const Neutral = createTagStory({ variant: 'neutral' });
-export const Information = createTagStory({ variant: 'information' });
-export const Success = createTagStory({ variant: 'success' });
-export const Error = createTagStory({ variant: 'error' });
-export const Brand05 = createTagStory({ variant: 'brand-05' });
+export const Blue = createTagStory({ variant: 'blue' });
+export const Green = createTagStory({ variant: 'green' });
+export const Yellow = createTagStory({ variant: 'yellow' });
+export const Red = createTagStory({ variant: 'red' });
 
 // For the following stories isStrong prop won't have any effect so it is excluded
+export const Brand = createTagStory({ variant: 'brand' }, {
+    controls: {
+        exclude: ['isStrong'],
+    },
+});
 
 export const NeutralAlternative = createTagStory({ variant: 'neutral-alternative' }, {
     bgColor: 'dark (container-dark)',
@@ -138,12 +142,6 @@ export const Outline = createTagStory({ variant: 'outline' }, {
 });
 
 export const Ghost = createTagStory({ variant: 'ghost' }, {
-    controls: {
-        exclude: ['isStrong'],
-    },
-});
-
-export const Brand02 = createTagStory({ variant: 'brand-02' }, {
     controls: {
         exclude: ['isStrong'],
     },
