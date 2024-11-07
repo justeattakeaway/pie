d8423f760a24d1e06a58ef973b516eddff244ccf
Ben Siggery <ben.siggery@justeattakeaway.com>
2024-11-07 15:47:35 +0000

    fix lint

--- packages/components/pie-cookie-banner/vite.config.js
+++ packages/components/pie-cookie-banner/vite.config.js
@@ -2,14 +2,14 @@ import viteConfig from '@justeattakeaway/pie-components-config/vite.config';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default viteConfig({
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: './locales/*',
-          dest: './locales'
-        }
-      ]
-    })
-  ]
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: './locales/*',
+                    dest: './locales',
+                }
+            ],
+        })
+    ],
 });
