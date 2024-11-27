a50a938eee81c59c44fd53bb38ef8376aa7eca22
Raouf <raouf.sawehli@justeattakeaway.com>
2024-11-27 17:53:48 +0100

    feat(pie-toast-provider): DSW-2222 add priorty order

--- apps/pie-storybook/data/tag-variants-to-statuses-map.ts
+++ apps/pie-storybook/data/tag-variants-to-statuses-map.ts
@@ -1,9 +1,9 @@
 import { type TagVariantToStatusMap } from '../interfaces/tag-variant-to-status-map';
 
 export const tagVariantToStatusMap: TagVariantToStatusMap = {
-    alpha: 'brand-05',
-    beta: 'brand-05',
+    alpha: 'brand-02',
+    beta: 'brand-02',
     deprecated: 'error',
     removed: 'error',
-    stable: 'success',
+    stable: 'information',
 };
--- packages/components/pie-toast-provider/src/defs.ts
+++ packages/components/pie-toast-provider/src/defs.ts
@@ -1,3 +1,50 @@
-// TODO - please remove the eslint disable comment below when you add props to this interface
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ToastProviderProps {}
+import { type ToastProps } from '@justeattakeaway/pie-toast';
+
+export const PRIORITY_ORDER: { [x: string]: number } = {
+    'error-actionable': 1,
+    error: 2,
+    'warning-actionable': 3,
+    'positive-actionable': 4,
+    'info-actionable': 5,
+    'neutral-actionable': 6,
+    warning: 7,
+    positive: 8,
+    info: 9,
+    neutral: 10,
+};
+
+export interface ExtendedToastProps extends ToastProps {
+    /**
+     * Callback for when the toast is closed.
+     */
+    onPieToastClose?: () => void;
+
+    /**
+     * Callback for when the toast is opened.
+     */
+    onPieToastOpen?: () => void;
+
+    /**
+     * Callback for when the leading action is clicked.
+     */
+    onPieToastLeadingActionClick?: (event: Event) => void;
+}
+
+export interface ToastProviderProps {
+    /**
+     * Default options for all toasts.
+     */
+    options?: Partial<ExtendedToastProps>;
+}
+
+export const defaultProps: ToastProviderProps = {
+    options: {},
+};
+
+/**
+ * Event name for when the chip is closed.
+ *
+ * @constant
+ */
+
+export const ON_TOAST_PROVIDER_QUEUE_UPDATE_EVENT = 'pie-toast-provider-queue-update';
