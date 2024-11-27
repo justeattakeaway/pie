47c43434a57d81064cc3216406f19c3af2f66cff
Ben Siggery <ben.siggery@justeattakeaway.com>
2024-11-27 14:52:07 +0000

    correct test

--- packages/tools/generator-pie-component/src/app/templates/test/accessibility/pie-placeholder.__spec__.ts
+++ packages/tools/generator-pie-component/src/app/templates/test/accessibility/pie-placeholder.__spec__.ts
@@ -6,10 +6,9 @@ test.describe('Pie<%= componentName %> - Accessibility tests', () => {
     test('a11y - should test the Pie<%= componentName %> component WCAG compliance', async ({ page, makeAxeBuilder }) => {
         // Arrange
         const basePage = new BasePage(page, '<%= fileName %>--default');
-        
-        await page.waitForTimeout(2500);
 
         basePage.load();
+        await page.waitForTimeout(2500);
 
         // Act
         const results = await makeAxeBuilder().analyze();
