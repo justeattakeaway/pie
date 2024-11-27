8ddb3da4a03c815a03c70688aa3de57927e6955c
Raouf <raouf.sawehli@justeattakeaway.com>
2024-11-27 17:53:48 +0100

    feat(pie-toast-provider): DSW-2222 add priorty order
    feat(pie-toast-provider): DSW-2222 add tests
    
    feat(pie-toast-provider): DSW-2222 update yarn
    
    fix(pie-toast-provider): DSW-2222 update formating
    
    fix(pie-toast-provider): DSW-2222 ensure duration is parsed correctly
    
    fix(pie-toast-provider): DSW-2222 update markup
    
    fix(pie-toast-provider): DSW-2222 update yarn
    
    fix(pie-toast-provider): DSW-2222 add validation
    
    fix(pie-toast-provider): DSW-2222 update logs
    
    fix(pie-toast-provider): DSW-2222 refactor tests
    
    fix(pie-toast-provider): DSW-2222 refactor tests
    
    fix(pie-toast-provider): DSW-2222 refactor tests

---
+++ .changeset/tiny-eels-play.md
@@ -0,0 +1,9 @@
+---
+"@justeattakeaway/pie-toast-provider": minor
+"@justeattakeaway/pie-toast": minor
+"@justeattakeaway/pie-webc": minor
+"pie-storybook": minor
+"pie-monorepo": minor
+---
+
+[Added] - priority order for the toast provider
---
+++ packages/components/pie-toast-provider/test/helpers/page-object/selectors.ts
@@ -0,0 +1,11 @@
+const toastProvider = {
+    selectors: {
+        container: {
+            description: 'The selector for the toast provider container',
+            dataTestId: 'pie-toast-provider',
+        },
+    },
+};
+export {
+    toastProvider,
+};
