49236ad03c871004c98aeb7f2420a1deabcf917f
Raouf <raouf.sawehli@justeattakeaway.com>
2024-11-29 16:09:36 +0100

    fix(pie-toast-provider): DSW-2222 update formating

--- packages/components/pie-toast-provider/src/index.ts
+++ packages/components/pie-toast-provider/src/index.ts
@@ -33,96 +33,96 @@ const componentSelector = 'pie-toast-provider';
  * @event {CustomEvent} pie-toast-provider-queue-update - when a toast is added or removed from the queue.
  */
 export class PieToastProvider extends RtlMixin(LitElement) implements ToastProviderProps {
-  @state()
+    @state()
     private _toasts: ExtendedToastProps[] = [];
 
-  @state()
-  private _currentToast: ExtendedToastProps | null = null;
+    @state()
+    private _currentToast: ExtendedToastProps | null = null;
 
-  @property({ type: Object })
-  public options = defaultProps.options;
+    @property({ type: Object })
+    public options = defaultProps.options;
 
-  updated (changedProperties: PropertyValues<this>): void {
-      if (changedProperties.has('_toasts' as keyof PieToastProvider)) {
-          this._dispatchQueueUpdateEvent();
-      }
-  }
+    updated (changedProperties: PropertyValues<this>): void {
+        if (changedProperties.has('_toasts' as keyof PieToastProvider)) {
+            this._dispatchQueueUpdateEvent();
+        }
+    }
 
-  private _dispatchQueueUpdateEvent () : void {
-      dispatchCustomEvent(
-          this, ON_TOAST_PROVIDER_QUEUE_UPDATE_EVENT,
-          this._toasts,
-      );
-  }
+    private _dispatchQueueUpdateEvent (): void {
+        dispatchCustomEvent(
+            this, ON_TOAST_PROVIDER_QUEUE_UPDATE_EVENT,
+            this._toasts,
+        );
+    }
 
-  /**
-   * Get the priority for a toast.
-   * @param {string} type - The variant type of the toast.
-   * @param {boolean} hasAction - Whether the toast has an action.
-   * @returns {number} - The priority based on the variant and action.
+    /**
+     * Get the priority for a toast.
+     * @param {string} type - The variant type of the toast.
+     * @param {boolean} hasAction - Whether the toast has an action.
+     * @returns {number} - The priority based on the variant and action.
+     */
+    private getPriority (type: ExtendedToastProps['variant'], hasAction: boolean): number {
+        const key = `${type}${hasAction ? '-actionable' : ''}`;
+        return PRIORITY_ORDER[key];
+    }
+
+    /**
+     * Handles the dismissal of the current toast and displays the next one in the queue (if any).
+     */
+    private _dismissToast () {
+        this._currentToast?.onPieToastClose?.();
+        this._currentToast = null;
+        requestAnimationFrame(() => { this._showNextToast(); });
+    }
+
+    /**
+     * Displays the next toast in the queue, if available.
+     */
+    private _showNextToast () {
+        if (this._toasts.length > 0) {
+            const [nextToast, ...remainingToasts] = this._toasts;
+            this._currentToast = nextToast;
+            this._toasts = remainingToasts;
+        } else {
+            this._currentToast = null;
+        }
+    }
+
+    /**
+     * Adds a new toast to the queue and triggers its display if no toast is currently active.
+     * @param {ToastProps} toast - The toast props to display.
+     */
+    public createToast (toast: ExtendedToastProps) {
+        const newToast = { ...this.options, ...toast };
+
+        this._toasts = [...this._toasts, newToast].sort((a, b) => {
+            const priorityB = this.getPriority(b.variant, !!b.leadingAction?.text);
+            const priorityA = this.getPriority(a.variant, !!a.leadingAction?.text);
+
+            return priorityA - priorityB;
+        });
+
+        if (!this._currentToast) {
+            this._showNextToast();
+        }
+    }
+
+    /**
+   *
+   * Clears all toasts from the queue and dismisses the currently visible toast.
    */
-  private getPriority (type: ExtendedToastProps['variant'], hasAction: boolean): number {
-      const key = `${type}${hasAction ? '-actionable' : ''}`;
-      return PRIORITY_ORDER[key];
-  }
+    public clearToasts () {
+        this._toasts = [];
+        this._currentToast = null;
+    }
 
-  /**
-   * Handles the dismissal of the current toast and displays the next one in the queue (if any).
-   */
-  private _dismissToast () {
-      this._currentToast?.onPieToastClose?.();
-      this._currentToast = null;
-      requestAnimationFrame(() => { this._showNextToast(); });
-  }
+    render () {
+        const { _currentToast, _dismissToast } = this;
 
-  /**
-   * Displays the next toast in the queue, if available.
-   */
-  private _showNextToast () {
-      if (this._toasts.length > 0) {
-          const [nextToast, ...remainingToasts] = this._toasts;
-          this._currentToast = nextToast;
-          this._toasts = remainingToasts;
-      } else {
-          this._currentToast = null;
-      }
-  }
-
-  /**
-   * Adds a new toast to the queue and triggers its display if no toast is currently active.
-   * @param {ToastProps} toast - The toast props to display.
-   */
-  public createToast (toast: ExtendedToastProps) {
-      const newToast = { ...this.options, ...toast };
-
-      this._toasts = [...this._toasts, newToast].sort((a, b) => {
-          const priorityB = this.getPriority(b.variant, !!b.leadingAction?.text);
-          const priorityA = this.getPriority(a.variant, !!a.leadingAction?.text);
-
-          return priorityA - priorityB;
-      });
-
-      if (!this._currentToast) {
-          this._showNextToast();
-      }
-  }
-
-  /**
- *
- * Clears all toasts from the queue and dismisses the currently visible toast.
- */
-  public clearToasts () {
-      this._toasts = [];
-      this._currentToast = null;
-  }
-
-  render () {
-      const { _currentToast, _dismissToast } = this;
-
-      return html`
+        return html`
       <div class="c-toast-provider" data-test-id="pie-toast-provider">
       ${_currentToast
-          ? html`
+            ? html`
               <pie-toast
                 message="${_currentToast.message}"
                 variant="${ifDefined(_currentToast.variant)}"
@@ -136,20 +136,20 @@ export class PieToastProvider extends RtlMixin(LitElement) implements ToastProvi
                 @pie-toast-leading-action-click="${_currentToast.onPieToastLeadingActionClick}">
               </pie-toast>
             `
-          : nothing}
+            : nothing}
       </div>
     `;
-  }
+    }
 
-  // Renders a `CSSResult` generated from SCSS by Vite
-  static styles = unsafeCSS(styles);
+    // Renders a `CSSResult` generated from SCSS by Vite
+    static styles = unsafeCSS(styles);
 }
 
 defineCustomElement(componentSelector, PieToastProvider);
 
 declare global {
-  interface HTMLElementTagNameMap {
-    [componentSelector]: PieToastProvider;
-  }
+    interface HTMLElementTagNameMap {
+        [componentSelector]: PieToastProvider;
+    }
 }
 
