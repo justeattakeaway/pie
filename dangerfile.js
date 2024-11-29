e70ff215a3cda5ebbec8afa9ba10ed7208c5ce28
personal <jamieolivermaguire@googlemail.com>
2024-11-29 09:37:55 +0000

    update danger

--- dangerfile.js
+++ dangerfile.js
@@ -8,7 +8,6 @@ const isDependabotPR = pr.user.login === 'dependabot[bot]';
 // Check for correct Changeset formatting
 danger.git.created_files.filter((filepath) => filepath.includes('.changeset/') && !filepath.includes('.changeset/pre.json'))
     .forEach((filepath) => {
-        // get the git diff for the changeset file
         const changesetDiff = danger.git.diffForFile(filepath);
         changesetDiff.then((result) => {
             const diffString = result.diff;
@@ -17,17 +16,15 @@ danger.git.created_files.filter((filepath) => filepath.includes('.changeset/') &
             const numberOfCategories = changesetCategories ? changesetCategories.length : 0;
 
             if (isDependabotPR) {
-                // Check if at least one of the valid changeset categories is present
                 if (numberOfCategories === 0) {
                     fail(`:memo: Your changeset doesn't include a category. Please add one of: \`${validChangesetCategories.join(', ')}\`. Filepath: \`${filepath}`);
                 } else if (!validChangesetCategories.some((cat) => changesetCategories.includes(cat))) {
                     fail(`:memo: Your changeset includes an invalid category. Please use one of: \`${validChangesetCategories.join(', ')}\`. Filepath: \`${filepath}`);
                 }
 
-                // Check that categories are followed are in the following format `[Category] - {Description}`
                 const changesetLineFormatRegex = /\[\w+\] - [\S].+/g;
                 const validChangesetEntries = diffString.match(changesetLineFormatRegex);
-                const numberOfValidChangesetEntries = (validChangesetEntries === null ? 0 : validChangesetEntries.length);
+                const numberOfValidChangesetEntries = validChangesetEntries ? validChangesetEntries.length : 0;
                 if (numberOfCategories !== numberOfValidChangesetEntries) {
                     fail(`:memo: Your changeset entries should be in the format: \`[Category] - {Description}\`. One or more of your entries does not follow this format. Filepath: \`${filepath}`);
                 }
@@ -37,7 +34,29 @@ danger.git.created_files.filter((filepath) => filepath.includes('.changeset/') &
         });
     });
 
-// Check for empty PR Description checkboxes - but not for automated version PRs
-if (pr.body.includes('- [ ]') && !isDependabotPR) {
-    fail('You currently have an unchecked checklist item in your PR description.\n\nPlease confirm this check has been carried out – if it\'s not relevant to your PR, delete this line from the PR checklist.');
+// Allow unchecked checkboxes only in "Not-applicable Checklist items"
+if (!isDependabotPR) {
+    const { body } = pr;
+    const uncheckedCheckboxRegex = /- \[ \]/g;
+
+    // Check for unchecked checkboxes
+    const uncheckedCheckboxMatches = body.match(uncheckedCheckboxRegex);
+
+    if (uncheckedCheckboxMatches) {
+        // Split the body into sections
+        const sections = body.split(/## /);
+        const notApplicableSection = sections.find((section) => section.startsWith('Not-applicable Checklist items'));
+
+        // Check for unchecked checkboxes outside the "Not-applicable Checklist items" section
+        const uncheckedOutsideNotApplicableSection = sections.some((section) => {
+            if (section !== notApplicableSection) {
+                return uncheckedCheckboxRegex.test(section);
+            }
+            return false;
+        });
+
+        if (uncheckedOutsideNotApplicableSection) {
+            fail('You have unchecked checklist items outside the "Not-applicable Checklist items" section.\n\nPlease ensure all unchecked checkboxes are moved to the appropriate section.');
+        }
+    }
 }
