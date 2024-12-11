beaeb292be1cc57f928551395faec9ee5d0ec30a
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2024-12-11 16:29:47 +0000

    fix(generator-pie-component): DSW-000 export new native playwright config (#2119)
    * fix(generator-pie-component): DSW-000 export new native playwright config
    
    * update generator and revert files
    
    * revert cookie-banner visual config
    
    ---------
    
    Co-authored-by: Ben Siggery <ben.siggery@justeattakeaway.com>

--- packages/components/pie-cookie-banner/playwright-lit-visual.config.ts
+++ packages/components/pie-cookie-banner/playwright-lit-visual.config.ts
@@ -2,3 +2,4 @@ import { defineConfig } from '@sand4rt/experimental-ct-web';
 import { getPlaywrightVisualConfig } from '@justeattakeaway/pie-components-config';
 
 export default defineConfig(getPlaywrightVisualConfig());
+
