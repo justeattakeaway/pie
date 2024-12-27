79fb49736965f843fdb1f187fe12d0dad3db9aa8
boris.netsov <boris.netsov@justeattakeaway.com>
2024-12-27 14:59:46 +0200

    feat(pie-thumbnail): DSW-2580 add basic thumbnail functionality

--- packages/components/pie-thumbnail/src/defs.ts
+++ packages/components/pie-thumbnail/src/defs.ts
@@ -1,3 +1,21 @@
-// TODO - please remove the eslint disable comment below when you add props to this interface
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ThumbnailProps {}
+import type { ComponentDefaultProps } from '@justeattakeaway/pie-webc-core';
+
+export const variants = [
+    'default', 'outline',
+] as const;
+
+export type Variant = typeof variants[number];
+
+export interface ThumbnailProps {
+    variant?: Variant;
+    src?: string;
+    alt?: string;
+}
+
+export type DefaultProps = ComponentDefaultProps<ThumbnailProps, 'variant' | 'src' | 'alt'>;
+
+export const defaultProps: DefaultProps = {
+    variant: 'default',
+    src: './assets/placeholder.png',
+    alt: 'placeholder',
+};
