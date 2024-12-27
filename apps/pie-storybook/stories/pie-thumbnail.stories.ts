0de40ae69bae31c14d6dc4a71e657cfe918fa9ca
boris.netsov <boris.netsov@justeattakeaway.com>
2024-12-27 14:59:46 +0200

    feat(pie-thumbnail): DSW-2580 add basic thumbnail functionality
    format(pie-thumbnail): DSW-2580 lint
    
    test(pie-thumbnail): DSW-2580 add visual tests
    
    test(pie-thumbnail): DSW-2580 update visual tests
    
    refactor(pie-thumbnail): DSW-2580 address comments
    
    refactor(pie-thumbnail): DSW-2580 remove placeholder image from storybook
    
    chore(pie-thumbnail): DSW-2580 update changelog
    
    refactor(pie-thumbnail): DSW-2580 add default src and alt props to visual test
    
    refactor(pie-thumbnail): DSW-2580 update based on comments
    
    feat(pie-thumbnail): DSW-2580 add basic thumbnail functionality
    
    refactor(pie-thumbnail): DSW-2580 address comments
    
    refactor(pie-thumbnail): DSW-2580 remove placeholder image from storybook
    
    fix(pie-thumbnail): DSW-2580 fix percy test
    
    fix(pie-thumbnail): DSW-2580 fix percy test
    
    refactor(pie-thumbnail): DSW-2580 update based on comments
    
    format(pie-thumbnail): DSW-2580 address comments
    
    feat(pie-thumbnail): DSW-2580 add basic thumbnail functionality
    
    test(pie-thumbnail): DSW-2580 add visual tests
    
    test(pie-thumbnail): DSW-2580 update visual tests
    
    refactor(pie-thumbnail): DSW-2580 address comments
    
    refactor(pie-thumbnail): DSW-2580 remove placeholder image from storybook
    
    feat(pie-thumbnail): DSW-2580 add basic thumbnail functionality
    
    test(pie-thumbnail): DSW-2580 add visual tests
    
    test(pie-thumbnail): DSW-2580 update visual tests
    
    refactor(pie-thumbnail): DSW-2580 address comments
    
    refactor(pie-thumbnail): DSW-2580 remove placeholder image from storybook

--- apps/pie-storybook/stories/pie-thumbnail.stories.ts
+++ apps/pie-storybook/stories/pie-thumbnail.stories.ts
@@ -2,33 +2,71 @@ import { html } from 'lit';
 import { type Meta } from '@storybook/web-components';
 
 import '@justeattakeaway/pie-thumbnail';
-import { type ThumbnailProps } from '@justeattakeaway/pie-thumbnail';
+import { type ThumbnailProps, defaultProps, variants } from '@justeattakeaway/pie-thumbnail';
 
-import { createStory } from '../utilities';
+import { createStory, type TemplateFunction } from '../utilities';
 
 type ThumbnailStoryMeta = Meta<ThumbnailProps>;
 
-const defaultArgs: ThumbnailProps = {};
+const defaultArgs: ThumbnailProps = {
+    ...defaultProps,
+    src: 'https://www.pie.design/assets/img/jet-logo-narrow.svg',
+    alt: 'JET logo',
+};
 
 const thumbnailStoryMeta: ThumbnailStoryMeta = {
     title: 'Thumbnail',
     component: 'pie-thumbnail',
-    argTypes: {},
+    argTypes: {
+        variant: {
+            description: 'Set the variant of the thumbnail.',
+            control: 'select',
+            options: variants,
+            defaultValue: {
+                summary: defaultArgs.variant,
+            },
+        },
+        src: {
+            description: 'Set the src attribute for the underlying image tag.',
+            control: 'text',
+            defaultValue: {
+                summary: defaultArgs.src,
+            },
+        },
+        alt: {
+            description: 'Set the alt attribute for the underlying image tag.',
+            control: 'text',
+            defaultValue: {
+                summary: defaultArgs.alt,
+            },
+        },
+    },
     args: defaultArgs,
     parameters: {
         design: {
             type: 'figma',
-            url: '',
+            url: 'https://www.figma.com/design/pPSC73rPin4csb8DiK1CRr/branch/z8B7RUnz2Oq8cplqN38E9j/%E2%9C%A8-%5BCore%5D-Web-Components-%5BPIE-3%5D?node-id=17054-19120&node-type=instance&m=dev',
         },
     },
 };
 
 export default thumbnailStoryMeta;
 
-// TODO: remove the eslint-disable rule when props are added
-// eslint-disable-next-line no-empty-pattern
-const Template = ({}: ThumbnailProps) => html`
-    <pie-thumbnail></pie-thumbnail>
-`;
+const Template: TemplateFunction<ThumbnailProps> = ({
+    variant,
+    src,
+    alt,
+}) => html`
+    <pie-thumbnail
+        variant="${variant}"
+        src="${src}"
+        alt="${alt}">
+    </pie-thumbnail>`;
 
-export const Default = createStory<ThumbnailProps>(Template, defaultArgs)();
+const createThumbnailStory = createStory<ThumbnailProps>(Template, defaultArgs);
+
+export const Default = createThumbnailStory({}, {});
+
+export const Outline = createThumbnailStory({
+    variant: 'outline',
+}, {});
