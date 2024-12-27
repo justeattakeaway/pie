8f4767daf2c0f3d622dad8a97c417f2b953541e3
boris.netsov <boris.netsov@justeattakeaway.com>
2024-12-27 14:59:46 +0200

    feat(pie-thumbnail): DSW-2580 add basic thumbnail functionality

--- packages/components/pie-thumbnail/src/index.ts
+++ packages/components/pie-thumbnail/src/index.ts
@@ -1,8 +1,12 @@
-import { LitElement, html, unsafeCSS } from 'lit';
+import {
+    LitElement, html, unsafeCSS,
+} from 'lit';
 
-import { defineCustomElement } from '@justeattakeaway/pie-webc-core';
+import { defineCustomElement, validPropertyValues } from '@justeattakeaway/pie-webc-core';
+import { classMap } from 'lit/directives/class-map.js';
+import { property } from 'lit/decorators.js';
+import { type ThumbnailProps, defaultProps, variants } from './defs';
 import styles from './thumbnail.scss?inline';
-import { type ThumbnailProps } from './defs';
 
 // Valid values available to consumers
 export * from './defs';
@@ -13,8 +17,33 @@ const componentSelector = 'pie-thumbnail';
  * @tagname pie-thumbnail
  */
 export class PieThumbnail extends LitElement implements ThumbnailProps {
+    @property({ type: String })
+    @validPropertyValues(componentSelector, variants, defaultProps.variant)
+    public variant = defaultProps.variant;
+
+    @property({ type: String })
+    public src = defaultProps.src;
+
+    @property({ type: String })
+    public alt = defaultProps.alt;
+
     render () {
-        return html`<h1 data-test-id="pie-thumbnail">Hello world!</h1>`;
+        const {
+            variant,
+            src,
+            alt,
+        } = this;
+
+        const wrapperClasses = {
+            'o-tn': true,
+            [`o-tn--${variant}`]: true,
+        };
+
+        return html`
+            <div data-test-id="pie-thumbnail" class="${classMap(wrapperClasses)}">
+                <img data-test-id="pie-thumbnail-img" src="${src}" class="o-tn-img" alt="${alt}">
+            </div>
+        `;
     }
 
     // Renders a `CSSResult` generated from SCSS by Vite
