3701928e0039e86e732caca384b93c67254e7461
Ben Siggery <ben.siggery@justeattakeaway.com>
2025-01-15 15:48:03 +0000

    fix config import

--- packages/components/pie-form-label/test/visual/pie-form-label.spec.ts
+++ packages/components/pie-form-label/test/visual/pie-form-label.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test';
 import percySnapshot from '@percy/playwright';
 import { percyWidths } from '@justeattakeaway/pie-webc-testing/src/percy/breakpoints.ts';
-import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page';
+import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
 
 test.describe('Pie Form Label - Visual tests`', () => {
     test('should display the Pie Form Label component successfully', async ({ page }) => {
