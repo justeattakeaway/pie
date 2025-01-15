8f706e2ea1fc13f58611e2d4b0dd676e3bf0e98f
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2025-01-15 14:06:48 +0000

    refactor(pie-card): DSW-2370 tests to use `@playwright/test` (#2153)
    * modify component tests - wip
    
    * refactor tests - wip
    
    * pie card tests
    
    * refactor(pie-card): DSW-2370 tests to use `@playwright/test`
    
    * add turbo.json
    
    * lint fix and rebase
    
    * remove redundent test
    
    * revert method signature
    
    * update turbo.json
    
    * fix divider test background
    
    * fix storybook background
    
    * createVariantStory background improvments
    
    * revert test-id change
    
    * update background colour logic for variant stories
    
    ---------
    
    Co-authored-by: Ben Siggery <ben.siggery@justeattakeaway.com>

---
+++ packages/components/pie-card/test/helpers/page-object/pie-card.component.ts
@@ -0,0 +1,16 @@
+import { type Locator, type Page } from '@playwright/test';
+import {
+    card,
+} from './selectors.ts';
+
+export class CardComponent {
+    readonly componentLocator: Locator;
+    readonly linkLocator: Locator;
+    readonly buttonLocator: Locator;
+
+    constructor (page: Page) {
+        this.componentLocator = page.locator(card.selectors.container.dataTestId);
+        this.linkLocator = this.componentLocator.getByTestId(card.selectors.link.dataTestId);
+        this.buttonLocator = this.componentLocator.getByTestId(card.selectors.button.dataTestId);
+    }
+}
---
+++ packages/components/pie-card/test/helpers/page-object/selectors.ts
@@ -0,0 +1,19 @@
+const card = {
+    selectors: {
+        container: {
+            description: 'The selector for the card container',
+            dataTestId: 'pie-card',
+        },
+        link: {
+            description: 'The selector for when \'tag\' is set to \'a\'',
+            dataTestId: 'pie-card-link',
+        },
+        button: {
+            description: 'The selector for when \'tag\' is set to \'button\'',
+            dataTestId: 'pie-card-button',
+        },
+    },
+};
+export {
+    card,
+};
