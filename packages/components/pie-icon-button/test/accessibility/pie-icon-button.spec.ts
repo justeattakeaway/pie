4f43208b284e276a6238c4cb24e317defc8cb777
Ben Siggery <ben.siggery@justeattakeaway.com>
2025-01-16 11:45:47 +0000

    icon button WIP

--- packages/components/pie-icon-button/test/accessibility/pie-icon-button.spec.ts
+++ packages/components/pie-icon-button/test/accessibility/pie-icon-button.spec.ts
@@ -1,37 +1,8 @@
-import { test, expect } from '@justeattakeaway/pie-webc-testing/src/playwright/webc-fixtures.ts';
-import type {
-    PropObject, WebComponentPropValues,
-} from '@justeattakeaway/pie-webc-testing/src/helpers/defs.ts';
-import {
-    getAllPropCombinations, splitCombinationsByPropertyValue,
-} from '@justeattakeaway/pie-webc-testing/src/helpers/get-all-prop-combos.ts';
-import { IconClose } from '@justeattakeaway/pie-icons-webc';
+import { test, expect } from '@justeattakeaway/pie-webc-testing/src/playwright/playwright-fixtures.ts';
 import { PieIconButton } from '../../src/index.ts';
+import type { IconButtonProps } from '../../src/defs.ts';
 import { variants } from '../../src/defs.ts';
-
-const props: PropObject = {
-    variant: variants,
-    disabled: [true, false],
-    aria: {
-        label: 'qux',
-    },
-};
-
-const componentPropsMatrix: WebComponentPropValues[] = getAllPropCombinations(props);
-const componentPropsMatrixByVariant: Record<string, WebComponentPropValues[]> = splitCombinationsByPropertyValue(componentPropsMatrix, 'variant');
-const componentVariants: string[] = Object.keys(componentPropsMatrixByVariant);
-
-test.beforeEach(async ({ mount }, testInfo) => {
-    testInfo.setTimeout(testInfo.timeout + 40000);
-
-    // This ensures the icon-button and icon components are registered in the DOM for each test.
-    // It appears to add them to a Playwright cache which we understand is required for the tests to work correctly.
-    const iconButtonComponent = await mount(PieIconButton);
-    await iconButtonComponent.unmount();
-    const iconComponent = await mount(IconClose);
-    await iconComponent.unmount();
-});
-
+å
 componentVariants.forEach((variant) => test(`should render all prop variations for Variant: ${variant}`, async ({ makeAxeBuilder, mount }) => {
     await Promise.all(componentPropsMatrixByVariant[variant].map(async (combo: WebComponentPropValues) => {
         await mount(
