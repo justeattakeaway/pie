cc912723ef50699d4a46a0222e6ea83e2d256c29
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2025-01-17 14:22:34 +0000

    refactor(pie-spinner): DSW-2381 tests to use `@playwright/test` (#2163)
    Co-authored-by: Ben Siggery <ben.siggery@justeattakeaway.com>

--- packages/components/pie-spinner/test/accessibility/pie-spinner.spec.ts
+++ packages/components/pie-spinner/test/accessibility/pie-spinner.spec.ts
@@ -1,12 +1,19 @@
-import { test, expect } from '@justeattakeaway/pie-webc-testing/src/playwright/webc-fixtures.ts';
-import { PieSpinner } from '../../src/index.ts';
+import { test, expect } from '@justeattakeaway/pie-webc-testing/src/playwright/playwright-fixtures.ts';
+import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
+import { variants } from '../../src/defs';
 
 test.describe('PieSpinner - Accessibility tests', () => {
-    test('a11y - should test the PieSpinner component WCAG compliance', async ({ makeAxeBuilder, mount }) => {
-        await mount(PieSpinner);
+    variants.forEach((variant) => {
+        test(`a11y - should test the PieSpinner component WCAG compliance - ${variant}`, async ({ makeAxeBuilder, page }) => {
+            // Arrange
+            const spinnerPage = new BasePage(page, `spinner---${variant}`);
+            await spinnerPage.load();
 
-        const results = await makeAxeBuilder().analyze();
+            // Act
+            const results = await makeAxeBuilder().analyze();
 
-        expect(results.violations).toEqual([]);
+            // Assert
+            expect(results.violations).toEqual([]);
+        });
     });
 });
