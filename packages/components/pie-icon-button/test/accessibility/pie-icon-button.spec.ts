67883f80efe3655274ac81f0ddfd8fcecaf62c00
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2025-01-17 14:43:43 +0000

    refactor(pie-icon-button): DSW-2376 tests to use `@playwright/test` (#2161)
    * icon button WIP
    
    * icon button WIP
    
    * refactor(pie-icon-button): DSW-2376 tests to use `@playwright/test`
    
    * turbo.json
    
    * fix typo
    
    * Update packages/components/pie-icon-button/test/helpers/page-object/selectors.ts
    
    Co-authored-by: Josh Ng <joshua.ng2332@gmail.com>
    
    ---------
    
    Co-authored-by: Ben Siggery <ben.siggery@justeattakeaway.com>
    Co-authored-by: Josh Ng <joshua.ng2332@gmail.com>

--- packages/components/pie-icon-button/test/accessibility/pie-icon-button.spec.ts
+++ packages/components/pie-icon-button/test/accessibility/pie-icon-button.spec.ts
@@ -1,51 +1,15 @@
-import { test, expect } from '@justeattakeaway/pie-webc-testing/src/playwright/webc-fixtures.ts';
-import type {
-    PropObject, WebComponentPropValues,
-} from '@justeattakeaway/pie-webc-testing/src/helpers/defs.ts';
-import {
-    getAllPropCombinations, splitCombinationsByPropertyValue,
-} from '@justeattakeaway/pie-webc-testing/src/helpers/get-all-prop-combos.ts';
-import { IconClose } from '@justeattakeaway/pie-icons-webc';
-import { PieIconButton } from '../../src/index.ts';
+import { test, expect } from '@justeattakeaway/pie-webc-testing/src/playwright/playwright-fixtures.ts';
+import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
 import { variants } from '../../src/defs.ts';
 
-const props: PropObject = {
-    variant: variants,
-    disabled: [true, false],
-    aria: {
-        label: 'qux',
-    },
-};
+variants.forEach((variant) => test(`should render all prop variations for Variant: ${variant}`, async ({ makeAxeBuilder, page }) => {
+    // Arrange
+    const iconButtonPage = new BasePage(page, `icon-button--${variant}--variations`);
+    await iconButtonPage.load();
 
-const componentPropsMatrix: WebComponentPropValues[] = getAllPropCombinations(props);
-const componentPropsMatrixByVariant: Record<string, WebComponentPropValues[]> = splitCombinationsByPropertyValue(componentPropsMatrix, 'variant');
-const componentVariants: string[] = Object.keys(componentPropsMatrixByVariant);
-
-test.beforeEach(async ({ mount }, testInfo) => {
-    testInfo.setTimeout(testInfo.timeout + 40000);
-
-    // This ensures the icon-button and icon components are registered in the DOM for each test.
-    // It appears to add them to a Playwright cache which we understand is required for the tests to work correctly.
-    const iconButtonComponent = await mount(PieIconButton);
-    await iconButtonComponent.unmount();
-    const iconComponent = await mount(IconClose);
-    await iconComponent.unmount();
-});
-
-componentVariants.forEach((variant) => test(`should render all prop variations for Variant: ${variant}`, async ({ makeAxeBuilder, mount }) => {
-    await Promise.all(componentPropsMatrixByVariant[variant].map(async (combo: WebComponentPropValues) => {
-        await mount(
-            PieIconButton,
-            {
-                props: { ...combo },
-                slots: {
-                    default: '<icon-close></icon-close>',
-                },
-            },
-        );
-    }));
-
+    // Act
     const results = await makeAxeBuilder().analyze();
 
+    // Assert
     expect(results.violations).toEqual([]);
 }));
--- packages/components/pie-icon-button/test/component/pie-icon-button.spec.ts
+++ packages/components/pie-icon-button/test/component/pie-icon-button.spec.ts
@@ -1,67 +1,51 @@
-import { test, expect } from '@sand4rt/experimental-ct-web';
-import { IconClose } from '@justeattakeaway/pie-icons-webc';
-import { PieIconButton } from '../../src/index.ts';
+import { test, expect } from '@playwright/test';
+import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
+import { iconButton } from '../helpers/page-object/selectors.ts';
+import type { IconButtonProps } from '../../src/defs.ts';
 
-test.beforeEach(async ({ mount }, testInfo) => {
-    testInfo.setTimeout(testInfo.timeout + 40000);
+test('should correctly work with native click events', async ({ page }) => {
+    // Arrange
+    const iconButtonPage = new BasePage(page, 'icon-button--default');
+    await iconButtonPage.load();
 
-    // This ensures the icon-button and icon components are registered in the DOM for each test.
-    // It appears to add them to a Playwright cache which we understand is required for the tests to work correctly.
-    const iconButtonComponent = await mount(PieIconButton);
-    await iconButtonComponent.unmount();
-    const iconComponent = await mount(IconClose);
-    await iconComponent.unmount();
-});
-
-test('should correctly work with native click events', async ({ mount }) => {
-    const messages: string[] = [];
     const expectedEventMessage = 'Native event dispatched';
-    const component = await mount(
-        PieIconButton,
-        {
-            props: {
-                variant: 'primary',
-            },
-            on: {
-                click: () => {
-                    messages.push(expectedEventMessage);
-                },
-            },
-            slots: {
-                default: '<icon-close></icon-close>',
-            },
-        },
-    );
 
-    await component.click();
+    // Set up a listener for console messages
+    const consoleMessages: string[] = [];
+    page.on('console', (message) => {
+        if (message.type() === 'info') {
+            consoleMessages.push(message.text());
+        }
+    });
 
-    expect(messages).toEqual([expectedEventMessage]);
+    // Act
+    const iconButtonComponent = await page.locator(iconButton.selectors.container.dataTestId);
+    await iconButtonComponent.click();
+
+    // Assert
+    expect(consoleMessages).toEqual([expectedEventMessage]);
 });
 
-test('should apply all aria attributes to the underlying button element', async ({ mount }) => {
-    const component = await mount(
-        PieIconButton,
-        {
-            props: {
-                aria: {
-                    label: 'foo',
-                    labelledby: 'bar',
-                    describedby: 'baz',
-                    expanded: true,
-                    controls: 'test-controls',
-                },
-            },
-            slots: {
-                default: '<icon-close></icon-close>',
-            },
+test('should apply all aria attributes to the underlying button element', async ({ page }) => {
+    // Arrange
+    const props: IconButtonProps = {
+        aria: {
+            label: 'foo',
+            labelledby: 'bar',
+            describedby: 'baz',
+            expanded: true,
+            controls: 'test-controls',
         },
-    );
+    };
+    const iconButtonPage = new BasePage(page, 'icon-button--default');
+    await iconButtonPage.load({ ...props });
 
-    const button = component.getByTestId('pie-icon-button');
+    const iconButtonComponent = await page.locator(iconButton.selectors.container.dataTestId).locator('button');
 
-    await expect(button).toHaveAttribute('aria-label', 'foo');
-    await expect(button).toHaveAttribute('aria-labelledby', 'bar');
-    await expect(button).toHaveAttribute('aria-describedby', 'baz');
-    await expect(button).toHaveAttribute('aria-expanded', 'true');
-    await expect(button).toHaveAttribute('aria-controls', 'test-controls');
+    // Assert
+    await expect(iconButtonComponent).toHaveAttribute('aria-label', 'foo');
+    await expect(iconButtonComponent).toHaveAttribute('aria-labelledby', 'bar');
+    await expect(iconButtonComponent).toHaveAttribute('aria-describedby', 'baz');
+    await expect(iconButtonComponent).toHaveAttribute('aria-expanded', 'true');
+    await expect(iconButtonComponent).toHaveAttribute('aria-controls', 'test-controls');
 });
