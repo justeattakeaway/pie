8efd7597eb483c0ef72734ce350e78948d4a44bb
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2025-01-17 09:30:58 +0000

    Update packages/components/pie-icon-button/test/helpers/page-object/selectors.ts
    Co-authored-by: Josh Ng <joshua.ng2332@gmail.com>

--- packages/components/pie-icon-button/test/helpers/page-object/selectors.ts
+++ packages/components/pie-icon-button/test/helpers/page-object/selectors.ts
@@ -1,7 +1,7 @@
 const iconButton = {
     selectors: {
         container: {
-            description: 'The selector for the modal container',
+            description: 'The selector for the icon button container',
             dataTestId: 'pie-icon-button',
         },
     },
