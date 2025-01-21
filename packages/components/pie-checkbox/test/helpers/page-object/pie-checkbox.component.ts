bb319d6104dd8806004b2deec5dfe63bc4401ac7
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2025-01-21 16:07:19 +0000

    refactor(pie-checkbox): DSW-2371 tests to use `@playwright/test` (#2164)
    * refactor(pie-checkbox): DSW-2371 tests to use `@playwright/test`
    
    * fix checkbox group tests
    
    * PR suggestions
    
    ---------
    
    Co-authored-by: Ben Siggery <ben.siggery@justeattakeaway.com>

---
+++ packages/components/pie-checkbox/test/helpers/page-object/pie-checkbox.component.ts
@@ -0,0 +1,28 @@
+import type { Locator, Page } from '@playwright/test';
+import { checkbox } from './selectors.ts';
+
+export class CheckboxComponent {
+    readonly componentLocator: Locator;
+    readonly inputLocator: Locator;
+    readonly labelLocator: Locator;
+    readonly assistiveTextLocator: Locator;
+    readonly page: Page;
+    constructor (page: Page) {
+        this.page = page;
+        this.componentLocator = page.locator(checkbox.selectors.container.dataTestId);
+        this.inputLocator = this.componentLocator.getByTestId(checkbox.selectors.input.dataTestId);
+        this.labelLocator = this.componentLocator.getByTestId(checkbox.selectors.label.dataTestId);
+        this.assistiveTextLocator = this.componentLocator.getByTestId(checkbox.selectors.assistiveText.dataTestId);
+    }
+
+    /**
+     * Check if the checkbox is valid
+     * @returns {Promise<boolean>}
+     */
+    async isValid () {
+        return this.page.evaluate(() => {
+            const element = document.querySelector('pie-checkbox')?.shadowRoot?.querySelector('input') as HTMLInputElement;
+            return element ? element.validity.valid : false;
+        });
+    }
+}
