a2aa3521db38278b50a46e9fbe9ba0a48e31f320
Ben Siggery <ben.siggery@justeattakeaway.com>
2025-01-28 16:16:50 +0000

    fix lint

--- packages/tools/pie-icons-webc/test/helpers/page-object/selectors.ts
+++ packages/tools/pie-icons-webc/test/helpers/page-object/selectors.ts
@@ -1,15 +1,15 @@
 const icons = {
-  selectors: {
-      alcoholFilled: {
-          description: 'The selector for the alcohol filled icon',
-          dataTestId: 'icon-alcohol-filled',
-      },
-      alcoholFilledLarge: {
-          description: 'The selector for the alcohol filled large icon',
-          dataTestId: 'icon-alcohol-filled-large',
-      },
-  },
+    selectors: {
+        alcoholFilled: {
+            description: 'The selector for the alcohol filled icon',
+            dataTestId: 'icon-alcohol-filled',
+        },
+        alcoholFilledLarge: {
+            description: 'The selector for the alcohol filled large icon',
+            dataTestId: 'icon-alcohol-filled-large',
+        },
+    },
 };
 export {
-  icons,
+    icons,
 };
