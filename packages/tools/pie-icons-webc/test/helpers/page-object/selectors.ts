2a36690084a9d42a92fbc4a736cd3cbe38eacd02
Ben Siggery <ben.siggery@justeattakeaway.com>
2025-01-28 15:58:37 +0000

    refactor(pie-icons-webc): DSW-2755 tests to use `@playwright/test`
    add visual tasks to turbo.json
    
    add invalid prop values to test
    
    add invalid prop values to test

---
+++ packages/tools/pie-icons-webc/test/helpers/page-object/selectors.ts
@@ -0,0 +1,15 @@
+const icons = {
+  selectors: {
+      alcoholFilled: {
+          description: 'The selector for the alcohol filled icon',
+          dataTestId: 'icon-alcohol-filled',
+      },
+      alcoholFilledLarge: {
+          description: 'The selector for the alcohol filled large icon',
+          dataTestId: 'icon-alcohol-filled-large',
+      },
+  },
+};
+export {
+  icons,
+};
