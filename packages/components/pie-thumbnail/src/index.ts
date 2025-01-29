56ad987f0f553ee37947f1a40557e858bffc62bb
Raouf <raouf.sawehli@justeattakeaway.com>
2025-01-29 18:51:58 +0300

    fix(pie-thumbnail): DSW-2582 update border radius

--- packages/components/pie-thumbnail/src/index.ts
+++ packages/components/pie-thumbnail/src/index.ts
@@ -57,11 +57,11 @@ export class PieThumbnail extends LitElement implements ThumbnailProps {
 
     private _generateSizeStyles (): string {
         const { size } = this;
-        let borderRadius = '--dt-radius-rounded-a';
+        let borderRadius = '--dt-radius-rounded-b';
         if (size <= 40) {
             borderRadius = '--dt-radius-rounded-a';
         } else if (size <= 56) {
-            borderRadius = '--dt-radius-rounded-a';
+            borderRadius = '--dt-radius-rounded-c';
         }
 
         return `
