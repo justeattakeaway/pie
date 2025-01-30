a26fcb0f2499698b4aed30f47d654ffa6faa2fe7
Ben Siggery <ben.siggery@justeattakeaway.com>
2025-01-30 15:11:00 +0000

    pr comments

--- packages/tools/pie-icons-webc/test/helpers/page-object/selectors.ts
+++ packages/tools/pie-icons-webc/test/helpers/page-object/selectors.ts
@@ -1,11 +1,9 @@
 const icons = {
     selectors: {
         alcoholFilled: {
-            description: 'The selector for the alcohol filled icon',
             dataTestId: 'icon-alcohol-filled',
         },
         alcoholFilledLarge: {
-            description: 'The selector for the alcohol filled large icon',
             dataTestId: 'icon-alcohol-filled-large',
         },
     },
