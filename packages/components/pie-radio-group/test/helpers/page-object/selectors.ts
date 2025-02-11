00132960f6829952ffef91492ee876977b521060
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2025-02-11 12:32:39 +0000

    refactor(pie-radio-group): DSW-2698 tests to use `@playwright/test` (#2201)
    Co-authored-by: Ben Siggery <ben.siggery@justeattakeaway.com>

---
+++ packages/components/pie-radio-group/test/helpers/page-object/selectors.ts
@@ -0,0 +1,16 @@
+const radioGroup = {
+    selectors: {
+        container: {
+            dataTestId: 'pie-radio-group',
+        },
+        fieldset: {
+            dataTestId: 'pie-radio-group-fieldset',
+        },
+        assistiveText: {
+            dataTestId: 'pie-radio-group-assistive-text',
+        },
+    },
+};
+export {
+    radioGroup,
+};
