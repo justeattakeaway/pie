d4fa9efbe7185c3617c275e93bdac8af29de1d68
Ben Siggery <ben.siggery@justeattakeaway.com>
2025-02-12 12:17:33 +0000

    remove setup file

--- vitest.setup.ts
+++
@@ -1,4 +0,0 @@
-import { MockComponent } from './packages/components/pie-webc-core/src/test/mixins/formControlMixin/MockComponent';
-
-// Register the custom element before tests run
-customElements.define('form-control-mixin-mock', MockComponent); 
