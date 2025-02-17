c54f61523bae8af0c5d18e98401f72627370d73c
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2025-02-17 17:59:29 +0100

    fix(generator-pie-component): DSW-000 update templates to avoid ESLint issues (#2225)
    fix(generator-pie-component): DSW-000 update templates to avoid eslint issues

--- packages/tools/generator-pie-component/src/app/templates/test/visual/pie-placeholder.__spec__.ts
+++ packages/tools/generator-pie-component/src/app/templates/test/visual/pie-placeholder.__spec__.ts
@@ -4,11 +4,11 @@ import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-obj
 
 test.describe('Pie<%= componentName %> - Visual tests`', () => {
     test('should display the Pie<%= componentName %> component successfully', async ({ page }) => {
-      const basePage = new BasePage(page, '<%= fileName %>--default');
+        const basePage = new BasePage(page, '<%= fileName %>--default');
 
-      basePage.load();
-      await page.waitForTimeout(2500);
+        basePage.load();
+        await page.waitForTimeout(2500);
 
-      await percySnapshot(page, 'Pie<%= componentName %> - Visual Test');
+        await percySnapshot(page, 'Pie<%= componentName %> - Visual Test');
     });
 });
