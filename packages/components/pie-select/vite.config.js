8e11b70e41a2a5d390e3efffbf0c0b08c58fc7fa
Raouf <raouf.sawehli@justeattakeaway.com>
2025-03-14 17:25:38 +0100

    fix(pie-select): DSW-2774 extend common vite config

--- packages/components/pie-select/vite.config.js
+++ packages/components/pie-select/vite.config.js
@@ -1,61 +1,23 @@
-import { defineConfig } from 'vite';
+import viteConfig from '@justeattakeaway/pie-components-config/vite.config';
 import dts from 'vite-plugin-dts';
-import { visualizer } from 'rollup-plugin-visualizer';
 
-import { deepmerge } from 'deepmerge-ts';
-
-// https://vitejs.dev/config/
-const sharedConfig = ({ build = {}, plugins = [], ...rest }) => defineConfig({
-    build: deepmerge({
+export default viteConfig({
+    build: {
         lib: {
             entry: {
-                index: 'src/index.ts',
-                react: 'src/react.ts',
                 'pie-option/index': 'src/pie-option/index.ts',
                 'pie-option/react': 'src/pie-option/react.ts',
                 'pie-option-group/index': 'src/pie-option-group/index.ts',
                 'pie-option-group/react': 'src/pie-option-group/react.ts',
             },
-            formats: ['es'],
         },
-        rollupOptions: {
-            external: (id) => {
-                if (['react', '@lit/react'].includes(id) || /^lit/.test(id)) {
-                    return true;
-                }
-
-                if (id.startsWith('@justeattakeaway/pie-')) {
-                    console.info(`Excluding ${id} from the bundle`);
-                    return true;
-                }
-
-                return false;
-            },
-        },
-    }, build),
-    test: {
-        dir: '.',
-        environment: 'jsdom',
-        globals: true,
-        include: [
-            './src/__tests__/**/*.{spec,test}.{js,ts}',
-            './test/unit/**/*.{spec,test}.{js,ts}',
-        ],
-        exclude: ['**/node_modules/**'],
     },
-    plugins: deepmerge([dts({
-        insertTypesEntry: true,
-        outputDir: 'dist',
-        entryRoot: 'src',
-        rollupTypes: false,
-    }),
-    visualizer({
-        gzipSize: true,
-        brotliSize: true,
-    })], plugins),
-
-    ...rest,
+    plugins: [
+        dts({
+            insertTypesEntry: true,
+            outputDir: 'dist',
+            entryRoot: 'src',
+            rollupTypes: false,
+        }),
+    ],
 });
-
-export default sharedConfig;
-
