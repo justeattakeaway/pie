b5e6e8d9fcae5c74fbdc80f61734350bedb21202
personal <jamieolivermaguire@googlemail.com>
2025-03-25 17:02:39 +0000

    correctly inherit declarations from config package in ts

--- packages/components/pie-cookie-banner/declaration.d.ts
+++ packages/components/pie-cookie-banner/declaration.d.ts
@@ -1,16 +1,3 @@
-declare module '*.scss' {
-    const content: Record<string, string>;
-    export default content;
-}
-
-declare module '*.scss?inline' {
-    const content: Record<string, string>;
-    export default content;
-}
-
-// This is a placeholder value that is swapped out
-// with the component package.json version at build time by Vite.
-declare const __PACKAGE_VERSION__: string;
 
 declare module '@justeattakeaway/pie-cookie-banner/locales/*.js' {
     export interface CookieBannerLocale {
--- packages/components/pie-lottie-player/declaration.d.ts
+++ packages/components/pie-lottie-player/declaration.d.ts
@@ -1,16 +1,3 @@
-declare module '*.scss' {
-    const content: Record<string, string>;
-    export default content;
-}
-
-declare module '*.scss?inline' {
-    const content: Record<string, string>;
-    export default content;
-}
-
-// This is a placeholder value that is swapped out
-// with the component package.json version at build time by Vite.
-declare const __PACKAGE_VERSION__: string;
 
 declare module 'lottie-web/build/player/lottie_light_canvas.min.js' {
     import { type LottiePlayer } from 'lottie-web';
