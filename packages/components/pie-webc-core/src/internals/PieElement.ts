f6d5a81c1b0a2f4c6621975ee6508c649a5a9276
personal <jamieolivermaguire@googlemail.com>
2025-04-03 13:19:27 +0100

    test lifecycle

--- packages/components/pie-webc-core/src/internals/PieElement.ts
+++ packages/components/pie-webc-core/src/internals/PieElement.ts
@@ -1,11 +1,11 @@
 import { LitElement } from 'lit';
-import { property } from 'lit/decorators.js';
 
 export class PieElement extends LitElement {
     public static readonly v = __PACKAGE_VERSION__;
 
-    constructor () {
-        super();
-        this.setAttribute('v', PieElement.v);
+    protected willUpdate (): void {
+        if (!this.getAttribute('v')) {
+            this.setAttribute('v', PieElement.v);
+        }
     }
 }
