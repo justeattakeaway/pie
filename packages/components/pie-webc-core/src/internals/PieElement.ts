be7ae45f838c62c56bf227ad3282705eef5a681f
personal <jamieolivermaguire@googlemail.com>
2025-04-03 11:48:30 +0100

    make property static

--- packages/components/pie-webc-core/src/internals/PieElement.ts
+++ packages/components/pie-webc-core/src/internals/PieElement.ts
@@ -3,5 +3,5 @@ import { property } from 'lit/decorators.js';
 
 export class PieElement extends LitElement {
     @property({ type: String, reflect: true })
-    public readonly v = __PACKAGE_VERSION__;
+    public static readonly v = __PACKAGE_VERSION__;
 }
