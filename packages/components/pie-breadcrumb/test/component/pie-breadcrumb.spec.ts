311fa2367a16ae300fa572352e91d5e9d0eae914
Raouf <raouf.sawehli@justeattakeaway.com>
2025-04-14 18:32:50 +0200

    fix(pie-breadcrumb): DSW-000 fix breadcrumb tests (#2325)

--- packages/components/pie-breadcrumb/test/component/pie-breadcrumb.spec.ts
+++ packages/components/pie-breadcrumb/test/component/pie-breadcrumb.spec.ts
@@ -1,55 +1,40 @@
 import { test, expect } from '@playwright/test';
 import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
-
-import { type BreadcrumbProps, componentSelector } from 'src/defs';
-import {
-    breadCrumbNavigationItemRole,
-    breadCrumbSeparatorRole,
-    componentName,
-    navigationItems,
-} from 'test/helpers';
+import { breadcrumb } from '../helpers/selectors';
 
 test.describe('PieBreadcrumb - Component tests', () => {
     test('should render successfully', async ({ page }) => {
         // Arrange
-        const basePage = new BasePage(page, componentName);
-        const props: Partial<BreadcrumbProps> = {
-            items: [navigationItems[0]],
-        };
+        const basePage = new BasePage(page, 'breadcrumb--default');
 
-        basePage.load({ ...props });
+        basePage.load();
         await page.waitForTimeout(2500);
 
         // Act
-        const breadcrumb = page.getByTestId(componentSelector);
+        const breadcrumbComponent = page.getByTestId(breadcrumb.selectors.container.dataTestId);
 
         // Assert
-        expect(breadcrumb).toBeVisible();
+        expect(breadcrumbComponent).toBeVisible();
     });
 
     test.describe('Props', () => {
         test.describe('items', () => {
-            test('should render navigation items', async ({ page }) => {
+            test('should render items', async ({ page }) => {
                 // Arrange
-                const basePage = new BasePage(page, componentName);
-                const props: Partial<BreadcrumbProps> = {
-                    items: navigationItems,
-                };
-
-                await basePage.load({ ...props });
-
-                const numberOfSeparators = navigationItems.length - 1;
+                const basePage = new BasePage(page, 'breadcrumb--default');
+                await basePage.load();
 
                 // Act
-                const breadcrumbComponent = page.getByTestId(componentSelector);
-                const breadcrumbSeparators = breadcrumbComponent.locator(`li[role="${breadCrumbSeparatorRole}"]`);
-                const breadCrumbNavigationItems = breadcrumbComponent.locator(`[role="${breadCrumbNavigationItemRole}"]`);
+                const breadcrumbComponent = page.getByTestId(breadcrumb.selectors.container.dataTestId);
+                const breadcrumbList = page.getByTestId(breadcrumb.selectors.list.dataTestId);
+                const breadcrumbSeparators = page.getByTestId(breadcrumb.selectors.separator.dataTestId);
+                const breadcrumbItemElements = page.getByTestId(breadcrumb.selectors.item.dataTestId);
 
                 // Assert
                 await expect(breadcrumbComponent).toBeVisible();
-                await expect(breadcrumbComponent).toHaveClass(' c-breadcrumb ');
-                await expect(breadcrumbSeparators).toHaveCount(numberOfSeparators);
-                await expect(breadCrumbNavigationItems).toHaveCount(navigationItems.length);
+                await expect(breadcrumbList).toBeVisible();
+                await expect(breadcrumbSeparators).toHaveCount(3);
+                await expect(breadcrumbItemElements).toHaveCount(4);
             });
         });
     });
--- packages/components/pie-breadcrumb/test/visual/pie-breadcrumb.spec.ts
+++ packages/components/pie-breadcrumb/test/visual/pie-breadcrumb.spec.ts
@@ -1,49 +1,25 @@
 import { test } from '@playwright/test';
 import percySnapshot from '@percy/playwright';
 import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
-import { componentName, navigationItems } from 'test/helpers';
-import { type BreadcrumbProps } from 'src/defs';
 
 const readingDirections = ['ltr', 'rtl'];
 
 test.describe('PieBreadcrumb - Visual tests`', () => {
-    test('should display the PieBreadcrumb component successfully', async ({ page }) => {
+    test('should truncate the breadcrumb text when the label is too long (bigger than 250px)', async ({ page }) => {
         // Arrange
-        const basePage = new BasePage(page, componentName);
-        const props: Partial<BreadcrumbProps> = {
-            items: [navigationItems[0]],
-        };
-
-        basePage.load({ ...props });
+        const basePage = new BasePage(page, 'breadcrumb--with-long-text');
+        basePage.load();
         await page.waitForTimeout(2500);
 
         // Assert
-        await percySnapshot(page, 'PieBreadcrumb - Visual Test');
-    });
-
-    test('should display crop the text from the last item when the label is too long (bigger than 250px)', async ({ page }) => {
-        // Arrange
-        const basePage = new BasePage(page, componentName);
-        const props: Partial<BreadcrumbProps> = {
-            items: [...navigationItems, { label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque eget velit quis mollis.', href: '#' }],
-        };
-
-        basePage.load({ ...props });
-        await page.waitForTimeout(2500);
-
-        // Assert
-        await percySnapshot(page, 'PieBreadcrumb - Visual Test');
+        await percySnapshot(page, 'PieBreadcrumb - Truncation Visual Test');
     });
 
     readingDirections.forEach((direction) => {
-        test(`should render navigation items in writing direction: ${direction}`, async ({ page }) => {
+        test(`should render items in writing direction: ${direction}`, async ({ page }) => {
             // Arrange
-            const selectVariationsPage = new BasePage(page, componentName);
-            const props: Partial<BreadcrumbProps> = {
-                items: navigationItems,
-            };
-
-            await selectVariationsPage.load({ ...props }, { writingDirection: direction });
+            const basePage = new BasePage(page, 'breadcrumb--default');
+            await basePage.load({}, { writingDirection: direction });
 
             // Assert
             await percySnapshot(page, `PieBreadcrumb - ${direction}`);
