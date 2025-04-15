711fd8ac6383954d9e5b03fd984f253511f9c0a8
Matthew Hardern <matthewhardern@gmail.com>
2025-04-15 10:43:42 +0100

    feat(pie-list): DSW-2936 base component from generator (#2319)
    Base component from generator
    
    Co-authored-by: matthew.hardern <matthew.hardern@justeattakeaway.com>

---
+++ apps/pie-storybook/stories/pie-list.stories.ts
@@ -0,0 +1,34 @@
+import { html } from 'lit';
+import { type Meta } from '@storybook/web-components';
+
+import '@justeattakeaway/pie-list';
+import { type ListProps } from '@justeattakeaway/pie-list';
+
+import { createStory } from '../utilities';
+
+type ListStoryMeta = Meta<ListProps>;
+
+const defaultArgs: ListProps = {};
+
+const listStoryMeta: ListStoryMeta = {
+    title: 'List',
+    component: 'pie-list',
+    argTypes: {},
+    args: defaultArgs,
+    parameters: {
+        design: {
+            type: 'figma',
+            url: '',
+        },
+    },
+};
+
+export default listStoryMeta;
+
+// TODO: remove the eslint-disable rule when props are added
+// eslint-disable-next-line no-empty-pattern
+const Template = ({}: ListProps) => html`
+    <pie-list></pie-list>
+`;
+
+export const Default = createStory<ListProps>(Template, defaultArgs)();
