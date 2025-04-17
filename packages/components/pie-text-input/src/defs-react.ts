138ac55cfd8557c7ed1a1b762687d4ef521be8b4
Raouf <raouf.sawehli@justeattakeaway.com>
2025-04-17 11:33:50 +0200

    fix(pie-text-input): DSW-2856 add a comment

--- packages/components/pie-text-input/src/defs-react.ts
+++ packages/components/pie-text-input/src/defs-react.ts
@@ -1,3 +1,4 @@
 import type React from 'react';
 
+// Omitting the native input 'size' type to avoid conflicts with our custom 'size' prop.
 export type ReactBaseType = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'onInput' | 'size'>
