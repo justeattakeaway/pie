6458d0d31a448e830c80b5087c4b72622fca3a7d
Raouf <raouf.sawehli@justeattakeaway.com>
2025-04-17 17:59:04 +0200

    fix(pie-monorepo): DSW-2856 update comments

--- packages/components/pie-text-input/src/defs-react.ts
+++ packages/components/pie-text-input/src/defs-react.ts
@@ -1,4 +1,4 @@
 import type React from 'react';
 
-// Omit `size` and event types from HTMLInputElement to avoid conflicting with PieInput's type definition
+// Omit `size` and event types to avoid conflicting with PieInput's type definition
 export type ReactBaseType = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'onInput' | 'size'>
--- packages/components/pie-textarea/src/defs-react.ts
+++ packages/components/pie-textarea/src/defs-react.ts
@@ -1,4 +1,4 @@
 import type React from 'react';
 
-// Omit event types from HTMLInputElement to avoid conflicting with PieTextarea's type definition
+// Omit event types to avoid conflicting with PieTextarea's type definition
 export type ReactBaseType = Omit<React.HTMLAttributes<HTMLTextAreaElement>, 'onChange' | 'onInput'>
