ed9de3eb8d1836f854993c97e28e118f2d2131ab
Katarina Neskovic <katarina.neskovic@justeattakeaway.com>
2025-05-02 11:39:26 +0200

    docs(pie-docs): DSW-3009 vreate category dropdown

--- apps/pie-docs/src/_11ty/shortcodes/categorisedIconList.js
+++ apps/pie-docs/src/_11ty/shortcodes/categorisedIconList.js
@@ -1,6 +1,10 @@
 const iconData = require('@justeattakeaway/pie-icons/dist/iconData.json');
 const pieIcons = require('../filters/pieIconsSvg')();
 const headingAnchor = require('../filters/headingAnchor');
+
+const categoryNames = iconData.categories.filter(({ name }) => name !== 'payment');
+const selectedCategory = 'all';
+
 /**
  *
  * @param {*} icon - An object representing the icon to be displayed in the card.
@@ -44,6 +48,104 @@ const buildIconCard = (icon) => {
         <p>${icon.displayName}</p>
     </li>`;
 };
+const handleSelect = (e) => {
+    console.log('change');
+    /*     const selectedCategory = e.target.value;
+        const iconsListContainer = document.querySelector('.c-categorisedIconList');
+        if (iconsListContainer) {
+            iconsListContainer.innerHTML = generateIconsList(selectedCategory);
+        } */
+};
+
+const generateIconsList = (filterCategory) => {
+    const filteredCategories = iconData.categories.filter((category) => {
+        if (!filterCategory) {
+            return category.name !== 'payment';
+        }
+        return category.name !== 'payment' && category.name === filterCategory;
+    });
+
+    return filteredCategories.map((cat) => `
+        <li>
+            <h3 class="c-categorisedIconList-heading" id="category-${cat.name}">
+                ${cat.displayName}
+            </h3>
+
+            <ul class="c-categorisedIconList-iconList" aria-labelledby="category-${cat.name}">
+                ${cat.icons.map((i) => buildIconCard(i)).join('')}
+            </ul>`).join('');
+};
+
+const categoryDropdown = () => {
+    const formattedOptions = [
+        { tag: 'option', text: 'All categories', value: '' },
+        ...categoryNames.map((category) => ({
+            tag: 'option',
+            text: category.displayName,
+            value: category.name,
+        })),
+    ];
+    const jsonOptions = JSON.stringify(formattedOptions).replace(/"/g, '&quot;');
+
+    return `<pie-select id="categoryFilter" options="${jsonOptions}"></pie-select>
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const categoryFilter = document.getElementById('categoryFilter');
+        
+        if (categoryFilter) {
+            categoryFilter.addEventListener('change', function(e) {
+                const selectedCategory = e.target.value;
+                console.log('Category selected:', selectedCategory);
+            });
+        }
+    });
+    </script>
+    `;
+};
+/* const categoryDropdown = () => {
+    const formattedOptions = [
+        { tag: 'option', text: 'All categories', value: '' },
+        ...categoryNames.map((category) => ({
+            tag: 'option',
+            text: category.displayName,
+            value: category.name,
+        }))
+    ];
+    const jsonOptions = JSON.stringify(formattedOptions).replace(/"/g, '&quot;');
+
+    function callme (obj) {
+        console.log(obj.name);
+    }
+    return `<input type="text" name="fieldname" onkeydown="${callme(this)};" />`;
+}; */
+
+/*
+       <script>
+      const select = document.getElementById('cat-select');
+      select?.addEventListener('change', (e) => {
+        const selectedCategory = e.target.value;
+        console.log('Custom change event triggered:', e.target.value);
+        const container = document.querySelector('.c-categorisedIconList');
+        if (container) {
+          container.innerHTML = (${generateIconsList.toString()})(selectedCategory);
+        }
+      });
+    </script>
+
+        <script>
+      window.addEventListener('DOMContentLoaded', () => {
+        const select = document.getElementById('cat-select');
+        if (select) {
+          select.change = (selectedValue) => {
+            console.log('Value changed via custom property:', selectedValue);
+            const container = document.querySelector('.c-categorisedIconList');
+            if (container) {
+              container.innerHTML = (${generateIconsList.toString()})(selectedValue);
+            }
+          };
+        }
+      });
+    </script>*/
 
 /**
  * An HTML component that takes in a list of icon categories
@@ -54,18 +156,10 @@ const buildIconCard = (icon) => {
  * @returns {string}
  */
 const categorisedIconList = () => headingAnchor(`<div>
+    ${categoryDropdown()}
         <ul class="c-categorisedIconList">
-            ${iconData.categories.filter(({ name }) => name !== 'payment').map((cat) => `
-                <li>
-                    <h3 class="c-categorisedIconList-heading" id="category-${cat.name}">
-                        ${cat.displayName}
-                    </h3>
-
-                    <ul class="c-categorisedIconList-iconList" aria-labelledby="category-${cat.name}">
-                        ${cat.icons.map((i) => buildIconCard(i)).join('')}
-                    </ul>`).join('')}
-                </li>
-            </ul>
-        </div>`);
+            ${generateIconsList()}
+        </ul>
+    </div>`);
 
 module.exports = categorisedIconList;
