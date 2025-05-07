f4aedf5ad09d44759d712f53d6b21f4274c44ca3
Ashley Watson-Nolan <nolly00@gmail.com>
2025-05-07 17:18:04 +0100

    fix(pie-monorepo): DSW-3025 security patch to add scopes to private unpublished pkgs

--- packages/tools/pie-monorepo-utils/__tests__/changeset-snapshot/create-and-publish.spec.js
+++ packages/tools/pie-monorepo-utils/__tests__/changeset-snapshot/create-and-publish.spec.js
@@ -89,7 +89,7 @@ describe('create and publish workflow', () => {
         test('should ignore monorepo/docs/storybook changes', async () => {
             // Arrange
             sampleOutput = `
-                New tag: pie-docs@0.0.0-snapshot-release-20231220110000
+                New tag: @justeattakeaway/pie-docs@0.0.0-snapshot-release-20231220110000
                 New tag: pie-monorepo@0.0.0-snapshot-release-20231220110000
                 New tag: pie-storybook@0.0.0-snapshot-release-20231220110000
             `;
