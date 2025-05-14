67a76b4fd61bc29c35fafb246b72e8cb1ed5e4c1
Jamie Maguire <jamieolivermaguire@googlemail.com>
2025-05-14 17:20:40 +0100

    docs(pie-storybook): DSW-2997 add browser support page to storybook
    * docs(pie-storybook): DSW-2996 add a css setup page to sb
    
    * docs(pie-storybook): DSW-2996 add a typography setup page to sb
    
    * improve grammar
    
    * typo
    
    * css fix
    
    * cdn
    
    ---------
    
    Co-authored-by: Ben Siggery <14013357+siggerzz@users.noreply.github.com>

--- apps/pie-storybook/.storybook/main.ts
+++ apps/pie-storybook/.storybook/main.ts
@@ -1,16 +1,17 @@
 import type { StorybookConfig } from '@storybook/web-components-vite';
+import remarkGfm from 'remark-gfm';
 
 const isBrowserTesting = process.env.BROWSER_TESTING === 'true';
 
 const config: StorybookConfig = {
-    stories: isBrowserTesting 
+    stories: isBrowserTesting
         ? [
             "../stories/testing/**/*.test.stories.ts"
         ]
         : [
             "../stories/**/*.mdx",
             "../stories/*.stories.@(js|ts|tsx)",
-          ],
+        ],
     addons: [
         "@storybook/addon-essentials",
         "@storybook/addon-a11y",
@@ -18,6 +19,16 @@ const config: StorybookConfig = {
         "@storybook/addon-designs",
         "@storybook/addon-links",
         "storybook-dark-mode",
+        {
+            name: '@storybook/addon-docs',
+            options: {
+                mdxPluginOptions: {
+                    mdxCompileOptions: {
+                        remarkPlugins: [remarkGfm],
+                    },
+                },
+            },
+        },
     ],
     framework: {
         name: "@storybook/web-components-vite",
