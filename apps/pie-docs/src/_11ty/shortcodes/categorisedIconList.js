9f516f1f78e051242b36855a79716ad52e610ce4
Raouf <raouf.sawehli@justeattakeaway.com>
2025-05-21 14:25:04 +0200

    fix(pie-docs): DSW-3009 ensure icons list is rendered in client side and build time (#2367)
    * fix(pie-docs): DSW-3009 ensure icons list rendered in client and build time
    
    * fix(pie-docs): DSW-3009 rename files
    
    * fix(pie-docs): DSW-3009 revert a change
    
    * fix(pie-docs): DSW-3009 rename files again and update snapshot

--- apps/pie-docs/src/_11ty/shortcodes/categorisedIconList.js
+++ apps/pie-docs/src/_11ty/shortcodes/categorisedIconList.js
@@ -1,10 +1,10 @@
 const iconData = require('@justeattakeaway/pie-icons/dist/iconData.json');
 const pieIcons = require('../filters/pieIconsSvg')();
 const headingAnchor = require('../filters/headingAnchor');
-
-const categoryNames = iconData.categories.filter(({ name }) => name !== 'payment');
+const { generateCategorisedIconList } = require('../../assets/js/categorised-icon-list-helpers');
 
 const categoryDropdown = () => {
+    const categoryNames = iconData.categories.filter(({ name }) => name !== 'payment');
     const formattedOptions = [
         { tag: 'option', text: 'All icons', value: '' },
         ...categoryNames.map((category) => ({
@@ -18,15 +18,27 @@ const categoryDropdown = () => {
     return `<pie-select id="categoryFilter" options="${jsonOptions}"></pie-select>`;
 };
 
-const categorisedIconList = () => headingAnchor(`<div>
-    ${categoryDropdown()}
-    <ul class="c-categorisedIconList" id="categorisedIconListContainer"></ul>
-<script>
-
-    window.iconData = ${JSON.stringify(iconData)};
-    window.pieIcons = ${JSON.stringify(pieIcons)};
-</script>
-<script src="/assets/js/categorisedIconList.js"></script>
-</div>`);
+/**
+ * An HTML component that takes in a list of icon categories
+ * and renders a list of icons (with subheadings) for each category.
+ *
+ * Icons are assumed to have regular and `-large` variants
+ * unless specified with `"oneSize": true`.
+ * @returns {string}
+ */
+const categorisedIconList = () => headingAnchor(`
+        <div>
+            ${categoryDropdown()}
+            <ul class="c-categorisedIconList" id="categorisedIconListContainer">
+                ${generateCategorisedIconList(iconData.categories, pieIcons)}
+            </ul>
+            <script>
+                window.iconData = ${JSON.stringify(iconData)};
+                window.pieIcons = ${JSON.stringify(pieIcons)};
+            </script>
+            <script src="/assets/js/categorised-icon-list-helpers.js"></script>
+            <script src="/assets/js/categorised-icon-list-filter.js"></script>
+        </div>
+    `);
 
 module.exports = categorisedIconList;
