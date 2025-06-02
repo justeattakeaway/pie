0b8785ea18785198ae02ebd33b47c92e803763d1
Raouf <raouf.sawehli@justeattakeaway.com>
2025-06-02 10:09:53 +0200

    feat(pie-breadcrumb): DSW-3080 support hideCurrentPage and aria-current (#2382)

--- packages/components/pie-breadcrumb/src/index.ts
+++ packages/components/pie-breadcrumb/src/index.ts
@@ -3,6 +3,7 @@ import { property } from 'lit/decorators.js';
 import { safeCustomElement, validPropertyValues } from '@justeattakeaway/pie-webc-core';
 import { classMap } from 'lit/directives/class-map.js';
 import { PieElement } from '@justeattakeaway/pie-webc-core/src/internals/PieElement';
+import { type PieBreadcrumbItem } from './pie-breadcrumb-item';
 
 import styles from './breadcrumb.scss?inline';
 import {
@@ -28,13 +29,36 @@ export class PieBreadcrumb extends PieElement implements BreadcrumbProps {
     @property({ type: Boolean, reflect: true })
     public isCompact = defaultProps.isCompact;
 
+    @property({ type: Boolean, reflect: true })
+    public hideCurrentPage = defaultProps.hideCurrentPage;
+
+    private updateAriaCurrentItem () {
+        const items = [...this.querySelectorAll('pie-breadcrumb-item')] as PieBreadcrumbItem[];
+        items.forEach((item, i) => {
+            const isLast = i === items.length - 1;
+            item.ariaCurrent = isLast ? 'page' : null;
+        });
+    }
+
+    /**
+     * Handles the slot change event.
+     *
+     * @private
+     */
+    private _handleSlotChange () {
+        this.updateAriaCurrentItem();
+    }
+
+    protected firstUpdated (): void {
+        this.updateAriaCurrentItem();
+    }
+
     render () {
-        const { variant, isCompact } = this;
+        const { variant } = this;
 
         const classes = {
             'c-breadcrumb': true,
             [`c-breadcrumb--${variant}`]: true,
-            'c-breadcrumb--compact': isCompact,
         };
 
         return html`
@@ -43,7 +67,7 @@ export class PieBreadcrumb extends PieElement implements BreadcrumbProps {
                 data-test-id="pie-breadcrumb"
                 class="${classMap(classes)}">
                     <ol class="c-breadcrumb-list" data-test-id="pie-breadcrumb-list">
-                        <slot></slot>
+                        <slot @slotchange=${this._handleSlotChange}></slot>
                     </ol>
             </nav>`;
     }
