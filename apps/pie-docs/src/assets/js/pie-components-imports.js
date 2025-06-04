f1707ef92eaec4af331e6072d55800ad50c03cb0
Katarina Neskovic <katarina.neskovic@justeattakeaway.com>
2025-06-04 14:52:44 +0200

    docs(pie-docs): DSW-3008 add notification, update styling

--- apps/pie-docs/src/assets/js/pie-components-imports.js
+++ apps/pie-docs/src/assets/js/pie-components-imports.js
@@ -2,3 +2,4 @@
 // so they are available at build time and in the client-side bundle.
 import('@justeattakeaway/pie-webc/components/select.js');
 import('@justeattakeaway/pie-webc/components/text-input.js');
+import('@justeattakeaway/pie-webc/components/notification.js');
