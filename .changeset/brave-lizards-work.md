70046f56ae9e616df249f8f9ec81c9b1b15cb493
personal <jamieolivermaguire@googlemail.com>
2025-06-19 17:58:36 +0100

    feat(pie-storybook): DSW-3001 add contribution guide to sb

---
+++ .changeset/brave-lizards-work.md
@@ -0,0 +1,5 @@
+---
+"@justeattakeaway/pie-storybook": patch
+---
+
+[Added] - Contribution guide
--- apps/pie-storybook/.storybook/preview.ts
+++ apps/pie-storybook/.storybook/preview.ts
@@ -95,6 +95,7 @@ export default {
                     ],
                     'Contribution',
                     [
+                        'Overview',
                         'Testing',
                         [
                             'Overview',
