e3b17beb6d805e7f133c8e82d5dfb3b36654efdb
matthew.hardern <matthew.hardern@justeattakeaway.com>
2025-06-20 12:58:35 +0100

    test: Update PieList component tests and remove unused code
    Updates PieList component tests to use correct story ID and adds
    new tests for variant and dividers. Removes unused code in
    pie-list-item and ListPage. Improves test coverage and removes
    unnecessary attributes.

--- packages/components/pie-list/test/component/pie-list.spec.ts
+++ packages/components/pie-list/test/component/pie-list.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
+import { type ListProps } from '../../src/defs.ts';
 
 test.describe('PieList - Component tests', () => {
     test('should render successfully', async ({ page }) => {
         // Arrange
-        const basePage = new BasePage(page, 'components-list--default');
+        const basePage = new BasePage(page, 'list--default');
         await basePage.load();
 
         // Act
@@ -12,5 +13,39 @@ test.describe('PieList - Component tests', () => {
 
         // Assert
         await expect(listComponent).toBeVisible();
+    });
+
+    test('should render with variant="compact"', async ({ page }) => {
+        // Arrange
+        const listPage = new BasePage(page, 'list--default');
+        const props: ListProps = {
+            variant: 'compact',
+        };
+
+        await listPage.load({ ...props });
+
+        // Act
+        const listComponent = page.locator('pie-list');
+
+        // Assert
+        await expect(listComponent).toBeVisible();
+        await expect(listComponent).toHaveAttribute('variant', 'compact');
+    });
+
+    test('should render with dividers enabled', async ({ page }) => {
+        // Arrange
+        const listPage = new BasePage(page, 'list--default');
+        const props: ListProps = {
+            dividers: true,
+        };
+
+        await listPage.load({ ...props });
+
+        // Act
+        const listComponent = page.locator('pie-list');
+
+        // Assert
+        await expect(listComponent).toBeVisible();
+        await expect(listComponent).toHaveAttribute('dividers');
     });
 });
