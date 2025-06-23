4add9de34c66092c2c4fa1a39499f80c73792eb0
Raouf <raoufswe@gmail.com>
2025-06-23 17:55:03 +0200

    fix(pie-toast-provider): DSW-3107 update tests

--- packages/components/pie-toast-provider/test/visual/pie-toast-provider.spec.ts
+++ packages/components/pie-toast-provider/test/visual/pie-toast-provider.spec.ts
@@ -3,6 +3,8 @@ import percySnapshot from '@percy/playwright';
 import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
 import { positions } from '../../src/defs.ts';
 
+const directions = ['ltr', 'rtl'];
+
 test.describe('PieToastProvider - Visual tests`', () => {
     test('should display the PieToastProvider component successfully', async ({ page }) => {
         const basePage = new BasePage(page, 'toast-provider--custom-z-index');
@@ -19,15 +21,17 @@ test.describe('PieToastProvider - Visual tests`', () => {
     });
 
     positions.forEach((position) => {
-        test(`should render toast with position: ${position}`, async ({ page }) => {
-            const basePage = new BasePage(page, `toast-provider--position-${position}`);
+        directions.forEach((direction) => {
+            test(`should render position: ${position} correctly with direction: ${direction}`, async ({ page }) => {
+                const basePage = new BasePage(page, `toast-provider--position-${position}`);
 
-            basePage.load();
+                await basePage.load({}, { writingDirection: direction });
 
-            const toastElement = page.locator('pie-toast');
-            await toastElement.waitFor({ state: 'visible' });
+                const toastElement = page.locator('pie-toast');
+                await toastElement.waitFor({ state: 'visible' });
 
-            await percySnapshot(page, `PieToastProvider - position: ${position} Visual Test`);
+                await percySnapshot(page, `PieToastProvider - position: ${position} - direction: ${direction}`);
+            });
         });
     });
 });
