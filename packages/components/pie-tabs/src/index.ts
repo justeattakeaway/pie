313a1a8ebb599a6279d09dc0899b25f9f431236d
Jose De Freitas Jr <jose.defreitasjr@justeattakeaway.com>
2025-06-30 14:55:27 -0600

    feat(pie-tabs): DSW-3169 made component render on storybook

--- packages/components/pie-tabs/src/index.ts
+++ packages/components/pie-tabs/src/index.ts
@@ -1,27 +1,31 @@
-import { LitElement, html, unsafeCSS } from 'lit';
-import { RtlMixin, defineCustomElement } from '@justeattakeaway/pie-webc-core';
+import { html, unsafeCSS } from 'lit';
+import { PieElement } from '@justeattakeaway/pie-webc-core/src/internals/PieElement';
+import { safeCustomElement } from '@justeattakeaway/pie-webc-core';
 
 import styles from './tabs.scss?inline';
 import { type TabsProps } from './defs';
 
+const componentSelector = 'pie-tabs';
+
 // Valid values available to consumers
 export * from './defs';
-
-const componentSelector = 'pie-tabs';
 
 /**
  * @tagname pie-tabs
  */
-export class PieTabs extends RtlMixin(LitElement) implements TabsProps {
+@safeCustomElement(componentSelector)
+export class PieTabs extends PieElement implements TabsProps {
     render () {
-        return html`<h1 data-test-id="pie-tabs">Hello world!</h1>`;
+        return html`
+            <div>
+                <h1 data-test-id="pie-tabs">Hello world!</h1>
+            </div> 
+        `;
     }
 
     // Renders a `CSSResult` generated from SCSS by Vite
     static styles = unsafeCSS(styles);
 }
-
-defineCustomElement(componentSelector, PieTabs);
 
 declare global {
     interface HTMLElementTagNameMap {
