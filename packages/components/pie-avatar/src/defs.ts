f9b70d466fb179fdd70e95c798b0ef1f79ca82ae
Katarina Neskovic <katarina.neskovic@justeattakeaway.com>
2025-07-08 18:19:59 +0200

    define properties

--- packages/components/pie-avatar/src/defs.ts
+++ packages/components/pie-avatar/src/defs.ts
@@ -1,3 +1,16 @@
-// TODO - please remove the eslint disable comment below when you add props to this interface
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface AvatarProps {}
+import { type ComponentDefaultProps } from '@justeattakeaway/pie-webc-core';
+
+export const tags = ['a', 'button', 'div'] as const;
+
+export interface AvatarProps {
+    label?: string;
+    tag: typeof tags[number];
+
+}
+
+export type DefaultProps = ComponentDefaultProps<AvatarProps>;
+export const defaultProps: DefaultProps = {
+    tag: 'div',
+    label: '',
+};
+
--- packages/components/pie-avatar/src/index.ts
+++ packages/components/pie-avatar/src/index.ts
@@ -1,9 +1,12 @@
 import { html, unsafeCSS } from 'lit';
 import { PieElement } from '@justeattakeaway/pie-webc-core/src/internals/PieElement';
-import { RtlMixin, safeCustomElement } from '@justeattakeaway/pie-webc-core';
+import { RtlMixin, safeCustomElement, validPropertyValues } from '@justeattakeaway/pie-webc-core';
 
+import { property } from 'lit/decorators';
 import styles from './avatar.scss?inline';
-import { type AvatarProps } from './defs';
+import {
+    type AvatarProps, defaultProps, tags,
+} from './defs';
 
 // Valid values available to consumers
 export * from './defs';
@@ -15,7 +18,20 @@ const componentSelector = 'pie-avatar';
  */
 @safeCustomElement('pie-avatar')
 export class PieAvatar extends RtlMixin(PieElement) implements AvatarProps {
-    render () {
+    @property({ type: String })
+    @validPropertyValues(componentSelector, tags, defaultProps.tag)
+    public tag = defaultProps.tag;
+
+    @property({ type: String })
+    public label = defaultProps.label;
+
+    private generateInitials(name:string) {
+      const 
+
+        
+    }
+
+    render() {
         return html`<h1 data-test-id="pie-avatar">Hello world!</h1>`;
     }
 
