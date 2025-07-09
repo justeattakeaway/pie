931e8476c178b35c77d3b02e7ea47592ca76b23e
personal <jamieolivermaguire@googlemail.com>
2025-07-09 10:49:49 +0100

    feat(pie-storybook): DSW-3138 add ts usage guide

--- apps/pie-storybook/.storybook/preview.ts
+++ apps/pie-storybook/.storybook/preview.ts
@@ -77,6 +77,7 @@ export default {
                         'Events',
                         'Design tokens cookbook',
                         'CSS variables',
+                        'Typescript usage',
                         'Component versions',
                         'Browser support',
                     ],
