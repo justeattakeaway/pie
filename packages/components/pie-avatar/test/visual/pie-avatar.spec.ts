eab7c48fa3b0de3c383810bf4c27c40a057ccaf8
Katarina Neskovic <katarina.neskovic@justeattakeaway.com>
2025-07-21 18:37:45 +0200

    add visual tests and update test Playwright tests

--- packages/components/pie-avatar/test/visual/pie-avatar.spec.ts
+++ packages/components/pie-avatar/test/visual/pie-avatar.spec.ts
@@ -1,14 +1,27 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import percySnapshot from '@percy/playwright';
 import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
+import { avatar } from '../helpers/selectors';
 
 test.describe('PieAvatar - Visual tests`', () => {
-    test('should display the PieAvatar component successfully', async ({ page }) => {
-        const basePage = new BasePage(page, 'avatar--default');
+    test('should display the PieAvatar default variant successfully', async ({ page }) => {
+        const avatarPage = new BasePage(page, 'avatar--default');
+        const avatarComponent = page.locator(avatar.selectors.container.dataTestId);
+        avatarPage.load();
 
-        basePage.load();
-        await page.waitForTimeout(2500);
+        await expect.soft(avatarComponent).toBeVisible();
+        await percySnapshot(page, 'PieAvatar - Default Test');
+    });
 
-        await percySnapshot(page, 'PieAvatar - Visual Test');
+    test('should display the PieAvatar component when label is provided', async ({ page }) => {
+        const avatarPage = new BasePage(page, 'avatar--default');
+        avatarPage.args = 'label:Alice Johnson';
+        const avatarComponent = page.locator(avatar.selectors.container.dataTestId);
+        avatarPage.load();
+
+        await expect.soft(avatarComponent).toBeVisible();
+        await percySnapshot(page, 'PieAvatar - Label Provided Test');
     });
 });
+
+// do just 2 different tests, then if i want to i can put it in a for-each loop
