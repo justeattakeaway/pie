e9029638fde40f0cdad9d9768147ec01cc69eca6
Jose De Freitas Jr <jose.defreitasjr@justeattakeaway.com>
2025-07-28 11:20:37 -0600

    feat(pie-data-table): DSW-3271 added files from generator

---
+++ apps/pie-storybook/stories/pie-data-table.stories.ts
@@ -0,0 +1,34 @@
+import { html } from 'lit';
+import { type Meta } from '@storybook/web-components';
+
+import '@justeattakeaway/pie-data-table';
+import { type DataTableProps } from '@justeattakeaway/pie-data-table';
+
+import { createStory } from '../utilities';
+
+type DataTableStoryMeta = Meta<DataTableProps>;
+
+const defaultArgs: DataTableProps = {};
+
+const dataTableStoryMeta: DataTableStoryMeta = {
+    title: 'Components/Data Table',
+    component: 'pie-data-table',
+    argTypes: {},
+    args: defaultArgs,
+    parameters: {
+        design: {
+            type: 'figma',
+            url: '',
+        },
+    },
+};
+
+export default dataTableStoryMeta;
+
+// TODO: remove the eslint-disable rule when props are added
+// eslint-disable-next-line no-empty-pattern
+const Template = ({}: DataTableProps) => html`
+    <pie-data-table></pie-data-table>
+`;
+
+export const Default = createStory<DataTableProps>(Template, defaultArgs)();
