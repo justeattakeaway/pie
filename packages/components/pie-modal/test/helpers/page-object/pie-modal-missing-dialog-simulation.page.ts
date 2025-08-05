4ed3254e9bc01ff2d71f0462fdea1c73b97a7d2f
personal <jamieolivermaguire@googlemail.com>
2025-08-05 17:07:58 +0100

    fix(pie-modal): DSW-3292 avoid scroll-lock freeze and TypeError on disconnectedCallback

---
+++ packages/components/pie-modal/test/helpers/page-object/pie-modal-missing-dialog-simulation.page.ts
@@ -0,0 +1,15 @@
+import { type Page, type Locator } from '@playwright/test';
+import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
+
+export class ModalMissingDialogSimulationPage extends BasePage {
+    readonly runTestButton: Locator;
+    readonly topOfPageCopy: Locator;
+    readonly bottomOfPageCopy: Locator;
+
+    constructor (page: Page) {
+        super(page, 'modal--missing-dialog-simulation');
+        this.runTestButton = page.locator('[data-test-id="run-test-button"]');
+        this.topOfPageCopy = page.getByText('Top of page copy');
+        this.bottomOfPageCopy = page.getByText('Bottom of page copy');
+    }
+}
