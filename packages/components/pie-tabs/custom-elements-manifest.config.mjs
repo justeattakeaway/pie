e683f011e555c74cdc663c05222402ddfbeaccb1
Jose De Freitas Jr <jose.defreitasjr@justeattakeaway.com>
2025-08-05 11:15:34 -0600

    feat(pie-tabs): DSW-3169 added files from generator
    # Conflicts:
    #	apps/pie-storybook/package.json
    #	packages/components/pie-webc/package.json
    #	yarn.lock

---
+++ packages/components/pie-tabs/custom-elements-manifest.config.mjs
@@ -0,0 +1,15 @@
+import { moduleFileExtensionsPlugin } from 'cem-plugin-module-file-extensions';
+
+export default {
+    globs: [
+        './src/**/!(*.css).ts',
+    ],
+    exclude: [
+        '**/*.d.ts',
+        '**/*.d.js',
+        '**/react.ts',
+        '**/test/**',
+        '**/node_modules/**',
+    ],
+    plugins: [moduleFileExtensionsPlugin()],
+};
