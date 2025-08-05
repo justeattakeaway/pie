0a96f63aec263b76542daf4e2f7dfb0cc14374f7
Jose De Freitas Jr <jose.defreitasjr@justeattakeaway.com>
2025-08-05 14:55:06 -0600

    feat(pie-tabs): DSW-3303 implemented default properties

--- packages/components/pie-tabs/src/defs.ts
+++ packages/components/pie-tabs/src/defs.ts
@@ -1,3 +1,24 @@
-// TODO - please remove the eslint disable comment below when you add props to this interface
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface TabsProps {}
+import { type ComponentDefaultProps } from '@justeattakeaway/pie-webc-core';
+
+export const variants = ['global', 'contained'] as const;
+export const orientations = ['horizontal', 'vertical'] as const;
+
+export interface TabsProps {
+  /**
+   * Optional variant for styling the tabs component.
+   * Default is 'global'.
+   */
+  variant?: typeof variants[number];
+  /**
+   * Optional property to set the orientation of the tabs.
+   * Default is 'horizontal'.
+   */
+  orientation?: typeof orientations[number];
+}
+
+export type DefaultProps = ComponentDefaultProps<TabsProps>;
+
+export const defaultProps: DefaultProps = {
+    variant: 'global',
+    orientation: 'horizontal',
+};
