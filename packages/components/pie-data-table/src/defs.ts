f163d1dd0bc0d5baac2d246bc1638884a446f8d4
Jose De Freitas Jr <jose.defreitasjr@justeattakeaway.com>
2025-09-05 14:37:37 -0600

    feat(pie-data-table): DSW-3363 exported missing types

--- packages/components/pie-data-table/src/defs.ts
+++ packages/components/pie-data-table/src/defs.ts
@@ -26,8 +26,8 @@ export interface Column {
   accessor?: string;
 }
 
-type AdditionalCell = {
-  content: string;
+export type AdditionalCell = {
+  content: string | number;
   textAlign?: TextAlign;
   colSpan?: number;
   hideCell?: boolean;
