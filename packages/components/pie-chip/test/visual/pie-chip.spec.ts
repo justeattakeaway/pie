b472d54672ff7eb3143620af80ca1dcfbd6fa482
personal <jamieolivermaguire@googlemail.com>
2025-09-05 12:57:51 +0100

    VRTs

--- packages/components/pie-chip/test/visual/pie-chip.spec.ts
+++ packages/components/pie-chip/test/visual/pie-chip.spec.ts
@@ -2,15 +2,23 @@ import { test } from '@playwright/test';
 import percySnapshot from '@percy/playwright';
 import { percyWidths } from '@justeattakeaway/pie-webc-testing/src/percy/breakpoints.ts';
 import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
-import { variants } from '../../src/defs.ts';
+import { type ChipProps, variants, types } from '../../src/defs.ts';
 
-variants.forEach((variant) => test(`should render all prop variations for Variant: ${variant}`, async ({ page }) => {
-    const basePage = new BasePage(page, `chip--${variant}-prop-variations`);
+types.forEach((type) => {
+    variants.forEach((variant) => {
+        test(`should render - Type: ${type}, Variant: ${variant}`, async ({ page }) => {
+            const basePage = new BasePage(page, `chip--${variant}-prop-variations`);
+            const props: Partial<ChipProps> = {
+                type,
+            };
 
-    basePage.load();
+            await basePage.load(props);
 
-    // Follow up to remove in Jan
-    await page.waitForTimeout(5000);
+            // Follow up to remove in Jan 2026
+            await page.waitForTimeout(5000);
 
-    await percySnapshot(page, `PIE Chip - Variant: ${variant}`, percyWidths);
-}));
+            // Note: The Percy snapshot name is now more specific for the button type
+            await percySnapshot(page, `PIE Chip - Type: ${type}, Variant: ${variant}`, percyWidths);
+        });
+    });
+});
