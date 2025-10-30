9fd7fb222f9e546f8142722b17b51d0e301e154f
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2025-10-30 17:49:18 +0100

    feat(eslint-plugin-snacks-pie-migration): DSW-3482 improve test structure

--- packages/tools/eslint-plugin-snacks-pie-migration/__tests__/deprecated-components.test.mjs
+++ packages/tools/eslint-plugin-snacks-pie-migration/__tests__/deprecated-components.test.mjs
@@ -1,35 +1,40 @@
 import { ESLint } from 'eslint';
 import { describe, expect, it } from 'vitest';
 
-// Configure ESLint to use our plugin AND rule
-const eslint = new ESLint({
-    useEslintrc: false,
-    baseConfig: {
-        plugins: ['@justeattakeaway/snacks-pie-migration'],
-        rules: {
-            '@justeattakeaway/snacks-pie-migration/deprecated-components': 'error'
+function getEslintInstance() {
+    return new ESLint({
+        useEslintrc: false,
+        baseConfig: {
+            plugins: ['@justeattakeaway/snacks-pie-migration'],
+            rules: {
+                '@justeattakeaway/snacks-pie-migration/deprecated-components': 'error'
+            },
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: 'module',
+            }
         },
-        parserOptions: {
-            ecmaVersion: 2020,
-            sourceType: 'module',
-        }
-    },
-});
+    });
+}
+
+function lintText(sourceCode) {
+    return getEslintInstance().lintText(sourceCode, { filePath: 'test.js' });
+}
 
 describe('deprecated-components rule', () => {
     describe('import declarations', () => {
         it('detects deprecated components', async () => {
             const sourceCode = `import { Button } from 'snacks-design-system';`;
-            const result = await eslint.lintText(sourceCode, { filePath: 'test.js' });
+            const result = await lintText(sourceCode);
 
             expect(result[0].messages[0]).toMatchSnapshot();
         })
         it('doesnt detect deprecated components when the component is not in the deprecation list', async () => {
             const sourceCode = `import { FooBar } from 'snacks-design-system';`;
-            const result = await eslint.lintText(sourceCode, { filePath: 'test.js' });
+            const result = await lintText(sourceCode);
 
             expect(result.length).toBe(1);
-            expect(result[0].messages.length).toBe(0);
+            expect(result[0].messages.length).toBe(0); // No errors here
         })
     });
 });
