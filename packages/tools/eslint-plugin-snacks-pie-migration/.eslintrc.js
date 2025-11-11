27003aa717431ff732e5b4c836866a695ccc93f9
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2025-11-11 14:29:12 +0100

    feat(eslint-plugin-snacks-pie-migration): DSW-3482 use eslint-plugin plugin

--- packages/tools/eslint-plugin-snacks-pie-migration/.eslintrc.js
+++ packages/tools/eslint-plugin-snacks-pie-migration/.eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-    extends: [require.resolve('@justeattakeaway/eslint-config-pie/strict')],
-    rules: {},
+    extends: [
+        require.resolve('@justeattakeaway/eslint-config-pie/strict'),
+        'plugin:eslint-plugin/recommended'
+    ],
     parserOptions: {
         ecmaVersion: 2020,
     },
