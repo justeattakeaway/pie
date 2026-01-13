bfdc7c61696be53d0c1cfd1c7927de942f83d8b6
personal <jamieolivermaguire@googlemail.com>
2026-01-13 15:44:40 +0000

    revert test skips

--- packages/components/pie-list/test/visual/pie-list.spec.ts
+++ packages/components/pie-list/test/visual/pie-list.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import percySnapshot from '@percy/playwright';
 import { BasePage } from '@justeattakeaway/pie-webc-testing/src/helpers/page-object/base-page.ts';
 
-test.describe.skip('PieList - Visual tests`', () => {
+test.describe('PieList - Visual tests`', () => {
     test('should display the PieList component successfully', async ({ page }) => {
         const basePage = new BasePage(page, 'list--default');
 
