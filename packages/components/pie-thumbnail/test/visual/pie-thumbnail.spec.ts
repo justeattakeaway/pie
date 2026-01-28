0586498f1c5c64cfcab4bba0e8df2691d390c629
Ashley Watson-Nolan <ashley.watson-nolan@justeattakeaway.com>
2026-01-28 11:38:59 +0000

    feat(pie-monorepo): DSW-123 temporarily comment out thumbnail test for chromatic trial

--- packages/components/pie-thumbnail/test/visual/pie-thumbnail.spec.ts
+++ packages/components/pie-thumbnail/test/visual/pie-thumbnail.spec.ts
@@ -44,15 +44,15 @@ test('should render all prop variations for the 16by9 aspect ratio', async ({ pa
     await percySnapshot(page, 'PIE Thumbnail - aspectRatio: 16by9', { widths: [1280] });
 });
 
-// test('should render the component default placeholder on image load failure', async ({ page }) => {
-//     const basePage = new BasePage(page, 'thumbnail--invalid-src-and-default-placeholder');
-//     await basePage.load();
+test.skip('should render the component default placeholder on image load failure', async ({ page }) => {
+    const basePage = new BasePage(page, 'thumbnail--invalid-src-and-default-placeholder');
+    await basePage.load();
 
-//     const thumbnailComponent = page.getByTestId(thumbnail.selectors.container.dataTestId).first();
-//     await expect.soft(thumbnailComponent).toBeVisible();
+    const thumbnailComponent = page.getByTestId(thumbnail.selectors.container.dataTestId).first();
+    await expect.soft(thumbnailComponent).toBeVisible();
 
-//     await percySnapshot(page, 'PIE Thumbnail - default placeholder', { widths: [1280] });
-// });
+    await percySnapshot(page, 'PIE Thumbnail - default placeholder', { widths: [1280] });
+});
 
 test('should not render the component default placeholder if the `hideDefaultPlaceholder` is set to true', async ({ page }) => {
     const basePage = new BasePage(page, 'thumbnail--default');
