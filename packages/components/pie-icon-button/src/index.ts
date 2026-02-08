6109fa17f427273092eab8b7ed22cc2b13edff87
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2026-02-08 10:46:55 +0100

    feat(pie-icon-button): DSW-3687 update pie-icon-button inverse-outline variant spinner variant

--- packages/components/pie-icon-button/src/index.ts
+++ packages/components/pie-icon-button/src/index.ts
@@ -49,6 +49,7 @@ export class PieIconButton extends DelegatesFocusMixin(PieElement) implements Ic
         if (/secondary|translucent/.test(variant)) spinnerVariant = 'secondary';
         if (variant === 'ghost-secondary-dark') spinnerVariant = 'secondary-dark';
         if (variant === 'ghost-inverse-light') spinnerVariant = 'inverse-light';
+        if (variant === 'inverse-outline') spinnerVariant = 'inverse';
         if ((variant.includes('primary') && !disabled) || variant === 'ghost-inverse') spinnerVariant = 'inverse';
 
         return html`
