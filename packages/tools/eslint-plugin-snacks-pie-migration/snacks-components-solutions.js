f22229669561a71d8a521a8079c4ce02c9bae657
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2026-03-11 16:26:05 +0100

    feat(eslint-plugin-snacks-pie-migration): DSW-3606 update docs

--- packages/tools/eslint-plugin-snacks-pie-migration/snacks-components-solutions.js
+++ packages/tools/eslint-plugin-snacks-pie-migration/snacks-components-solutions.js
@@ -1,5 +1,6 @@
-// This file is used by the eslint plugin to warn about usage of deprecated Snacks components.
-// Update this file when a Snacks component is deprecated and doesn't map directly to a PIE component.
+// This file is used by the eslint plugin to hint developers on how to migrate away from deprecated Snacks components
+// - Update this file when a Snacks component is deprecated and doesn't map directly to a PIE component
+// - Update this file when a Snacks migration skill is available for components replaced by a PIE component
 // Unlike `snacks-components-data.json`, this file can be edited by hand.
 
 const migrateWithSkill = 'Use the "snacks-migration-assistant" skill to migrate this component.\nCheck this page for more details: https://www.pie.design/agents/resources/#snacks-migration-skill';
