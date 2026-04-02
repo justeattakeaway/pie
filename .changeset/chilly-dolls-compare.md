58652bf8bb17f0b85e8f099c64290bf9564cb3e8
Ashley Watson-Nolan <ashley.watson-nolan@justeattakeaway.com>
2026-04-02 11:15:53 +0100

    ci(pie-monorepo): DSW-3816 update workflows from bot to gh app (#2746)

---
+++ .changeset/chilly-dolls-compare.md
@@ -0,0 +1,6 @@
+---
+"@justeattakeaway/pie-icons": minor
+"pie-monorepo": minor
+---
+
+[Changed] - update workflows from bot to gh app
--- packages/tools/pie-icons/bin/update-icons.mjs
+++ packages/tools/pie-icons/bin/update-icons.mjs
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 import path from 'path';
-import { execSync } from 'child_process';
+import { execFileSync, execSync } from 'child_process';
 import {
     emptyDirSync, removeSync, readJSONSync, writeJsonSync,
 } from 'fs-extra/esm';
@@ -212,8 +212,11 @@ async function updateIcons () {
         // check if is running on GHA and setup the git user
         if (process.env.GITHUB_ACTIONS) {
             // configure git and push
-            execSync('git config --global user.name "pie-design-system-bot"');
-            execSync('git config --global user.email "username@users.noreply.github.com"');
+            const gitUserName = process.env.GIT_USER_NAME || 'github-actions[bot]';
+            const gitUserEmail = process.env.GIT_USER_EMAIL || '41898282+github-actions[bot]@users.noreply.github.com';
+
+            execFileSync('git', ['config', '--global', 'user.name', gitUserName]);
+            execFileSync('git', ['config', '--global', 'user.email', gitUserEmail]);
         }
 
         const gitUpdatedPaths = [changesetFilePath, iconsDataFilePath, pieDocsTestsPath, pieDocsChangesetFilePath]
