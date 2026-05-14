d2972cecf5457d45235732506168f85d675a2ea6
siggerzz <bensiggery28@gmail.com>
2026-05-14 12:37:01 +0100

    chore(pie-monorepo): DSW-123 add new Axe \`best-practice\` rule
    Update preview.ts
    
    remove status link in tag
    
    update target in fixtures
    
    chore: disable certain pie-docs a11y rules until fixed
    
    chore: disable certain pie-docs a11y rules until fixed
    
    chore: disable certain pie-docs a11y rules until fixed
    
    chore: disable certain pie-docs a11y rules until fixed

--- packages/components/pie-webc-testing/src/playwright/playwright-fixtures.ts
+++ packages/components/pie-webc-testing/src/playwright/playwright-fixtures.ts
@@ -12,8 +12,9 @@ interface ExtendedTestContext {
 export const test = baseTest.extend<ExtendedTestContext>({
     makeAxeBuilder: [async ({ page }: { page: Page }, use: (builder: () => AxeBuilder) => Promise<void>) => {
         const makeAxeBuilder = () => new AxeBuilder({ page })
-        .withTags(['wcag21a', 'wcag21aa', 'wcag143', 'cat.color', 'cat.aria'])
-        .disableRules(['color-contrast-enhanced']);
+        .withTags(['wcag21a', 'wcag21aa', 'wcag143', 'cat.color', 'cat.aria', 'best-practice'])
+        .disableRules(['color-contrast-enhanced'])
+        .include('#root-inner');
 
         await use(makeAxeBuilder);
     }, {
