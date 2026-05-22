0b4c36dd15ae190332c2eb67135b5396ff6c07d2
siggerzz <bensiggery28@gmail.com>
2026-05-22 14:04:30 +0100

    chore(pie-monorepo): DSW-123 update deps

--- packages/tools/pie-icons/bin/process-svg.js
+++ packages/tools/pie-icons/bin/process-svg.js
@@ -38,9 +38,6 @@ function optimizeSVG (svg) {
                     name: 'preset-default',
                     params: {
                         overrides: {
-                            // viewBox is required to resize SVGs with CSS.
-                            // @see https://github.com/svg/svgo/issues/1128
-                            removeViewBox: false,
                             convertShapeToPath: false,
                             mergePaths: false,
                             cleanupIds: false,
