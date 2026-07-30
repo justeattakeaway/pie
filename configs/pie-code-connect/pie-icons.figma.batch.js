d5075bcaea5ef3421582c32d59b12578c27668d8
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2026-07-30 17:16:30 +0200

    chore(pie-code-connect): DSW-4203 simplify icons template

--- configs/pie-code-connect/pie-icons.figma.batch.js
+++ configs/pie-code-connect/pie-icons.figma.batch.js
@@ -9,27 +9,23 @@ const iconPrefix = isReact ? 'Icon' : 'icon-';
 const isFilled = getInstanceProp('getEnum', 'Fill', { True: true, False: false });
 const isLarge = getInstanceProp('getEnum', 'Size', { Large: true, Small: false });
 
-function getFillSuffix (isReact) {
-    if (isFilled) {
-        return isReact ? 'Filled' : '-filled';
-    }
+function getFillSuffix () {
+    if (isFilled) return isReact ? 'Filled' : '-filled';
     return '';
 }
 
-function getSizeSuffix (isReact) {
-    if (isLarge) {
-        return isReact ? 'Large' : '-large';
-    }
+function getSizeSuffix () {
+    if (isLarge) return isReact ? 'Large' : '-large';
     return '';
 }
 
 function getComponentName (isReact) {
-    return `${isReact ? componentNameReact : componentName}${getFillSuffix(isReact)}${getSizeSuffix(isReact)}`;
+    return `${isReact ? componentNameReact : componentName}${getFillSuffix()}${getSizeSuffix()}`;
 }
 
 const importStatement = isReact
-    ? `import { ${getComponentName(isReact)} } from "@justeattakeaway/pie-icons-webc/dist/react/${getComponentName(isReact)}.js"`
-    : `import '@justeattakeaway/pie-icons-webc/dist/${getComponentName(true)}.js'`;
+    ? `import { ${getComponentName(true)} } from "@justeattakeaway/pie-icons-webc/dist/react/${getComponentName(true)}.js"`
+    : `import "@justeattakeaway/pie-icons-webc/dist/${getComponentName(true)}.js"`;
 
 export default {
     example: figma.code`<${iconPrefix}${getComponentName(isReact)}></${iconPrefix}${getComponentName(isReact)}>`,
