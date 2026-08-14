1d5d8de238b688d81ac4d211e279929545422a83
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2026-08-14 14:59:11 +0200

    feat(pie-code-connect): DSW-4206 add cc mapping (tag)

---
+++ configs/pie-code-connect/pie-tag.figma.batch.js
@@ -0,0 +1,65 @@
+const figma = require('figma');
+const createGetInstanceProp = require('./utils/get-instance-prop.js');
+const renderProp = require('./utils/render-prop.js');
+const getIconSnippet = require('./utils/get-icon-snippet.js');
+const getImportStatement = require('./utils/get-import-statement.js');
+
+const getInstanceProp = createGetInstanceProp(figma);
+const { componentName, componentNameReact } = figma.batch;
+const selectedComponentName = process.env.FRAMEWORK === 'react' ? componentNameReact : componentName;
+
+// Map figma props
+const state = getInstanceProp('getPropertyValue', 'State');
+const isDimmed = state === 'Disabled';
+
+const isStrong = getInstanceProp('getPropertyValue', 'Strong') === 'True';
+const hasLeadingIcon = getInstanceProp('getBoolean', 'Leading icon');
+
+const label = getInstanceProp('getString', '[𝐓] Label');
+
+const variant = getInstanceProp('getEnum', 'Type', {
+    Neutral: 'neutral',
+    'Neutral - alternative': 'neutral-alternative',
+    Ghost: 'ghost',
+    Outline: 'outline',
+    Translucent: 'translucent',
+    Information: 'information',
+    Success: 'success',
+    Error: 'error',
+    Warning: 'warning',
+    '02 Orange subtle': 'brand-02',
+    '03 Cupcake': 'brand-03',
+    '04 Berry': 'brand-04',
+    '05 Turmeric': 'brand-05',
+    '06 Aubergine': 'brand-06',
+    '08 Latte': 'brand-08',
+});
+
+const size = getInstanceProp('getEnum', 'Size', {
+    Large: 'large',
+    Small: 'small',
+});
+
+// Get icon instance and add the slot prop to the snippet
+const iconInstance = hasLeadingIcon && getInstanceProp('getInstanceSwap', 'Replace icon');
+const iconSnippet = getIconSnippet(iconInstance, (code) => code.replace('></', ' slot="icon"></'));
+
+const props = [
+    renderProp('variant', variant, 'neutral'),
+    renderProp('size', size, 'large'),
+    renderProp('isStrong', isStrong, false),
+    renderProp('isDimmed', isDimmed, false),
+    renderProp('hasLeadingIcon', hasLeadingIcon, false),
+].filter(Boolean).join('\n    ');
+
+const template = figma.code`<${selectedComponentName}
+    ${props}>
+    ${iconSnippet}
+    ${label}
+</${selectedComponentName}>`;
+
+export default {
+    example: template,
+    imports: [getImportStatement(componentName, componentNameReact)],
+    id: 'pie-tag',
+};
