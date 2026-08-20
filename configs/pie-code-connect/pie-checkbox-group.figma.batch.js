1394ea6d9a4ee4882a1e445cb25984bba33206b6
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2026-08-20 11:06:42 +0200

    feat(pie-code-connect): DSW-4209 add Code Connect template for pie-checkbox-group

--- configs/pie-code-connect/pie-checkbox-group.figma.batch.js
+++ configs/pie-code-connect/pie-checkbox-group.figma.batch.js
@@ -2,11 +2,12 @@ const figma = require('figma');
 const createGetInstanceProp = require('./utils/get-instance-prop.js');
 const renderProp = require('./utils/render-prop.js');
 const getImportStatement = require('./utils/get-import-statement.js');
+const createGetInstanceTemplate = require('./utils/get-instance-template.js');
 
 const getInstanceProp = createGetInstanceProp(figma);
+const getInstanceTemplate = createGetInstanceTemplate(figma);
 const { componentName, componentNameReact } = figma.batch;
 const selectedComponentName = process.env.FRAMEWORK === 'react' ? componentNameReact : componentName;
-const selectedChildName = process.env.FRAMEWORK === 'react' ? 'PieCheckbox' : 'pie-checkbox';
 const selectedLabelName = process.env.FRAMEWORK === 'react' ? 'PieFormLabel' : 'pie-form-label';
 
 const isInline = getInstanceProp('getPropertyValue', 'Orientation') === 'Horizontal';
@@ -19,12 +20,16 @@ const status = getInstanceProp('getEnum', 'Error', {
 
 const assistiveText = getInstanceProp(['Assistive text'], 'getPropertyValue', '[𝐓] Assistive text') || '';
 
-const hasCheckbox3 = getInstanceProp('getBoolean', 'Checkbox 3');
-const hasCheckbox4 = getInstanceProp('getBoolean', 'Checkbox 4');
-const hasCheckbox5 = getInstanceProp('getBoolean', 'Checkbox 5');
-
 const hasFormLabel = getInstanceProp('getBoolean', 'Form label');
 const formLabelSnippet = `<${selectedLabelName} slot="label" optional="Optional">Label</${selectedLabelName}>`;
+
+const checkboxes = [
+    getInstanceTemplate(['Checkbox 1']),
+    getInstanceTemplate(['Checkbox 2']),
+    getInstanceTemplate(['Checkbox 3']),
+    getInstanceTemplate(['Checkbox 4']),
+    getInstanceTemplate(['Checkbox 4']),
+];
 
 const props = [
     renderProp('isInline', isInline, false),
@@ -33,25 +38,17 @@ const props = [
     renderProp('assistiveText', assistiveText, ''),
 ].filter(Boolean).join('\n    ');
 
-const children = [
-    hasFormLabel ? formLabelSnippet : '',
-    `<${selectedChildName} name="checkbox-1">Label 1</${selectedChildName}>`,
-    `<${selectedChildName} name="checkbox-2">Label 2</${selectedChildName}>`,
-    hasCheckbox3 ? `<${selectedChildName} name="checkbox-3">Label 3</${selectedChildName}>` : '',
-    hasCheckbox4 ? `<${selectedChildName} name="checkbox-4">Label 4</${selectedChildName}>` : '',
-    hasCheckbox5 ? `<${selectedChildName} name="checkbox-5">Label 5</${selectedChildName}>` : '',
-].filter(Boolean).join('\n    ');
-
 const template = figma.code`<${selectedComponentName}
     ${props}>
-    ${children}
+    ${hasFormLabel ? formLabelSnippet : ''}
+    ${checkboxes}
 </${selectedComponentName}>`;
 
 export default {
     example: template,
     imports: [
         getImportStatement(componentName, componentNameReact),
-        getImportStatement('pie-checkbox', 'PieCheckbox'),
+        getImportStatement('pie-form-label', 'PieFormLabel'),
     ],
-    id: 'pie-checkbox-group',
+    id: componentName,
 };
