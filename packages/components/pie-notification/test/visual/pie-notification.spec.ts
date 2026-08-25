e11fd1d5dddc1c218389378215c1244e4adcf7dc
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2026-08-25 11:58:38 +0200

    test(pie-storybook): DSW-4291 add dismissible notification story with long copy

--- packages/components/pie-notification/test/visual/pie-notification.spec.ts
+++ packages/components/pie-notification/test/visual/pie-notification.spec.ts
@@ -433,3 +433,11 @@ test.describe('Slotted Action Buttons', () => {
         await percySnapshot(page, 'PieNotification - Slotted Both Actions Stacked Compact - Should Not Stack', screenWidths);
     });
 });
+
+test('should render dismissible notification with long copy', async ({ page }) => {
+    const basePage = new BasePage(page, 'notification--dismissible-long-copy');
+
+    await basePage.load();
+
+    await percySnapshot(page, 'PieNotification - Dismissible with long copy', screenWidths);
+});
